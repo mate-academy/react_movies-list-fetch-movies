4884ecf20b18cacd4e748e51e344250f7b7b58bf
AlexanderShpilka <sandro.bot.evil@gmail.com>
2020-03-06 20:08:03 +0200

    Fix url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 const API_KEY = '9260da49';
 
-export const BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+export const BASE_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&`;
 export const imdbUrl = 'https://www.imdb.com/title';

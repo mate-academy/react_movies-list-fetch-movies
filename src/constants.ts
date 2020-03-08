c676f86f214dd8c3ae2293731df81da62ef39c2d
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-08 17:33:23 +0200

    https

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://www.omdbapi.com/?apikey=f9a214f6&t=';
+export const BASE_URL = 'https://www.omdbapi.com/?apikey=f9a214f6&t=';
 export const IMDB_URL = 'https://www.imdb.com/title/';

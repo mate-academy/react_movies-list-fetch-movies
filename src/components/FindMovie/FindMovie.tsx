012893740ccac59316b6592473f83bf4a0da1409
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-08 17:27:11 +0200

    Movies

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,55 +1,122 @@
-import React from 'react';
+import React, {
+  FC,
+  useState,
+  ChangeEvent,
+  FormEvent,
+} from 'react';
 import './FindMovie.scss';
 
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { getMovie } from '../../api';
+import { IMDB_URL } from '../../constants';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+interface Props {
+  addMovie: (movie: Movie) => void;
+}
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+export const FindMovie: FC<Props> = ({ addMovie }) => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [error, setError] = useState(false);
+
+  const findMovie = (): void => {
+    getMovie(query)
+      .then(movieFromServer => {
+        const {
+          Title: title,
+          Plot: description,
+          Poster: imgUrl,
+          imdbID: imdbId,
+          Response,
+        } = movieFromServer;
+
+        if (Response === 'True') {
+          const imdbUrl = IMDB_URL + imdbId;
+          const newMovie = {
+            title,
+            description,
+            imgUrl,
+            imdbId,
+            imdbUrl,
+          };
+
+          setMovie(newMovie);
+        } else {
+          setError(true);
+        }
+      });
+  };
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target: { value } } = event;
+
+    setQuery(value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (movie) {
+      addMovie(movie);
+      setQuery('');
+      setMovie(null);
+    }
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={handleSubmit}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              value={query}
+              onChange={handleInput}
+              className={error ? 'input is-danger' : 'input'}
+            />
+          </div>
+          {error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary"
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && <MovieCard {...movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+    </>
+  );
+};
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://www.omdbapi.com/?apikey=f9a214f6&t=';
+export const IMDB_URL = 'https://www.imdb.com/title/';

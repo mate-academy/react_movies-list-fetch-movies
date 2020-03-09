88732c65cd900fd2d47ac08a0e85eb300a13e796
Vitalii <vitaliybondarenko1982@gmail.com>
2020-03-09 22:07:46 +0200

    on review

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
+import { Movie } from './utils/interfaces';
 
-export class App extends Component {
+interface State {
+  movies: Movie[];
+}
+
+export class App extends Component <{}, State> {
   state = {
     movies: data,
   };
+
+  addMovie = (movie: Movie): void => {
+    this.setState(prevState => ({
+      movies: [...prevState.movies, { ...movie }],
+    }));
+  };
+
 
   render() {
     const { movies } = this.state;
@@ -18,7 +30,7 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <FindMovie />
+          <FindMovie addMovie={this.addMovie} />
         </div>
       </div>
     );

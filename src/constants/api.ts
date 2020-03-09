8ea07865848bfc6ba73f099824dc4f6be2e08e2d
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-03-09 20:31:34 +0200

    solution_1.2

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://www.omdbapi.com/?apikey=a4fd7600&t=';
+export const BASE_URL = 'https://www.omdbapi.com/?apikey=a4fd7600&t=';
 export const URL_IMDB = 'https://www.imdb.com/title/';

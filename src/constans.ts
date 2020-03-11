7a90ae978b9827b4a396f6ec2df28d95097b989d
SahaPylypyuk <sashapulupyk@gmail.com>
2020-03-11 13:14:51 +0200

    URL fixed

--- src/constans.ts
+++ src/constans.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=fbcb17c&t=';
+export const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=fbcb17c&t=';
 export const IMDB_URL = 'https://www.imdb.com/title/';

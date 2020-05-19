9a1be7664e5cf425948ebc727cfb8fea57a47b51
Andreas_Just <andreas.wq@gmail.com>
2020-05-19 19:50:31 +0300

    Solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
   extends: '@mate-academy/eslint-config-react-typescript',
   rules: {
-    "jsx-a11y/label-has-associated-control": 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': ['error', {
+      'allowShortCircuit': true,
+      'allowTernary': true,
+    }],
+    'arrow-body-style': 0,
+    'object-curly-newline': [2, {
+      'ObjectExpression': {
+        'consistent': true,
+        'minProperties': 5,
+      },
+    }],
   }
 };
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,55 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import './FindMovie.scss';
-
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { getMovies } from '../../api/getMovies';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+const defaultMovie: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+type Props = {
+  movies: Movie[];
+  setMovies: (value: Movie[]) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ movies, setMovies }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResult, setSearchResult] = useState<Movie>(defaultMovie);
+  const [isLoaded, setLoaded] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState('');
+
+  const findMovie = async () => {
+    setLoading(true);
+    const data = await getMovies(searchQuery)
+      .then(response => response.json())
+      .catch(error => setError(`Something went wrong! ${error.message}.`));
+
+    if (data) {
+      const foundMovie: Movie = {
+        title: data.Title,
+        description: data.Plot,
+        imgUrl: data.Poster,
+        imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
+        imdbId: data.imdbID,
+      };
+
+      setLoading(false);
+      setLoaded(true);
+      setSearchResult(foundMovie);
+    }
+  };
+
+  const checkMovie = () => {
+    if (!searchResult.imdbId) {
+      return;
+    }
+
+    if (movies.find(movie => movie.imdbId === searchResult.imdbId)) {
+      setError('This movie is already in the catalog.');
+
+      return;
+    }
+
+    setMovies([...movies, searchResult]);
+    setSearchResult(defaultMovie);
+    setLoaded(false);
+    setSearchQuery('');
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    findMovie();
+  };
+
+  const handleClickSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    checkMovie();
+  };
+
+  return (
+    <>
+      <form className="find-movie" onSubmit={handleFormSubmit}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={cn('input', { 'is-danger': isError })}
+              value={searchQuery}
+              autoComplete="off"
+              onChange={e => {
+                setSearchQuery(e.target.value);
+                setLoaded(false);
+                setError('');
+              }}
+            />
+          </div>
+          {isError && <p className="help is-danger">{isError}</p>}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
-      </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className={cn('button is-warning', {
+                'is-light': isLoaded,
+                'is-loading': isLoading,
+                'border-dark': isLoaded,
+              })}
+              disabled={isLoaded}
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
+          <div className="control">
+            <button
+              type="button"
+              className={cn('button is-warning', {
+                'is-light': !isLoaded,
+                'border-dark': !isLoaded,
+              })}
+              disabled={!isLoaded}
+              onClick={handleClickSubmit}
+            >
+              Add to the list
+            </button>
+          </div>
         </div>
+      </form>
 
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
+      {searchResult.imdbId && (
+        <div className="container">
+          <h2 className="title">Preview</h2>
+          <MovieCard {...searchResult} />
         </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+      )}
+    </>
+  );
+};

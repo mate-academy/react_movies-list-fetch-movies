1501221910015a4eae2df46d587f9cadbc13d8d2
Anatasia <krajnova.stasy@gmail.com>
2020-09-11 20:46:40 +0300

    trying to fix link

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=57694d91&t=';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=57694d91&t=';
 
 export function getMovie(movie) {
   return fetch(`${BASE_URL}${movie}`)

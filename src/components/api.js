da4019caf7c5ae9db5e5d40162f4524f1b85f674
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-11 17:24:32 +0300

    solution

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 export const getFilm = async(title) => {
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=6d421132&t=${title}`,
+    `https://www.omdbapi.com/?apikey=6d421132&t=${title}`,
   );
 
   return response.json();

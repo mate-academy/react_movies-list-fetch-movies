3cbe7d2d4a2506f66c84302a57c73deb9c60718c
Alisa <cheshyr.alisa@gmail.com>
2020-09-12 13:13:32 +0300

    https

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const OMDB_API = 'http://www.omdbapi.com/?apikey=65c1586c&t=';
+const OMDB_API = 'https://www.omdbapi.com/?apikey=65c1586c&t=';
 
 export const getMovie = title => fetch(`${OMDB_API}${title}`)
   .then(response => response.json());

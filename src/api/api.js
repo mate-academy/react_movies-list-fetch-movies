9e42236ade4b2e089e2a333574993c5a7eded87b
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-09-13 11:41:17 +0300

    link fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com';
+const BASE_URL = 'https://www.omdbapi.com';
 
 export const getFilm = async(title) => {
   const film = await fetch(`${BASE_URL}/?apikey=21a609de&t=${title}`);

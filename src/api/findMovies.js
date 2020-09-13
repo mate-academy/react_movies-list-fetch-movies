0b1dd72fdc25724b33b33ad94ed8e650342de8f3
Anton <tsymbal044@gmail.com>
2020-09-13 23:38:25 +0300

    link fixed

--- src/api/findMovies.js
+++ src/api/findMovies.js
@@ -1,5 +1,5 @@
 const API_FIND_MOVIES
-  = 'http://www.omdbapi.com/?i=tt3896198&apikey=2cb09ab0&t=';
+  = 'https://www.omdbapi.com/?i=tt3896198&apikey=2cb09ab0&t=';
 
 export function findMoviesFromOMDb(title) {
   return fetch(`${API_FIND_MOVIES}${title}`)

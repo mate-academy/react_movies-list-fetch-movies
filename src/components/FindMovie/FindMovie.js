41c09fa9ad9d85e09143d20f675df9bf2f86bf10
Yurii Rysyn <zerocool_cs@ukr.net>
2020-09-15 09:26:38 +0300

    solution

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import PropTypes from 'prop-types';
 
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ movies, setMovies }) => {
+  const [movieName, setMovieName] = useState('');
+  const [previewMovie, setPreviewMovie] = useState(null);
+  const [error, setError] = useState(null);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const onChangeFilter = (event) => {
+    if (error !== null) {
+      setError(null);
+    }
+
+    setMovieName(event.target.value);
+  };
+
+  // eslint-disable-next-line consistent-return
+  const findMovie = async() => {
+    try {
+      const response = await
+      fetch(`http://www.omdbapi.com/?apikey=b65353eb&t=${movieName}`);
+      const data = await response.json();
+
+      if (data.Response === 'False') {
+        return setError(data.Error);
+      }
+
+      setPreviewMovie({
+        title: data.Title,
+        description: data.Plot,
+        imgUrl: data.Poster,
+        imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
+        imdbid: data.imdbID,
+      });
+      // console.log(data);
+    } catch (err) {
+      setError('404 no connection to the server');
+    }
+  };
+
+  const addMovie = () => {
+    let movieIncludes = false;
+
+    if (previewMovie) {
+      Object.values(movies).forEach((movie) => {
+        if (movie.imdbid === previewMovie.imdbid) {
+          movieIncludes = true;
+        }
+      });
+      if (movieIncludes === false) {
+        setMovies([...movies, previewMovie]);
+        setMovieName('');
+      }
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input is-danger"
+              value={movieName}
+              onChange={onChangeFilter}
+            />
+          </div>
+          {error
+            && (
+              <p className="help is-danger">
+                {
+                  error
+                }
+              </p>
+            )
+          }
+
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addMovie}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {previewMovie && <MovieCard {...previewMovie} />}
+
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
+FindMovie.propTypes = {
+  movies: PropTypes.shape({
+    imdbid: PropTypes.number.isRequired,
 
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+  }).isRequired,
+  setMovies: PropTypes.func.isRequired,
+};

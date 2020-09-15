9e957a02f88c0bbc899681e7604146a1eb631f4d
martakupka <marta-k2008@ukr.net>
2020-09-15 17:11:31 +0300

    Fetch fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 export function getMovie(title) {
-  return fetch(`http://www.omdbapi.com/?apikey=29a96ee7&t=${title}`)
+  return fetch(`https://www.omdbapi.com/?apikey=29a96ee7&t=${title}/`)
     .then(response => response.json());
 }

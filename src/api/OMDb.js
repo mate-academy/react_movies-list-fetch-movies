f334bd923c4fbbe559b1ba81ee6f9b17a64e931c
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-09-15 15:07:58 +0300

    deletes comments and added s to http in OMDb

--- src/api/OMDb.js
+++ src/api/OMDb.js
@@ -1,12 +1,6 @@
-const URL_API = 'http://www.omdbapi.com/';
+const URL_API = 'https://www.omdbapi.com/';
 
 export function getInfo(title) {
   return fetch(`${URL_API}?apikey=d905f6c1&t=${title}`)
     .then(response => response.json());
 }
-
-// export const getInfo = async(title) => {
-//   const movie = await fetch(`${URL_API}?apikey=d905f6c1&t=${title}`);
-
-//   return movie.json();
-// };

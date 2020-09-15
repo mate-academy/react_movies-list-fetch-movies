ff7a162770dbe656adf5906ee1de3d4765c4328e
Andrii Suboch <andrii.suboch@gmail.com>
2020-09-15 17:08:08 +0300

    changed fetch...then to async...await

--- src/api/omdbApi.js
+++ src/api/omdbApi.js
@@ -1,6 +1,7 @@
 const URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f7dddb7c&t=';
 
-export const fetchMovie = title => (
-  fetch(`${URL}${title}`)
-    .then(response => response.json())
-);
+export const fetchMovie = async(title) => {
+  const data = await fetch(`${URL}${title}`);
+
+  return data.json();
+};

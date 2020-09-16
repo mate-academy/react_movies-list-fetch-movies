d8182537c8db2ebf9feb00b3eeb8d371966bf764
Artur <artroyal.pro@gmail.com>
2020-09-16 09:46:41 +0300

    Fixed problem with URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'http://www.omdbapi.com/?apikey=af43bc31&t=';
+const API_URL = 'https://www.omdbapi.com/?apikey=af43bc31&t=';
 
 export async function getMovie(title) {
   const response = await fetch(`${API_URL}${title}`);

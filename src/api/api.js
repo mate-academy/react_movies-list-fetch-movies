251e32c2557ac7dcca2f0785d4df569f0fb9c387
Ivan Beletskiy <beletsky02@gmail.com>
2020-11-03 21:06:31 +0200

    try fix gh-pages

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=87aae092&t=';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=87aae092&t=';
 
 function request(url) {
   return fetch(`${BASE_URL}${url}`)

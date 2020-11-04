52725a986a115a7203537c1a10fd7f817d89dd34
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-11-04 16:27:47 +0200

    fix: http api url

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com/';
+const API_URL = 'https://www.omdbapi.com/';
 const API_KEY = '91441aae';
 
 export const request = title => fetch(`${API_URL}?apikey=${API_KEY}&t=${title}`)

fcc1852afe22395b8e2397218c207012646a5b6a
Alina <alina90565@gmail.com>
2020-11-07 21:23:51 +0200

    fixed API_URL

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,5 +1,5 @@
 const API_KEY = 'db946b80';
-const API_URL = 'http://www.omdbapi.com/';
+const API_URL = 'https://www.omdbapi.com/';
 
 export function getMoviesByTitle(title) {
   return fetch(`${API_URL}?apikey=${API_KEY}&t=${title}`)

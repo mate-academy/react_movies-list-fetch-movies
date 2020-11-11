3612f1c13f03441c8a5edcfaa16e2344a5f583f3
VoduDay <andriy.maniak@gmail.com>
2020-11-11 20:23:30 +0200

    used acync await insted then()

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,9 @@
 const BASE_URL = 'https://www.omdbapi.com/?apikey=892b42fa';
 
-const request = url => fetch(`${BASE_URL}${url}`)
-  .then(response => response.json());
+const request = async(url) => {
+  const response = await fetch(`${BASE_URL}${url}`);
+
+  return response.json();
+};
 
 export const getFilm = title => request(`&t=${title}`);

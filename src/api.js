aad25d3ddb0acfef77387a1be6aa2788069197a2
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-11-11 10:58:05 +0200

    fixed https

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=ebaa6af1';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=ebaa6af1';
 
 export const fetchMovie = title => fetch(`${BASE_URL}&t=${title}`)
   .then(res => res.json());

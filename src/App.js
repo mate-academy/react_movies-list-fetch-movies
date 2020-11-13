945fb27d85422f1466f86e991e54e7da7c16add8
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-13 20:57:23 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+/* eslint-disable arrow-parens */
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: data,
+export function App() {
+  const [movies, setMovies] = useState(data);
+
+  const addMovie = (movie) => {
+    setMovies((current) => [...current, movie]);
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} movies={movies} />
+      </div>
+    </div>
+  );
 }
--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,120 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable arrow-parens */
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { request } from '../../api/api';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = (props) => {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState(false);
+  const [movie, setMovie] = useState(null);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
+  const findMovie = (e) => {
+    e.preventDefault();
+    if (query) {
+      request(query).then((res) => {
+        if (res.Error) {
+          setError(true);
+          setMovie(null);
+        } else {
+          setError(false);
+          setMovie(res);
+        }
+      });
+    }
+  };
+
+  const addToList = () => {
+    const isAdded = props.movies.some((item) => item.imdbId === movie.imdbID);
+
+    if (isAdded) {
+      return;
+    }
+
+    if (movie) {
+      props.addMovie({
+        title: movie.Title,
+        description: movie.Plot,
+        imgUrl: movie.Poster,
+        imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
+        imdbId: movie.imdbID,
+      });
+      setQuery('');
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie" onSubmit={findMovie}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', { 'is-danger': error })}
+              value={query}
+              onChange={(e) => {
+                setError(false);
+                setQuery(e.target.value);
+              }}
+            />
+          </div>
+
+          {error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button type="submit" className="button is-light">
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addToList}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && (
+          <MovieCard
+            title={movie.Title}
+            description={movie.Plot}
+            imgUrl={movie.Poster}
+            imdbUrl={`https://www.imdb.com/title/${movie.imdbID}`}
           />
-        </div>
-
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        )}
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
+FindMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.shape({ imdbId: PropTypes.string })),
+};
 
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.defaultProps = {
+  movies: [],
+};

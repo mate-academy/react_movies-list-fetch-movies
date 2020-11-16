3b1fa980c88325b538f62f2afdaf396e720e8773
Yazheviks <vicsergeyeva@gmail.com>
2020-11-16 13:08:22 +0200

    solution

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import PropTypes from 'prop-types';
 
+import { movieShape } from '../../shapes/movieShape';
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ addMovie, movies }) => {
+  const [error, setError] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [movie, setMovie] = useState(null);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
+  const checkMovie = () => {
+    const isMovieAdded = movies.find(film => film.imdbID === movie.imdbID);
+
+    if (isMovieAdded) {
+      return;
+    }
+
+    addMovie([...movies, movie]);
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              value={inputValue}
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input is-danger"
+              onChange={(event) => {
+                setError('');
+                setInputValue(event.target.value);
+              }}
+            />
+          </div>
+
+          {error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => {
+                fetch(`http://www.omdbapi.com/?apikey=bcb65536&t=${inputValue}`)
+                  .then(result => result.json())
+                  .then(setMovie)
+                  .catch(() => {
+                    setError(1);
+                  });
+              }}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => {
+                checkMovie();
+                setInputValue('');
+                setMovie(null);
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      {movie && (
+        <div className="container">
+          <h2 className="title">Preview</h2>
+          <MovieCard
+            title={movie.Title}
+            description={movie.Plot}
+            imgUrl={movie.Poster}
+            imdbUrl={`https://www.imdb.com/title/${movie.imdbID}`}
           />
         </div>
+      )}
+    </>
+  );
+};
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
-      </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+  movies: PropTypes.arrayOf(movieShape).isRequired,
+};
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -3,21 +3,24 @@ import React from 'react';
 import './MoviesList.scss';
 import PropTypes from 'prop-types';
 import { MovieCard } from '../MovieCard';
+import { movieShape } from '../../shapes/movieShape';
 
 export const MoviesList = ({
   movies = [],
 }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.imdbId} {...movie} />
+      <MovieCard
+        key={movie.imdbID}
+        title={movie.Title}
+        description={movie.Plot}
+        imgUrl={movie.Poster}
+        imdbUrl={`https://www.imdb.com/title/${movie.imdbID}`}
+      />
     ))}
   </div>
 );
 
 MoviesList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      imdbId: PropTypes.string.isRequired,
-    }).isRequired,
-  ).isRequired,
+  movies: PropTypes.arrayOf(movieShape).isRequired,
 };

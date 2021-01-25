f79f024eb08bc1ce79ce1ab226d383461144e95c
Lora Levitska <larakad94@gmail.com>
2021-01-25 22:24:57 +0200

    try to resolve blocked my demo

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export function getMovie(title) {
-  return fetch(`http://www.omdbapi.com/?apikey=25bd52c9&t=${title}`)
+  return fetch(`https://www.omdbapi.com/?apikey=25bd52c9&t=${title}`)
     .then(response => response.json())
     .catch(er => 'error');
 }

6fbd6db0815b0a75e22240ea84ddb47a4f5e2ce7
PaulT <ptimofejev@gmail.com>
2021-01-27 01:32:07 +0200

    Fixed mixed content error ???

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-const API_URL = `https://www.omdbapi.com/?apikey=859d9c7f&`;
+const API_URL = `http://www.omdbapi.com/?apikey=859d9c7f&`;
 
 export function getMovieByTitle(title) {
   return fetch(`${API_URL}t=${title}`)

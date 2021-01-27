74547027b58633c9fa7eff3220e45dfd0d691bb5
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-27 23:05:44 +0200

    http to https

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,7 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=8b4080ef&';
-
-const POSTER_URL = 'http://img.omdbapi.com/?apikey=8b4080ef&';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=8b4080ef&';
 
 export const baseRequest = async(title = '') => {
   const response = await fetch(`${BASE_URL}t=${title}`);
-
-  return response.json();
-};
-
-export const posterRequest = async(url) => {
-  const response = await fetch(`${POSTER_URL}${url}`);
 
   return response.json();
 };

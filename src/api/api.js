d1abf12edcfe47848774340af30b6a1617cd407f
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-28 21:31:35 +0200

    Solution

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'http://www.omdbapi.com/?t=';
+// const DATA_URL = '/?apikey=1aae898a&';
+const apikey = '&apikey=1aae898a';
+
+const request = (url, options) => fetch(`${BASE_URL}${url}${apikey}`, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+
+    return response.json().then((apiMovie) => {
+      if (apiMovie.Response === 'False') {
+        throw new Error(apiMovie.Error);
+      }
+
+      return {
+        title: apiMovie.Title,
+        description: apiMovie.Plot,
+        imgUrl: apiMovie.Poster,
+        imdbUrl: apiMovie.imdbID,
+        imdbId: apiMovie.imdbID,
+      };
+    });
+  });
+
+export const getFilm = title => request(title);

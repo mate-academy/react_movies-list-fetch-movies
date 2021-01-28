45c57d356e0587ea8c9332bfe1057a7022e2441c
Dima Bond <neverpooh@gmail.com>
2021-01-28 16:43:00 +0200

    switched to https

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
-const BASE_URL = 'http://www.omdbapi.com/?';
+const BASE_URL = 'https://www.omdbapi.com/?';
 
-export const getMovie = query => fetch(`${BASE_URL}t=${query}&apikey=214049a0`)
-  .then(response => response.json());
+export function getMovie(query) {
+  return fetch(`${BASE_URL}t=${query}&apikey=214049a0`)
+    .then(response => response.json());
+}

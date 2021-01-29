67f12f78cc5973ab33fba1ffd2c7659cc1d74496
Zhenya <e.mezhueva@gmail.com>
2021-01-29 21:11:43 +0200

    solution

---
+++ src/api/helpers.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'http://www.omdbapi.com/?apikey=';
+const API_EY = 'b753e75f';
+
+export const request = title => fetch(`${BASE_URL}${API_EY}&t=${title}`)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status}: ${response.statusText}`);
+  });

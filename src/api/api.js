b59bf9ac1cd42e92bd8d6e9fded3588734414949
Alex <alexmogwaiii@gmail.com>
2021-01-29 21:14:11 +0200

    minor fix in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const API_KEY = '605e0ff4';
-const BASE_URL = 'http://www.omdbapi.com/?apikey=';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=';
 
 export const getMovie = async(title) => {
   const url = `${BASE_URL}${API_KEY}&t=${title}`;

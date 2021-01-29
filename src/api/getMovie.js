1e4a0281ae8677a78c4f91b3c81fb12b1c019787
Layrovell <marina95prodan@gmail.com>
2021-01-30 01:34:09 +0200

    'dev'

--- src/api/getMovie.js
+++ src/api/getMovie.js
@@ -1,7 +1,7 @@
 const key = 'apikey=e533ffbf';
 
 export const getMovie = async(query) => {
-  const response = await fetch(`http://www.omdbapi.com/?${key}&t=${query}`);
+  const response = await fetch(`https://www.omdbapi.com/?${key}&t=${query}`);
   const result = await response.json();
 
   return result;

66b832dbc4f6114619afa82f90ad58bc20b19e53
Marina <59792949+Layrovell@users.noreply.github.com>
2021-01-30 13:35:35 +0200

    Update getMovie.js

--- src/api/getMovie.js
+++ src/api/getMovie.js
@@ -1,11 +1,3 @@
-/* eslint-disable */
-// export const getMovie = async(query) => {
-//   const response = await fetch(`http://www.omdbapi.com/?apikey=85d016e4&t=${query}`);
-//   const result = await response.json();
-//
-//   return result;
-// };
-
 const URL = `https://www.omdbapi.com/?apikey=85d016e4&t=`;
 
 export const getMovie = async(query) => {

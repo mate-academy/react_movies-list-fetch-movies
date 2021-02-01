1cdb20281bc6a77d9e95cf54444de0f489823773
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-02-01 23:50:14 +0200

    updated request

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=9104d067';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=9104d067';
 
 export function getMovie(title) {
   return fetch(`${BASE_URL}&t=${title}`)

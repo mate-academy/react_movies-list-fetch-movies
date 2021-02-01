f80c1a287805f33bdbfa6b5a63e89b87536c4245
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2021-02-01 16:57:55 +0200

    fixed http to https

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=d83d550f';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=d83d550f';
 
 export const getMovies = title => fetch(`${BASE_URL}&t=${title}`)
   .then(res => res.json())

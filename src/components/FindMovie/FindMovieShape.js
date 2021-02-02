16126b010ad2d7bfd61cb323ad41c8fed88c0d59
Vitalii Siedykh <siedykh.v@gmail.com>
2021-02-02 09:45:38 +0200

    solution

---
+++ src/components/FindMovie/FindMovieShape.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const FindMovieShape = {
+  addMovie: PropTypes.func.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string.isRequired,
+    imdbUrl: PropTypes.string.isRequired,
+  }).isRequired).isRequired,
+};

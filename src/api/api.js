bbb8c3b39cb32ded879a62ef3bed6c434b96d853
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-02-20 14:13:54 +0200

    fix https

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,4 @@
-
-const BASE_URL = 'http://www.omdbapi.com/?apikey=8189ef4d&t=';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=8189ef4d&t=';
 
 export const requestMovie = (title) => {
   const movie = fetch(BASE_URL + title);

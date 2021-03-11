3b7bdead70988d04ffde666cb378a95c215c67bd
Oleksandr Orel <oleksandrorel@ukr.net>
2021-03-11 19:13:32 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: data,
+export const App = () => {
+  const [movies, setMovies] = useState(data);
+  const [duplicatedMovie, setDuplicatedMovie] = useState(false);
+
+  const addMovie = (movie) => {
+    const isMoviePresented = movies.some(mov => movie.imdbId === mov.imdbId);
+
+    if (isMoviePresented) {
+      setDuplicatedMovie(true);
+
+      return;
+    }
+
+    setMovies(prevMovies => ([...prevMovies, movie]));
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie
+          addMovie={addMovie}
+          duplicatedMovie={duplicatedMovie}
+          setDuplicatedMovie={setDuplicatedMovie}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
-
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { getMovie } from '../../api/newMovie';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const imdbUrl = 'https://www.imdb.com/title/';
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
+export const FindMovie = ({
+  addMovie,
+  duplicatedMovie,
+  setDuplicatedMovie,
+}) => {
+  const [movie, setMovie] = useState({});
+  const [query, setQuery] = useState('');
+  const [movieFound, setMovieFound] = useState(true);
+
+  const loadMovie = async() => {
+    setMovie({});
+
+    const loadedMovie = await getMovie(query);
+
+    if (loadedMovie.Response === 'False') {
+      setMovieFound(false);
+
+      return;
+    }
+
+    const newMovie = {
+      title: loadedMovie.Title,
+      description: loadedMovie.Plot,
+      imgUrl: loadedMovie.Poster,
+      imdbUrl: `${imdbUrl}${loadedMovie.imdbID}`,
+      imdbId: loadedMovie.imdbID,
+    };
+
+    setMovieFound(true);
+    setMovie(newMovie);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loadMovie();
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={handleSubmit}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classnames('input',
+                { 'is-danger': movieFound === false })}
+              value={query}
+              onChange={(e) => {
+                setQuery(e.target.value);
+                setDuplicatedMovie(false);
+              }}
+            />
+          </div>
+
+          {!movieFound
+            && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+            )}
+
+          {duplicatedMovie
+            && (
+            <p className="help is-danger">
+              Movie already added to the list of movies
+            </p>
+            )}
+
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-light"
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              disabled={!query || !movie.imdbId}
+              onClick={() => {
+                addMovie(movie);
+                setQuery('');
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      {movie.imdbId
+        && (
+        <div className="container">
+          <h2 className="title">Preview</h2>
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={`${imdbUrl}${movie.imdbId}`}
           />
         </div>
+        )
+      }
+    </>
+  );
+};
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
-      </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+  duplicatedMovie: PropTypes.bool.isRequired,
+  setDuplicatedMovie: PropTypes.func.isRequired,
+};

5b9db7c88369a188b3c737bad774a0640b68155f
Taras <seman6745@gmail.com>
2021-03-18 02:37:15 +0200

    second

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-const titleUrl = 'http://www.omdbapi.com/?apikey=de242cb8&t=';
+const titleUrl = 'https://www.omdbapi.com/?apikey=de242cb8&t=';
 
 export const getFilm = breakPoint => (
   fetch(`${titleUrl}${breakPoint}`).then(response => response.json())

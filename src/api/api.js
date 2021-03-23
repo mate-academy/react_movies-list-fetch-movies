1b92bbad463ac4989cd9223d830cda997393a8d8
Andrii Hrushetskyi <andrii.hrushetskii@gmail.com>
2021-03-23 16:03:49 -0400

    http -> https

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const API_KEY = '4c3b4bea';
-const MOVIES_BASIC_LINK = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const MOVIES_BASIC_LINK = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export const getMovieByTitle = title => fetch(
   `${MOVIES_BASIC_LINK}&t=${title}`,

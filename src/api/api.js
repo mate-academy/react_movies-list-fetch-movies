5b1d13a0c1b0f42ea63f06e99650c98637e3bd8b
Misha Lysak <myhaillysak@gmail.com>
2021-03-24 21:19:44 +0200

    problem just was in https, becouse i wrote http :))))))))

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export const BASE_URL
-  = 'http://www.omdbapi.com/?apikey=e2844a57&t=';
+  = 'https://www.omdbapi.com/?apikey=e2844a57&t=';
 
 export const getMovie = title => (
   fetch(`${BASE_URL}${title}`)

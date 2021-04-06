1706ad62e663d6899ec2c501ecbd3e3eaf8ad24e
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-04-06 16:03:02 +0300

    fixed catch

--- src/api/findMovieApi.js
+++ src/api/findMovieApi.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=66d63ac4';
 export function getMovie(title) {
   return fetch(`${BASE_URL}&t=${title}`)
     .then(response => response.json())
-    .catch(e => e);
-  // .catch(error => throw new Error(`${error}`));
-  // Выдаёт ошибку https://prnt.sc/115poqe
+    .catch((error) => {
+      throw new Error(error);
+    });
 }

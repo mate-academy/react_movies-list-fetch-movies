b780eae63843eba718f791fe7785a674168e90a7
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-04-10 00:25:30 +0300

    fix https aveliable

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE__URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d49fb33b&t=';
+const BASE__URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=d49fb33b&t=';
 
 export const request = endPointTitle => (
   fetch(`${BASE__URL}${endPointTitle}`)

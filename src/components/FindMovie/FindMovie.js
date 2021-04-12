73682affc5f4e6a14b38aa260a2e37b8737ba6a2
Oleg Bezriadin <muhachov86@gmail.com>
2021-04-12 08:25:21 +0300

    Solution

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import PropTypes from 'prop-types';
 
+import { getMovie } from '../../api/api';
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ movies, setMovies }) => {
+  const [inputText, setInputText] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [isMovieFound, setMovieState] = useState(false);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const handleChange = (changeEvent) => {
+    setMovieState(false);
+    setInputText(changeEvent.target.value);
+  };
+
+  const searchMovie = (movieEvent) => {
+    movieEvent.preventDefault();
+    getMovie(inputText)
+      .then((response) => {
+        try {
+          setMovie({
+            title: response.Title,
+            description: response.Plot,
+            imdbId: response.imdbID,
+            imgUrl: response.Poster,
+            imdbUrl: `https://www.imdb.com/title/${response.imdbID}`,
+          });
+          setMovieState(false);
+        } catch {
+          setMovieState(true);
+        }
+      });
+  };
+
+  const addMovie = () => {
+    if (!movies.find(film => film.imdbId === movie.imdbId)
+      && !isMovieFound
+      && movie
+    ) {
+      setMovies([...movies, movie]);
+      setMovie(null);
+      setInputText('');
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie" onSubmit={searchMovie}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              value={inputText}
+              onChange={handleChange}
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={isMovieFound ? 'input is-danger' : 'input'}
+            />
+          </div>
+
+          {isMovieFound
+            && (
+              <p className="help is-danger">
+                Can&apos;t find a movie with such a title
+              </p>
+            )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-light"
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addMovie}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {!isMovieFound && movie
+          && (
+            <MovieCard {...movie} />
+          )}
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+  setMovies: PropTypes.func.isRequired,
+};

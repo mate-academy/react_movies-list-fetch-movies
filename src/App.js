3a3bd23656d3ce22a834447f2aa33f547a08b8aa
Ping <4ivrik@ukr.net>
2021-04-12 13:36:45 +0300

    solution using hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: data,
-  };
+export const App = () => {
+  const [movies, setMovie] = useState(data);
 
-    addMovie = (newMovie) => {
-      if (!this.state.movies.find(movie => movie.imdbId === newMovie.imdbId)) {
-        this.setState(prevState => ({
-          movies: [...prevState.movies, newMovie],
-        }));
-      }
-    };
-
-    render() {
-      const { movies } = this.state;
-
-      return (
-        <div className="page">
-          <div className="page-content">
-            <MoviesList movies={movies} />
-          </div>
-          <div className="sidebar">
-            <FindMovie addMovie={this.addMovie} />
-          </div>
-        </div>
-      );
+  const addMovie = useCallback((newMovie) => {
+    if (!movies.find(movie => movie.imdbId === newMovie.imdbId)) {
+      setMovie(prevMovies => [...prevMovies, newMovie]);
     }
-}
+  }, [movies]);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
+      </div>
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,138 +1,121 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { request } from '../api';
 import { MovieCard } from '../MovieCard';
 import './FindMovie.scss';
 
-export class FindMovie extends React.Component {
-  state = {
-    title: '',
-    hasTitleError: false,
-    isMovieError: false,
-    movie: null,
-  }
+export const FindMovie = ({ addMovie }) => {
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setTitleError] = useState(false);
+  const [isMovieError, setMovieError] = useState(false);
+  const [newMovie, setMovie] = useState(null);
 
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
+  const handleInputChange = (event) => {
+    const { value } = event.target;
 
-    this.setState({
-      [name]: value,
-      hasTitleError: false,
-    });
-  }
+    setTitle(value);
+    setTitleError(false);
+  };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    const { title } = this.state;
-
     request(title)
-      .then((movie) => {
-        this.setState(movie.Response !== 'False' ? {
-          movie: {
-            ...movie,
-            title: movie.Title,
-            description: movie.Plot,
-            imdbId: movie.imdbID,
-            imgUrl: movie.Poster,
-            imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
-          },
-          isMovieError: false,
-        } : {
-          movie: null,
-          isMovieError: true,
-        });
+      .then((film) => {
+        if (film.Response !== 'False') {
+          setMovie({
+            ...film,
+            title: film.Title,
+            description: film.Plot,
+            imdbId: film.imdbID,
+            imgUrl: film.Poster,
+            imdbUrl: `https://www.imdb.com/title/${film.imdbID}`,
+          });
+          setMovieError(false);
+        } else if (film.Response === 'False') {
+          setMovie(null);
+          setMovieError(true);
+        }
       });
 
     if (!title) {
-      this.setState({
-        hasTitleError: true,
-      });
+      setTitleError(true);
     }
-  }
+  };
 
-  addNewMovie = () => {
-    const { isMovieError } = this.state;
-
-    if (!isMovieError) {
-      this.props.addMovie(this.state.movie);
-      this.setState({
-        movie: null,
-      });
+  const addNewMovie = () => {
+    if (!isMovieError && newMovie) {
+      addMovie(newMovie);
+      setMovie(null);
     }
 
-    this.setState({
-      title: '',
-    });
-  }
+    setMovieError(false);
+    setTitle('');
+  };
 
-  render() {
-    const { movie, title, isMovieError } = this.state;
+  return (
+    <>
+      <form className="find-movie" onSubmit={handleSubmit}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
 
-    return (
-      <>
-        <form className="find-movie" onSubmit={this.handleSubmit}>
-          <div className="field">
-            <label className="label" htmlFor="movie-title">
-              Movie title
-            </label>
-
-            <div className="control">
-              <input
-                type="text"
-                name="title"
-                id="movie-title"
-                placeholder="Enter a title to search"
-                className="input is-danger"
-                value={title}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            {this.state.hasTitleError && (
+          <div className="control">
+            <input
+              type="text"
+              name="title"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input is-danger"
+              value={title}
+              onChange={handleInputChange}
+            />
+          </div>
+          {hasTitleError && (
             <p className="help is-danger">
               Can&apos;t find a movie with such a title
             </p>
-            )}
+          )}
 
-            {this.state.isMovieError && (
+          {isMovieError && (
             <p className="help is-danger">
               Movie doesn&apos;t exist!
             </p>
-            )}
-          </div>
-
-          <div className="field is-grouped">
-            <div className="control">
-              <button
-                type="submit"
-                className="button is-light"
-              >
-                Find a movie
-              </button>
-            </div>
-
-            <div className="control">
-              <button
-                type="button"
-                className="button is-primary"
-                onClick={() => this.addNewMovie()}
-              >
-                Add to the list
-              </button>
-            </div>
-          </div>
-        </form>
-
-        <div className="container">
-          <h2 className="title">Preview</h2>
-          {movie && !isMovieError
-          && <MovieCard movie={movie} />
-          }
+          )}
         </div>
-      </>
-    );
-  }
-}
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-light"
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addNewMovie}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {newMovie && !isMovieError
+          && <MovieCard movie={newMovie} />
+          }
+      </div>
+    </>
+  );
+};
 
 FindMovie.propTypes = {
   addMovie: PropTypes.func.isRequired,

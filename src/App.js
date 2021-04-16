2d7e7f6fd1813b039ab8b4145a95173d4d71c745
Onishchuk <onishchuk.danil@gmail.com>
2021-04-16 20:03:24 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: data,
+export const App = () => {
+  const [movies, setMovies] = useState(data);
+  const [hiddenErMovieRepeat, setHiddenErMovieRepeat] = useState(true);
+
+  const addMovie = (movie) => {
+    if (movie.title === undefined) {
+      setHiddenErMovieRepeat(false);
+
+      return;
+    }
+
+    setMovies(current => [
+      ...current,
+      movie,
+    ]);
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie
+          movies={movies}
+          addMovie={addMovie}
+          setHiddenErMovieRepeat={setHiddenErMovieRepeat}
+          hiddenErMovieRepeat={hiddenErMovieRepeat}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './FindMovie.scss';
 
-import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { MovieCard, MovieCardType } from '../MovieCard';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ addMovie, movies }) => {
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+  });
+  const [hiddenError, setHiddenError] = useState('hidden');
+  const [hiddenPreview, setPreview] = useState(true);
+  const [hiddenErMovieRepeat, setHiddenErMovieRepeat] = useState('hidden');
+  const [submitDisabled, setSubmitDisabled] = useState(true);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  function getMovie() {
+    return fetch(`https://www.omdbapi.com/?apikey=6e3cbe75&t=${title}`)
+      .then(response => response.json());
+  }
+
+  const findMovie = async() => {
+    const newMovie = await getMovie(title);
+    const allImdbID = movies.map(movieData => movieData.imdbId);
+
+    if (newMovie.Title) {
+      setSubmitDisabled(false);
+      setMovie({
+        title: newMovie.Title,
+        description: newMovie.Plot,
+        imgUrl: newMovie.Poster,
+        imdbUrl: `https://www.imdb.com/title/${newMovie.imdbID}`,
+        imdbId: newMovie.imdbID,
+      });
+    }
+
+    if (allImdbID.includes(newMovie.imdbID)) {
+      setHiddenErMovieRepeat('help is-danger');
+      setSubmitDisabled(true);
+    } else {
+      setHiddenErMovieRepeat('hidden');
+    }
+
+    if (newMovie.Title === undefined) {
+      setHiddenError('help is-danger');
+      setPreview(true);
+    } else {
+      setHiddenError('hidden');
+      setPreview(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addMovie(movie);
+    setTitle('');
+    setPreview(true);
+    setSubmitDisabled(true);
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={e => handleSubmit(e)}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              value={title}
+              onChange={(e) => {
+                setHiddenError('hidden');
+                setTitle(e.target.value);
+              }}
+              placeholder="Enter a title to search"
+              className="input"
+            />
+          </div>
+
+          <p className={hiddenError}>
+            Can&apos;t find a movie with such a title
+          </p>
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => findMovie()}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary"
+              disabled={submitDisabled}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <p className={hiddenErMovieRepeat}>
+          We have such movie!
         </p>
-      </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
+        <div hidden={hiddenPreview}>
+          <MovieCard {...movie} />
         </div>
       </div>
-    </form>
+    </>
+  );
+};
 
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    ...MovieCardType,
+    imdbId: PropTypes.string.isRequired,
+  })).isRequired,
+};

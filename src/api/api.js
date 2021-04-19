5b1f6884d37a6e30bc6ae4f6911d53a46028f273
olena <melolena33@gmail.com>
2021-04-19 14:43:05 +0300

    Changed http to https

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 
 export const getMovie = (title) => {
-  const URL = `http://www.omdbapi.com/?apikey=d38276c0&t=${title}`;
+  const URL = `https://www.omdbapi.com/?apikey=d38276c0&t=${title}`;
 
   return fetch(URL)
     .then(response => response.json());

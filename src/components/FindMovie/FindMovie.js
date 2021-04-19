87090323012335413c27318d65f4d87f62a34373
Artem Maksimikhin <uanov86@gmail.com>
2021-04-19 21:04:46 +0300

    'Solution'

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
-
+import PropTypes from 'prop-types';
+import { getMovie } from '../../api/api';
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ addMovies }) => {
+  const [movie, setMovie] = useState('');
+  const [movieTitle, setMovieTitle] = useState('');
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const findMovie = () => {
+    getMovie(movieTitle)
+      .then(result => setMovie(result));
+  };
+
+  const clearMovie = () => {
+    setMovie('');
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input is-danger"
+              value={movieTitle}
+              onChange={(e) => {
+                setMovieTitle(e.target.value);
+              }}
+            />
+          </div>
+          {(movie && !movie.Title) && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => {
+                if (movie.Title) {
+                  addMovies(movie);
+                  clearMovie();
+                }
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        {movie.Title
+        && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard {...movie} />
+          </>
+        )
+        }
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovies: PropTypes.func.isRequired,
+};

9109a069e9a9a93891df4724faf8b4288b7d483f
AndriiKreshchenko <mail2andyk@gmail.com>
2021-05-08 21:05:58 +0300

    Small fix (fetch and CORS)

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=a9946729';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=a9946729';
 
 export const getMovie = async(url) => {
   const response = await fetch(`${BASE_URL}&t=${url}`);

c3bed4f5e4fca51a4eca6af60738b561b73e6a82
Bohdan Polienko <mignatov756@gmail.com>
2021-05-14 15:44:35 +0300

    Solution

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
-
+import classnames from 'classnames';
+import propTypes from 'prop-types';
+import { request } from '../../api/getApi';
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie = ({ addMovie }) => {
+  const [title, getTitle] = useState('');
+  const [api, getApi] = useState('');
+  const [showErrorMessage, getErrorMessage] = useState(true);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  async function loadApi() {
+    const loadingApi = await request(title);
+
+    getApi(loadingApi);
+    if (loadingApi.Response === 'False') {
+      getErrorMessage(false);
+    }
+  }
+
+  const movie = {
+    title: api.Title,
+    description: api.Plot,
+    imgUrl: api.Poster,
+    imdbId: api.imdbID,
+    imdbUrl: `https://www.imdb.com/title/${api.imdbID}`,
+  };
+
+  const addMovieToMoviesList = () => {
+    if (movie.title !== undefined) {
+      addMovie(movie);
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              onChange={(event) => {
+                getTitle(event.target.value);
+                getErrorMessage(true);
+              }}
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classnames(showErrorMessage
+                ? 'input'
+                : 'input is-danger')}
+            />
+          </div>
+          {showErrorMessage
+            ? ''
+            : (
+              <p className="help is-danger">
+                Can&apos;t find a movie with such a title
+              </p>
+            )
+          }
         </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={title
+                ? loadApi
+                : null}
+            >
+              Find a movie
+            </button>
+          </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addMovieToMoviesList}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="container">
+        {movie.title
+          && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard {...movie} />
+          </>
+          )}
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovie: propTypes.func.isRequired,
+};

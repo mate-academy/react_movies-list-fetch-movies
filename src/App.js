58289199bfda86e402795ba761c0b62e2270c6d3
Anton Kostylov <antoniok003@gmail.com>
2021-06-02 10:28:33 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ export class App extends Component {
     movies: data,
   };
 
+  addMovie = (movie) => {
+    const { movies } = this.state;
+    const inTheList = movies.some(item => item.imdbId === movie.imdbId);
+
+    if (!inTheList) {
+      this.setState(state => ({
+        movies: [...state.movies, movie],
+      }));
+    }
+  }
+
   render() {
     const { movies } = this.state;
 
@@ -18,7 +29,7 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <FindMovie />
+          <FindMovie addMovie={this.addMovie} />
         </div>
       </div>
     );

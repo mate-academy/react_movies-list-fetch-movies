b7152fe8fb21eaf38a970207f1499e6c3cbcf01c
victor <victorarytionowmail@gmail.com>
2021-06-03 20:37:02 +0300

    change http to https

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@
 const apikey = '4d5d73d6';
 
 export const request = title => (
-  fetch(`http://www.omdbapi.com/?t=${title}&apikey=${apikey}`)
+  fetch(`https://www.omdbapi.com/?t=${title}&apikey=${apikey}`)
     .then(response => response.json())
     .catch(error => error)
 );

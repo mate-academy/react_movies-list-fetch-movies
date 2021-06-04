cfef53644d2ab3ae8e451541476d635a820b011e
Maria Yasevina <maria.yasevina@gmail.com>
2021-06-04 17:04:29 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import { FindMovie } from './components/FindMovie';
+
 import data from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: data,
+import { MoviesList } from './components/MoviesList';
+import { FindMovie } from './components/FindMovie';
+
+export const App = () => {
+  const [movies, setMovies] = useState(data);
+
+  const addMovie = (newMovie) => {
+    const { imdbUrl } = newMovie;
+    const isUnique = movies.some(movie => movie.imdbUrl === imdbUrl);
+
+    if (!isUnique === false) {
+      return;
+    }
+
+    setMovies(currentMovies => [...currentMovies, newMovie]);
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};

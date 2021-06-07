34813b8ebb93e33abeec80e24d28531e3b145caa
Igor Lisogor <illisogori@gmail.com>
2021-06-07 14:37:19 +0300

    fetch movies implemented

--- src/components/FindMovie/FindMovie.js
+++ src/components/FindMovie/FindMovie.js
@@ -1,55 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import { getFilm } from '../../api/api';
+import { MovieCard } from '../MovieCard';
+
 import './FindMovie.scss';
 
-import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+export const FindMovie = ({ addMovie }) => {
+  const [searchQuery, setsearchQuery] = useState('');
+  const [newMovie, setNewMovie] = useState(null);
+  const [searchQueryError, setSearchQueryError] = useState(false);
 
-export const FindMovie = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+  const searchMovie = () => {
+    if (!searchQuery) {
+      return;
+    }
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+    getFilm(searchQuery).then((result) => {
+      if (result.Error) {
+        setSearchQueryError(true);
+
+        return;
+      }
+
+      setNewMovie({
+        title: result.Title,
+        description: result.Plot,
+        imgUrl: result.Poster,
+        imdbId: result.imdbID,
+      });
+      setSearchQueryError(false);
+    });
+  };
+
+  const onAddMovie = () => {
+    addMovie((movies) => {
+      if (newMovie === null) {
+        return movies;
+      }
+
+      return [...movies, newMovie];
+    });
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', { 'is-danger': searchQueryError })}
+              onChange={event => setsearchQuery(event.target.value)}
+              value={searchQuery}
+            />
+          </div>
+
+          {searchQueryError && (
+          <p className="help is-danger">
+            Can&apos;t find a movie with such a title
+          </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={searchMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={onAddMovie}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {newMovie && <MovieCard {...newMovie} />}
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+FindMovie.propTypes = {
+  addMovie: PropTypes.func.isRequired,
+};

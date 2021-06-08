8623e0002f8b5f2a415fc0192c6438edca178b53
Ivan Shpynda <ivanshpynda@gmail.com>
2021-06-08 14:49:34 +0300

    04

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const URL = 'https://www.omdbapi.com/?apikey=c501a498&t=';
 
-const getMovieFromServer = async Title => fetch(`${URL}${Title}`)
+const getMovieFromServer = Title => fetch(`${URL}${Title}`)
   .then(response => response.json())
   .then(result => result)
   .catch(error => error);

46422fadb3d367cfb4a2bc0756518171eec6aa59
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-06-10 18:37:37 +0300

    protocol for url is changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6811da81&';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=6811da81&';
 
 const getData = param => (fetch(`${BASE_URL}${param}`).then((response) => {
   if (!response.ok) {

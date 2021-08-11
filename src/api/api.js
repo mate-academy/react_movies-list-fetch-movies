aa44110470973234e7ad42be4026524be51e0370
NYaremchuk <nazarmails@gmail.com>
2021-08-11 10:36:42 +0300

    http replaced with https

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=efb1a018`;
+const API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=efb1a018`;
 
 export async function getMovies(title) {
   return fetch(`${API_URL}&t=${title}`)

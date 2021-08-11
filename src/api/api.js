f539be33dd79be80ac7bfc28a7c488b272278b2e
Михаил <mihay0113@gmail.com>
2021-08-11 09:46:05 +0300

    delete comments

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,3 @@
-
-// export const request = async title => fetch(`${BASE_URL}&t=${title}`)
-//   .then((response) => {
-//     // if (!response.ok) {
-//     //   throw new Error(`${response.status} - ${response.statusText}`);
-//     // }
-
-//     return response.json();
-//   });
-
 const BASE_URL = 'https://www.omdbapi.com/?apikey=c77ea3c8';
 
 export const request = async title => fetch(`${BASE_URL}&t=${title}`)

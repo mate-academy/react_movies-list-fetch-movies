0146a0e028d9b39a13c9cc4cf3e17e58b28fc671
Михаил <mihay0113@gmail.com>
2021-08-11 09:43:54 +0300

    movie finder

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
+const moviesKey = 'movies';
+
 export class App extends Component {
+  moviesFromStorage = JSON.parse(window.localStorage.getItem(moviesKey));
+
   state = {
-    movies: data,
+    movies: this.moviesFromStorage || data,
   };
+
+  addMovie = (movie) => {
+    if (movie !== null) {
+      if (!this.state.movies.find(film => film.imdbId === movie.imdbId)) {
+        this.setState(state => ({
+          movies: [
+            ...state.movies,
+            movie,
+          ],
+        }),
+        () => {
+          const stringifiedMovies = JSON.stringify(this.state.movies);
+
+          window.localStorage.setItem(moviesKey, stringifiedMovies);
+        });
+      }
+    }
+  }
 
   render() {
     const { movies } = this.state;
+    const { addMovie } = this;
 
     return (
       <div className="page">
@@ -18,7 +41,7 @@ export class App extends Component {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <FindMovie />
+          <FindMovie addMovie={addMovie} />
         </div>
       </div>
     );

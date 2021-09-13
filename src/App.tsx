91335fb098a16f1ca303d97ba554489594adee07
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-09-13 19:55:46 +0300

    Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,26 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState(data);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: data,
-  };
-
-  addNewMovie = (movie: Movie | null) => {
-    const { movies } = this.state;
-
+  const addNewMovie = (movie: Movie | null) => {
     if (movie && !movies.find(film => film.imdbId === movie.imdbId)) {
-      this.setState(prevState => ({
-        movies: [
-          ...prevState.movies,
-          movie,
-        ],
-      }));
+      setMovies([...movies, movie]);
     }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie onAdd={this.addNewMovie} />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie onAdd={addNewMovie} />
+      </div>
+    </div>
+  );
+};

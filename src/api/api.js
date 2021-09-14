b5891962ca7260f0a675c0f753a1770f936f570f
lera-yaskevych <redrum.back@gmail.com>
2021-09-14 07:17:04 +0300

    fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=91e17a04&t=';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=91e17a04&t=';
 
 export const getMovie = (movieTitle) => {
   return fetch(`${BASE_URL}${movieTitle}`)

8f76c645f3305932e83537ffcb438cfede3f65e1
Kostrica <romiorw@ukr.net>
2021-09-14 20:32:17 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [
+    movies,
+    setMovies,
+  ] = useState(data);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: data,
+  const addFoundMovie = (movieThatFound: Movie) => {
+    if (movies.every(({ imdbId }) => imdbId !== movieThatFound.imdbId)) {
+      setMovies([...movies, movieThatFound]);
+    }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie handleAddFoundMovie={addFoundMovie} />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,55 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './FindMovie.scss';
 
 import { MovieCard } from '../MovieCard';
-import movies from '../../api/movies.json';
+import { getMovies } from '../../api/Api';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+interface Props {
+  handleAddFoundMovie: any;
+}
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+const movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+export const FindMovie: React.FC<Props> = ({ handleAddFoundMovie }) => {
+  const [query, setQuery] = useState('');
+  const [movieThatFound, setMovieThatFound] = useState(movie);
+  const [showMessage, setShowMessage] = useState(false);
+
+  const getTitle = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(target.value);
+  };
+
+  const sendAMovieSearchRequest = () => {
+    getMovies(query)
+      .then(({
+        Title,
+        Plot,
+        Poster,
+        imdbID,
+      }) => {
+        const newMovie = {
+          title: Title,
+          description: Plot,
+          imgUrl: Poster,
+          imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+          imdbId: imdbID,
+        };
+
+        setMovieThatFound(newMovie);
+        setShowMessage(!newMovie.title);
+      });
+  };
+
+  useEffect(() => {
+    setShowMessage(false);
+  }, [query]);
+
+  const addMovieThatFound = () => {
+    handleAddFoundMovie(movieThatFound);
+    setQuery('');
+    setMovieThatFound(movie);
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input is-danger"
+              value={query}
+              onChange={getTitle}
+            />
+          </div>
+
+          {showMessage && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={sendAMovieSearchRequest}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={addMovieThatFound}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movieThatFound.imdbId
+          ? <MovieCard {...movieThatFound} />
+          : <p>No information to display</p>}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      <MovieCard {...movies[0]} />
-    </div>
-  </>
-);
+    </>
+  );
+};

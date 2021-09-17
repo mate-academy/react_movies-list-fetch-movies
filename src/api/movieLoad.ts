b9242ac9e5b55eac13b26915c6dec3948d5d0d8f
Moroz Dmytro <dmytro101100@gmail.com>
2021-09-17 22:12:47 +0300

    Small fix in protocol

--- src/api/movieLoad.ts
+++ src/api/movieLoad.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=de16e785';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=de16e785';
 
 export const loadMovie = (title: string) => {
   return fetch(`${BASE_URL}&t=${title}`)

fc57f32efe7a82f1b259b782dc59d4406800ff11
Andrii Serhovetc <serhovetc88@gmail.com>
2021-09-17 15:34:51 +0300

    solution react-movies-list-fetch-movie first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovie] = useState(data);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: data,
+  const addMovie = (newMovie: Movie) => {
+    setMovie([...movies, newMovie]);
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};

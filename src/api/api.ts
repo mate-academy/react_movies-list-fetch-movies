46d61a10bc87f820d1235e882219f4c9785a5165
Denys Chemeriakov <chemeriakov@gmail.com>
2021-09-19 13:23:25 +0300

    api-link was changed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d2fc13e4';
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=b6b0e744g';
 
 export const loadMovie = async (title: string) => {
   const response = await fetch(`${API_URL}&t=${title}`);

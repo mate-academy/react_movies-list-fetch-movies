b26aa4d2d49bc7f2752bf1530519c063e993c793
Igor <2492345@gmail.com>
2021-09-20 16:54:20 +0300

    api fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c01d3eb8&';
+const API_URL = 'http://www.omdbapi.com/?apikey=c01d3eb8&';
 
 export const getMovie = async (title: string) => {
   const response = await fetch(`${API_URL}&t=${title}`);

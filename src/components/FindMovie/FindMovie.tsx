31329a18c3c795dfd196da11b458fb1a5313f04b
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-09-20 19:45:44 +0300

    task is done

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { loadMovie } from '../../api/api';
+import { MovieCard } from '../MovieCard';
 
-// import { MovieCard } from '../MovieCard';
+type Props = {
+  addMovie: (movie: Movie) => void,
+};
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [title, setTitle] = useState('');
+  const [foundedMovie, setFoundedMovie] = useState<Movie | null>(null);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const findMovie = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const movie = await loadMovie(title);
+
+    setFoundedMovie(movie);
+  };
+
+  const handleMovieAdd = () => {
+    if (foundedMovie) {
+      const newMovie = {
+        Poster: foundedMovie.Poster,
+        Title: foundedMovie.Title,
+        Plot: foundedMovie.Plot,
+        imdbID: foundedMovie.imdbID,
+      };
+
+      addMovie(newMovie);
+    }
+
+    setFoundedMovie(null);
+  };
+
+  return (
+    <>
+      <form className="find-movie" onSubmit={findMovie}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              value={title}
+              onChange={handleInput}
+              className="input is-danger"
+            />
+          </div>
+          {!foundedMovie && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              onClick={handleMovieAdd}
+              className="button is-primary"
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {foundedMovie
+          && <MovieCard movie={foundedMovie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

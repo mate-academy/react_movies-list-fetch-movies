869608cd9182df3f279349ebd6084b80a71dadc7
Andrii Serhovetc <serhovetc88@gmail.com>
2021-09-20 11:19:58 +0300

    some fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = ' http://www.omdbapi.com/?apikey=ddf4c823&t=';
+const BASE_URL = ' https://www.omdbapi.com/?apikey=ddf4c823&t=';
 
 export const getFilm = (title: string) => {
   return fetch(`${BASE_URL}${title}`)

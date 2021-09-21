6104e8cafc3432f54dd0c762e51937490d9c3cc2
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-09-21 14:39:19 +0300

    App component was rewrited on functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,32 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
-  };
-
-  addMovie = (movie: Movie): boolean => {
-    const includes = this.state.movies
+  const addMovie = (movie: Movie): boolean => {
+    const includes = movies
       .findIndex(m => m.imdbID === movie.imdbID);
 
     if (includes >= 0) {
       return false;
     }
 
-    this.setState(state => ({
-      movies: [...state.movies, movie],
-    }));
+    setMovies([...movies, movie]);
 
     return true;
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie addMovie={this.addMovie} />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};

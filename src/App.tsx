c412cff44f21c5e0e9fafd6df1c8b2a3afd03d66
NastyaVovk <winc4ester@gmail.com>
2021-09-22 16:31:02 +0300

    changed app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,28 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import data from './api/movies.json';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState(data);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: data,
-  };
-
-  addMovie = (newMovie: Movie) => {
-    const { movies } = this.state;
-    let isAlreadyAdded = false;
-
-    movies.forEach(movie => {
-      if (movie.imdbId === newMovie.imdbId) {
-        isAlreadyAdded = true;
-      }
-    });
+  const addMovie = (newMovie: Movie) => {
+    const isAlreadyAdded = movies.find(movie => movie.imdbId === newMovie.imdbId);
 
     if (!isAlreadyAdded) {
-      this.setState(currentState => ({
-        movies: [...currentState.movies, newMovie],
-      }));
+      setMovies(current => [...current, newMovie]);
     }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie onAdd={this.addMovie} />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie onAdd={addMovie} />
+      </div>
+    </div>
+  );
+};

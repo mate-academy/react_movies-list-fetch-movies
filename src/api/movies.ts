8077848fef117b7c10d2a989b460107b1340a4e2
Andrii Malyshev <andrey.malyshev.97@gmail.com>
2021-10-24 13:41:31 +0300

    react_movies-list-fetch-movies fixed https in movies.ts

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=14dd0af7';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=14dd0af7';
 
 export const movies = (title: string) => {
   return fetch(`${BASE_URL}&t=${title}`)

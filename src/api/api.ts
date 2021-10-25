b8d080e7a56652dc21080a447e77ec715f10f33e
Igor Igor <you@example.com>
2021-10-25 13:49:06 +0300

    s

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
 export const getMovie = (title: string) => {
   return fetch(`http://www.omdbapi.com/?apikey=83c3ed73&t=${title}`)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(`${res.status} - ${res.statusText}`);
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
       }
 
-      return res.json();
+      return response.json();
     });
 };

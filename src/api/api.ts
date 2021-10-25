e30692869f30eda9eb76c1fe960c7b98620b4a74
Igor Igor <you@example.com>
2021-10-25 13:54:51 +0300

    http to https

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 export const getMovie = (title: string) => {
-  return fetch(`http://www.omdbapi.com/?apikey=83c3ed73&t=${title}`)
+  return fetch(`https://www.omdbapi.com/?apikey=83c3ed73&t=${title}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status} - ${response.statusText}`);

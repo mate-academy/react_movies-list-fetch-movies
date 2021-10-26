03839d8739c46404911f254dcf94ac46c3e14449
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-26 11:35:15 +0300

    Solution

---
+++ src/api.ts
@@ -0,0 +1,22 @@
+const key = 'f019d615';
+const API_URL = 'http://www.omdbapi.com/';
+const request = (url: string) => {
+  return fetch(`${API_URL}${url}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
+
+export const getMovie = async (title: string) => {
+  const movie = await request(`/?apikey=${key}&t=${title}`);
+
+  if (movie.Response === 'False') {
+    throw new Error('Movie not found');
+  }
+
+  return movie;
+};
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,82 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+interface Props {
+  addMovie: (movie: Movie) => void,
+}
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(false);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+            <div className="control">
+              <input
+                type="text"
+                id="movie-title"
+                value={query}
+                placeholder="Enter a title to search"
+                className={classNames('input', { 'is-danger': error })}
+                onChange={event => {
+                  setQuery(event.target.value);
+                  setError(false);
+                }}
+              />
+            </div>
+          </label>
+
+          <p className="help is-danger" style={{ visibility: error ? 'visible' : 'hidden' }}>
+            Can&apos;t find a movie with such a title
+          </p>
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={async () => {
+                await getMovie(query)
+                  .then(film => setMovie(film))
+                  .catch(() => setError(true));
+              }}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              disabled={!movie}
+              onClick={() => {
+                if (movie !== null) {
+                  addMovie(movie);
+                  setQuery('');
+                  setMovie(null);
+                }
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && <MovieCard movie={movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

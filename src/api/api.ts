d92402517376415ef53711a5d9977af6af494928
caelum29 <caelum29@gmail.com>
2021-10-26 15:04:04 +0300

    https added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,10 @@
 export const getMovie = (query: string): Promise<Movie> => {
-  return fetch(`http://www.omdbapi.com/?apikey=ef5f9f&t=${query}`).then(response => {
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
+  return fetch(`https://www.omdbapi.com/?apikey=ef5f9f&t=${query}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
 
-    return response.json();
-  });
+      return response.json();
+    });
 };

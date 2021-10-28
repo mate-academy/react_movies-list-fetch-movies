fc6e591ff783d866e8554eabf6827a4236602d58
DY-JS <delo1604@ukr.net>
2021-10-28 13:49:43 +0300

    Solution

---
+++ src/components/api.ts
@@ -0,0 +1,14 @@
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&api';
+const key = 'key=ef64fce9';
+const param = '&t=';
+
+export const request = (title: string) => {
+  return fetch(`${API_URL}${key}${param}${title}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`${res.status}-${res.statusText}`);
+      }
+
+      return res.json();
+    });
+};

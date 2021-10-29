16e8f1a28a1f50ceb55f49c75ebd36be369ac8fb
Ruslan-Mykhalchak <ruslan1993x@gmail.com>
2021-10-29 09:55:42 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
 
-// import { MovieCard } from '../MovieCard';
+type Props = {
+  onAddMovie: (movieToAdd: Movie) => void,
+};
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ onAddMovie }) => {
+  const [Query, setQuery] = useState('');
+  const [Movie, setMovie] = useState<Movie | null>(null);
+  const [Error, setError] = useState(false);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (Movie) {
+      onAddMovie(Movie);
+    }
+
+    setMovie(null);
+    setQuery('');
+  };
+
+  const findMovie = async () => {
+    const movie = await getMovie(Query);
+
+    if (movie.Response === 'True') {
+      setMovie({
+        Title: movie.Title,
+        Poster: movie.Poster,
+        Plot: movie.Plot,
+        imdbID: movie.imdbID,
+        Response: movie.Response,
+      });
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie" onSubmit={handleFormSubmit}>
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+
+            <div className="control">
+              <input
+                type="text"
+                id="movie-title"
+                placeholder="Enter a title to search"
+                className={classNames('input', { 'is-danger': Error })}
+                onChange={event => {
+                  setQuery(event.target.value);
+                  setError(false);
+                }}
+              />
+            </div>
+          </label>
+
+          {Error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => {
+                if (Movie) {
+                  onAddMovie(Movie);
+                }
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {Movie && <MovieCard movie={Movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

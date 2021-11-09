9b40a0c52b14de40fb4e834ad81941b3de2d6433
Anton Forzun <anton_forzun@ukr.net>
2021-11-09 14:40:52 +0200

    movies_list_fetch

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { getMovie } from '../../api/api';
+import { MovieCard } from '../MovieCard';
+
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+type Props = {
+  addToTheList: (movie: Movie) => void;
+};
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addToTheList }) => {
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(false);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
+  const sendRequest = async () => {
+    const movieFromServer = await getMovie(title);
+
+    if (movieFromServer.Title) {
+      return setMovie(movieFromServer);
+    }
+
+    return setError(current => !current);
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label
+            className="label"
+            htmlFor="movie-title"
+          >
+            Movie title
+            <div className="control">
+              <input
+                type="text"
+                id="movie-title"
+                placeholder="Enter a title to search"
+                className="input is-danger"
+                value={title}
+                onChange={(event) => {
+                  setError(false);
+                  setTitle(event.target.value);
+                }}
+              />
+            </div>
+          </label>
+
+          {error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={sendRequest}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => {
+                if (movie) {
+                  addToTheList(movie);
+                  setTitle('');
+                }
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && (
+          <MovieCard
+            movie={movie}
           />
-        </div>
-
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        )}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

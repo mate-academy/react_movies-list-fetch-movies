1bba215fd4ccb0c81e509f7cb69389990ed5e264
Roman Mefokov <meforoma@gmail.com>
2021-11-12 13:13:56 +0200

    Solution

---
+++ src/api/api.ts
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://www.omdbapi.com/?apikey=';
+const apiKey = 'e7eaf18f';
+
+export const request = (searchQuery: string) => {
+  return fetch(`${BASE_URL}${apiKey}&t=${searchQuery}`)
+    .then(result => {
+      if (!result.ok) {
+        throw new Error(`${result.status}-${result.statusText}`);
+      }
+
+      return result.json();
+    });
+};

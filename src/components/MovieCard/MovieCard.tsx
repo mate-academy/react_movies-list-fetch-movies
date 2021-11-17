3ffa47094fe8a042eb344556ed981a3c455926e9
Taras Kozak <taraskozakmail@gmail.com>
2021-11-17 17:13:52 +0200

    solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -5,39 +5,41 @@ type Props = {
   movie: Movie;
 };
 
-export const MovieCard: React.FC<Props> = (props) => {
-  const { movie } = props;
-
+export const MovieCard: React.FC<Props> = ({ movie }) => {
   return (
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={movie.Poster}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
+    <div>
+      {movie && (
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
               <img
-                src="images/imdb-logo.jpeg"
-                alt="imdb"
+                src={movie.Poster}
+                alt="Film logo"
               />
             </figure>
           </div>
-          <div className="media-content">
-            <p className="title is-8">{movie.Title}</p>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="images/imdb-logo.jpeg"
+                    alt="imdb"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-8">{movie.Title}</p>
+              </div>
+            </div>
+
+            <div className="content">
+              {movie.Plot}
+              <br />
+            </div>
           </div>
         </div>
-
-        <div className="content">
-          {movie.Plot}
-          <br />
-        </div>
-      </div>
+      )}
     </div>
   );
 };

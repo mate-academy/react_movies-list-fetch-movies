078b7a1ef34d687099ded0a5434340d7a84d60f0
Artem Fedoseev <afedoseev359@gmail.com>
2021-11-18 20:41:42 +0200

    Change URL to https

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 export const findMovie = (title: string) => {
-  const URL = `http://www.omdbapi.com/?apikey=7477c903&t=${title}`;
+  const URL = `https://www.omdbapi.com/?apikey=7477c903&t=${title}`;
 
   return fetch(URL)
     .then(res => res.json());

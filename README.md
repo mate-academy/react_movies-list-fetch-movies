74e96e4954a600332838c188c7c705639f799d35
Misha Rosa <misharosa.lviv@gmail.com>
2021-11-23 23:36:02 +0200

    Fetch Movies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Movies list - Fetch movies
 - Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_movies-list-fetch-movies/)
+ [DEMO LINK](https://misharosa.github.io/react_movies-list-fetch-movies/)
 - Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 - Use [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
 
@@ -14,3 +14,19 @@ Implement `FindMovie` component to load movies from [OMDb API](http://www.omdbap
 1. If a film has been found show the preview as a `MovieCard`.
 1. Add a movie to the list after submitting a form and clear the input.
 1. Don't add the same film to the list twice (compare by `imdbId`).
+
+
+# Список фільмів - отримати фільми
+- Дотримуйтесь [Рекомендації щодо виконання завдань React](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
+- Використовуйте [шпаргалку React TypeScript](https://mate-academy.github.io/fe-program/js/extra/react-typescript)
+
+## Завдання
+Реалізуйте компонент `FindMovie` для завантаження фільмів із [OMDb API](http://www.omdbapi.com/) (Вам потрібно зареєструватися та отримати ключ API).
+
+> Не використовуйте компоненти класу. Натомість використовуйте React Hooks.
+
+1. Коли користувач введе назву фільму та натисне кнопку пошуку, надішліть запит на адресу `https://www.omdbapi.com/?apikey=[yourkey]&t=[title]`.
+1. Якщо плівку не знайдено, відобразіть повідомлення про помилку під вводом. Приховати помилку після зміни назви.
+1. Якщо фільм було знайдено, покажіть попередній перегляд як «Картку фільму».
+1. Додайте фільм до списку після заповнення форми та очистіть введені дані.
+1. Не додавайте один і той самий фільм до списку двічі (порівняйте за `imdbId`).
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,103 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
 
-// import { MovieCard } from '../MovieCard';
+interface Props {
+  addMovie: (movie: Movie) => void
+}
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [error, setError] = useState(false);
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const findMovie = () => {
+    getMovie(title)
+      .then(newMovie => {
+        if (newMovie.Response === 'False') {
+          setMovie(null);
+          setError(true);
+        } else {
+          setMovie(newMovie);
+          setError(false);
+        }
+      });
+  };
+
+  const handleMovie: React.FormEventHandler = (event) => {
+    event.preventDefault();
+
+    if (movie) {
+      addMovie(movie);
+      setTitle('');
+      setMovie(null);
+    }
+  };
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setError(false);
+    setTitle(value);
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              value={title}
+              onChange={(event) => handleInput(event)}
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', {
+                'is-danger': error,
+              })}
+            />
+          </div>
+          {error
+          && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={findMovie}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={handleMovie}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        { movie && <MovieCard movie={movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

73a13345d56e7de1ef641034e1a530ccd6debaa7
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-11-29 22:20:08 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Component } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
@@ -5,23 +6,63 @@ import { FindMovie } from './components/FindMovie';
 
 interface State {
   movies: Movie[];
+  hasLoadingError: boolean;
 }
 
 export class App extends Component<{}, State> {
   state: State = {
     movies: [],
+    hasLoadingError: false,
+  };
+
+  loadFilm = (nameFilm: Movie) => {
+    console.log(this.state.movies);
+    console.log(nameFilm);
+
+    if (this.state.movies.includes(nameFilm)) {
+      return 0;
+    }
+
+    return this.setState((state: State) => ({
+      movies: [...state.movies, nameFilm],
+    }));
+  };
+
+  loadErrorTrue = () => {
+    this.setState((state: State) => ({
+      ...state,
+      hasLoadingError: true,
+    }));
+  };
+
+  loadErrorFalse = () => {
+    this.setState((state: State) => ({
+      ...state,
+      hasLoadingError: false,
+    }));
   };
 
   render() {
-    const { movies } = this.state;
+    const { movies, hasLoadingError } = this.state;
 
     return (
       <div className="page">
         <div className="page-content">
-          <MoviesList movies={movies} />
+          {movies.length > 0 ? (
+            <MoviesList movies={movies} />
+          ) : (
+            <>
+            </>
+          )}
         </div>
         <div className="sidebar">
-          <FindMovie />
+          <FindMovie
+            movies={movies}
+            loadFilm={this.loadFilm}
+            errorTrue={this.loadErrorTrue}
+            errorFalse={this.loadErrorFalse}
+            hasLoadingError={hasLoadingError}
+          />
         </div>
       </div>
     );

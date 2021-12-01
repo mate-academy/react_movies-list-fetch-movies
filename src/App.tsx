01fefdee60f05ccc80c8051182d14e8d827cd4e2
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-12-01 14:31:53 +0200

    Get movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
+import { Movie } from './types/Movie';
 
-interface State {
-  movies: Movie[];
-}
+const addedMovies = new Set<Movie['imdbID']>();
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    if (addedMovies.has(newMovie.imdbID)) {
+      return;
+    }
+
+    setMovies([...movies, newMovie]);
+    addedMovies.add(newMovie.imdbID);
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};

75756bf1c7f31a80ef7962786fd322622fd01628
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-12-02 10:20:40 +0200

    Delete set

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,15 @@ import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
-const addedMovies = new Set<Movie['imdbID']>();
-
 export const App: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   const addMovie = (newMovie: Movie) => {
-    if (addedMovies.has(newMovie.imdbID)) {
+    if (movies.find(movie => movie.imdbID === newMovie.imdbID)) {
       return;
     }
 
     setMovies([...movies, newMovie]);
-    addedMovies.add(newMovie.imdbID);
   };
 
   return (

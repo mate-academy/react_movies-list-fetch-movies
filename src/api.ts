dc22d5809d9076bd86607a34ae14b631aa18e4c7
Alina Shchavinska <shchavinska.alina@gmail.com>
2021-12-24 10:13:31 +0200

    url fix

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const OMDB_API = 'http://www.omdbapi.com/?apikey=b2b9c7f0';
+const OMDB_API = 'https://www.omdbapi.com/?i=tt3896198&apikey=b2b9c7f0';
 
 export const getMovie = async (searchString: string) => {
   return fetch(`${OMDB_API}&t=${searchString}`)

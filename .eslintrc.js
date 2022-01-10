66ca4c19b91b9c26d8975ee3bc735d8f148c4778
nadiaH <nadiahrb79@gmailcom>
2022-01-10 09:31:19 +0200

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 // Uncomment eslint-config-react if you want use react without TS
 
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  // extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
+  // extends: ['@mate-academy/eslint-config-react-typescript',
+  // 'plugin:cypress/recommended'],
+  extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
+  rules: {
+    'jsx-a11y/label-has-associated-control': 'off',
+    'no-console': 'off',
+  },
 };

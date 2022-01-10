beb4497ec7649485cdf10fa48dc6693e00899404
nadiaH <nadiahrb79@gmailcom>
2022-01-10 16:32:40 +0200

    http to https

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,5 @@
 export const getMovie = (title: string) => {
-  return fetch(`http://www.omdbapi.com/?apikey=973a8428&t=${title}`)
+  return fetch(`https://www.omdbapi.com/?apikey=973a8428&t=${title}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(`${response.status} - ${response.statusText}`);

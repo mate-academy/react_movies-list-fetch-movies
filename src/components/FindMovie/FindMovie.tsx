77cae45574e642c44d50be1975f4dceeb59a5ee5
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-01-25 15:23:54 +0200

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getMovie } from '../api/api';
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+type Props = {
+  addMovie: (movie: Movie) => void
+};
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [searchTitle, setSearchTitle] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [isMovieFind, setFindCheck] = useState(true);
+
+  const searchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTitle(event.target.value);
+  };
+
+  const movieFind = async () => {
+    try {
+      const movieFromServer = await getMovie(searchTitle);
+
+      setMovie(movieFromServer);
+
+      if (movieFromServer.Response === 'False') {
+        setFindCheck(false);
+        setMovie(null);
+      }
+    } catch {
+      throw new Error();
+    }
+  };
+
+  const addMovieToList = (movieToList: Movie) => {
+    if (movieToList) {
+      addMovie(movieToList);
+      setMovie(null);
+    }
+
+    setSearchTitle('');
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              value={searchTitle}
+              onChange={searchInput}
+              className={`input ${!isMovieFind ? 'is-danger' : ''}`}
+            />
+          </label>
+
+          {!isMovieFind && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={movieFind}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => movie && addMovieToList(movie)}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        {movie && <h2 className="title">Preview</h2>}
+        {movie && <MovieCard movie={movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

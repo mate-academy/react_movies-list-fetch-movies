d3693b77dccdf5109b0d74cce6cb025ec2863d77
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-02-02 09:13:25 +0200

    Solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { MovieCard } from '../MovieCard';
+import { getMovie } from '../../api';
 
-// import { MovieCard } from '../MovieCard';
+type Props = {
+  addMovie: (movie: Movie) => void,
+};
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [isMovieValid, setIsMovieValid] = useState(true);
+  const [findedMovieTitle, setFindedMovieTitle] = useState('');
+  const [findedMovie, setFindedMovie] = useState({
+    Poster: '',
+    Title: '',
+    Plot: '',
+    imdbID: '',
+  });
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const setMovie = async (title: string) => {
+    if (!(findedMovie.Title || '').toLowerCase().includes(title.toLowerCase())) {
+      const movie = await getMovie(title);
+
+      setFindedMovie(movie);
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+
+            <div className="control">
+              <input
+                type="text"
+                id="movie-title"
+                placeholder="Enter a title to search"
+                className="input is-danger"
+                onChange={(event) => {
+                  setIsMovieValid(true);
+                  setFindedMovieTitle(event.target.value);
+                }}
+              />
+            </div>
+          </label>
+
+          {!isMovieValid && (
+            <p className="help is-danger">Can&apos;t find a movie with such a title</p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
-      </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => setMovie(findedMovieTitle)}
+            >
+              Find a movie
+            </button>
+          </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => addMovie(findedMovie)}
+            >
+              Add to the list
+            </button>
+          </div>
         </div>
+      </form>
 
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
+      {findedMovie.imdbID && (
+        <div className="container">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={findedMovie} />
         </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+      )}
+    </>
+  );
+};

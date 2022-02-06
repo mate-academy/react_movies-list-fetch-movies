1837604f5348853e4bec861e2523336a87611efd
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-02-06 10:34:02 +0200

    Solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com';
+const API_URL = 'https://www.omdbapi.com';
 
 export async function getMovie(title: string): Promise<Movie> {
   const response = await fetch(`${API_URL}/?apikey=1bff3e24&t=${title}`);

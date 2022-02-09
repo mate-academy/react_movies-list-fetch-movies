0b3872bd10fd6cac02325a18c5cf397c58f49bd3
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-02-09 16:45:13 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,66 +2,30 @@ import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
-import { getMovie } from './api/api';
 
 export const App: React.FC = () => {
-  const [movie, setMovie] = useState<Movie>({
-    Poster: '',
-    Title: '',
-    Plot: '',
-    imdbID: '',
-  });
   const [queryInput, setQueryInput] = useState('');
-  const [addedMovie, setAddedMovie] = useState<Movie[]>([]);
-  const [searchError, setSearchError] = useState(false);
-
-  const handleChange = async () => {
-    const movieFromServer = await getMovie(queryInput);
-
-    setMovie(movieFromServer);
-
-    if (!movieFromServer.Title) {
-      return setSearchError(true);
-    }
-
-    return setSearchError(false);
-  };
+  const [addedMovies, setAddedMovies] = useState<Movie[]>([]);
 
   const handleAddMovie = (newMovie: Movie) => {
     if (!newMovie.Title) {
       return;
     }
 
-    setAddedMovie([...addedMovie, newMovie]);
-  };
-
-  const clearInput = () => {
-    setQueryInput('');
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    clearInput();
-    if (addedMovie.find(movieOnList => movieOnList.imdbID === movie.imdbID)) {
-      return;
-    }
-
-    handleAddMovie(movie);
+    setAddedMovies([...addedMovies, newMovie]);
   };
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={addedMovie} />
+        <MoviesList movies={addedMovies} />
       </div>
       <div className="sidebar">
         <FindMovie
+          movies={addedMovies}
+          handleAddMovie={handleAddMovie}
           queryInput={queryInput}
           setQueryInput={setQueryInput}
-          handleChange={handleChange}
-          movie={movie}
-          handleSubmit={handleSubmit}
-          searchError={searchError}
         />
       </div>
     </div>
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,78 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
 
 import { MovieCard } from '../MovieCard';
+import { getMovie } from '../../api/api';
 
 type Props = {
+  handleAddMovie: (newMovie: Movie) => void;
   queryInput: string;
   setQueryInput: React.Dispatch<React.SetStateAction<string>>;
-  handleChange: () => Promise<void>;
-  movie: Movie;
-  handleSubmit: (event: React.FormEvent) => void;
-  searchError: boolean;
+  movies: Movie[];
 };
 
 export const FindMovie: React.FC<Props> = ({
+  handleAddMovie,
   queryInput,
   setQueryInput,
-  handleChange,
-  movie,
-  handleSubmit,
-  searchError,
-}) => (
-  <>
-    <form
-      className="find-movie"
-      onSubmit={handleSubmit}
-    >
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
+  movies,
+}) => {
+  const [movie, setMovie] = useState<Movie>({
+    Poster: '',
+    Title: '',
+    Plot: '',
+    imdbID: '',
+  });
+  const [searchError, setSearchError] = useState(false);
+
+  const handleChange = async () => {
+    const movieFromServer = await getMovie(queryInput);
+
+    setMovie(movieFromServer);
+
+    if (!movieFromServer.Title) {
+      return setSearchError(true);
+    }
+
+    return setSearchError(false);
+  };
+
+  const clearInput = () => {
+    setQueryInput('');
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    clearInput();
+    if (movies.find(movieOnList => movieOnList.imdbID === movie.imdbID)) {
+      return;
+    }
+
+    handleAddMovie(movie);
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={handleSubmit}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+            <div className="control">
+              <input
+                type="text"
+                id="movie-title"
+                placeholder="Enter a title to search"
+                className={searchError ? 'input is-danger' : 'input'}
+                value={queryInput}
+                onChange={event => setQueryInput(event.target.value)}
+              />
+            </div>
+          </label>
+          {searchError && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+        </div>
+
+        <div className="field is-grouped">
           <div className="control">
-            <input
-              type="text"
-              id="movie-title"
-              placeholder="Enter a title to search"
-              className={searchError ? 'input is-danger' : 'input'}
-              value={queryInput}
-              onChange={event => setQueryInput(event.target.value)}
-            />
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => handleChange()}
+            >
+              Find a movie
+            </button>
           </div>
-        </label>
-        {searchError && (
-          <p className="help is-danger">
-            Can&apos;t find a movie with such a title
-          </p>
-        )}
-      </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-            onClick={() => handleChange()}
-          >
-            Find a movie
-          </button>
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary"
+            >
+              Add to the list
+            </button>
+          </div>
         </div>
+      </form>
 
-        <div className="control">
-          <button
-            type="submit"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie.Title ? (
+          <MovieCard movie={movie} />
+        ) : '' }
       </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {movie.Title ? (
-        <MovieCard movie={movie} />
-      ) : '' }
-    </div>
-  </>
-);
+    </>
+  );
+};

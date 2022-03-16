c15808c93c67ae38b675a101f35743f9d6a985b2
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-03-16 17:50:53 +0200

    fetch moviki

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,102 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import classNames from 'classnames';
+import { getMovie } from '../../api/api';
+
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+type Props = {
+  setMovies: (callback: (movies: Movie[]) => Movie[]) => void;
+};
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+export const FindMovie: React.FC<Props> = React.memo(({ setMovies }) => {
+  const [isInvalid, setInvalid] = useState(false);
+  const [movie, setMovie] = useState<Movie>();
+
+  const titleRef = useRef(document.createElement('input'));
+
+  const fetchMovie = async () => {
+    const res = await getMovie(titleRef.current.value);
+
+    if (!res) {
+      setMovie(undefined);
+      setInvalid(true);
+    } else {
+      setMovie(res);
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <div className="control">
+            <label className="label" htmlFor="movie-title">
+              Movie title
+              <input
+                type="text"
+                id="movie-title"
+                ref={titleRef}
+                placeholder="Enter title"
+                className={classNames('input', { 'is-danger': isInvalid })}
+                onChange={() => {
+                  if (isInvalid) {
+                    setInvalid(false);
+                  }
+                }}
+              />
+            </label>
+          </div>
+
+          {isInvalid && (
+            <p className="help is-danger">
+              Couldn&apos;t find movie with this title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => {
+                fetchMovie();
+              }}
+            >
+              Search
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              onClick={() => {
+                if (movie) {
+                  setMovies((movies) => {
+                    if (movies.find(m => m.imdbID === movie.imdbID)) {
+                      return movies;
+                    }
+
+                    return [...movies, movie];
+                  });
+                  setMovie(undefined);
+                  titleRef.current.value = '';
+                }
+              }}
+            >
+              Add to list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && <MovieCard movie={movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+});

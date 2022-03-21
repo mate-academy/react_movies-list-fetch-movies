7a8d0f7fdfff19f26ceaa9fa4b704c82cfebddbd
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-21 17:39:15 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,16 @@ export class App extends Component<{}, State> {
     movies: [],
   };
 
+  addMovie = (movie: Movie): void => {
+    const newState = this.state.movies;
+
+    if (!newState.some(film => film.imdbID === movie.imdbID)) {
+      this.setState({
+        movies: [...newState, movie],
+      });
+    }
+  };
+
   render() {
     const { movies } = this.state;
 
@@ -21,7 +31,7 @@ export class App extends Component<{}, State> {
           <MoviesList movies={movies} />
         </div>
         <div className="sidebar">
-          <FindMovie />
+          <FindMovie addMovie={this.addMovie} />
         </div>
       </div>
     );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,101 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+import classnames from 'classnames';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+import { MovieCard } from '../MovieCard';
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+const API_KEY = '&apikey=a552bcb7';
+
+const API_SERVER = 'https://www.omdbapi.com/';
+
+type AddMovie = {
+  addMovie: (movie: Movie) => void,
+};
+
+export const FindMovie: React.FC<AddMovie> = ({ addMovie }) => {
+  const [movie, setMovie] = useState();
+  const [searchTitle, setSearchTitle] = useState('');
+  const [isMovieFind, setIsMovieFind] = useState(true);
+
+  const findMovie = async (movieTitle: string) => {
+    const response = await fetch(`${API_SERVER}?t=${movieTitle}${API_KEY}`);
+
+    if (!response.ok) {
+      setIsMovieFind(false);
+
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    if (result.Response === 'False') {
+      setIsMovieFind(false);
+    } else {
+      setIsMovieFind(true);
+
+      setMovie(result);
+    }
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classnames('input', { 'is-danger': !isMovieFind })}
+              value={searchTitle}
+              onChange={(event) => {
+                setSearchTitle(event.target.value);
+                setIsMovieFind(true);
+              }}
+            />
+          </div>
+
+          {!isMovieFind && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={() => findMovie(searchTitle)}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className={classnames('button', 'is-primary')}
+              disabled={!isMovieFind}
+              onClick={() => movie && addMovie(movie)}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && <MovieCard movie={movie} />}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

dbe9b942868159f9da21d58cd6bcdd561e4e2a89
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-06 13:38:43 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { Component } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+  const addMovie = (movie:Movie) => {
+    setMovies(currMovies => {
+      if (currMovies.some(currMovie => currMovie.imdbID === movie.imdbID)) {
+        return currMovies;
+      }
+
+      return [...currMovies, { ...movie }];
+    });
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovie={addMovie} />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,106 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { request } from '../../api/requests';
 import './FindMovie.scss';
+import { MovieCard } from '../MovieCard';
 
-// import { MovieCard } from '../MovieCard';
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [titleQuery, setTitleQuery] = useState('');
+  const [queriedMovie, setQueridMovie] = useState<Movie | null>(null);
+  const [hasError, setHasError] = useState(false);
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHasError(false);
+    setTitleQuery(event.target.value);
+  };
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+  const reset = () => {
+    setTitleQuery('');
+    setQueridMovie(null);
+  };
+
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (queriedMovie) {
+      addMovie(queriedMovie);
+      reset();
+    }
+  };
+
+  const getFilm = () => {
+    request(titleQuery).then(loadedMovie => {
+      // eslint-disable-next-line no-console
+      if (loadedMovie.Title) {
+        setQueridMovie(loadedMovie);
+      } else {
+        setHasError(true);
+      }
+    });
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={submitHandler}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', { 'is-danger': hasError })}
+              value={titleQuery}
+              onChange={changeHandler}
+            />
+          </div>
+
+          {hasError && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={getFilm}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary"
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {queriedMovie && (
+          <MovieCard movie={queriedMovie} />
+        )}
       </div>
+    </>
+  );
+};
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+interface Props {
+  addMovie: (movie: Movie) => void;
+}

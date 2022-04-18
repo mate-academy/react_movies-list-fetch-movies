d344d3e8ea4fb92ba388be6ec67c78f66abb8e04
Tykhon Shuliak <tiandzh2013@gmail.com>
2022-04-18 13:29:59 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+  const addMovie = (movie: Movie) => {
+
+    setMovies(prev => {
+      const filteredMovies = prev.filter(i => i.imdbID !== movie.imdbID);
+
+      return [...filteredMovies, movie];
+    });
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie onAdd={addMovie} />
+      </div>
+    </div>
+  );
+};
+
+export default App;

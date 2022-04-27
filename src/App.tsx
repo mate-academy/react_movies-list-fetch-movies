094ff2eaa73f93509108d7b7d9a2d1d6c64ddaa3
Dina Samoilova <s13.dina@gmail.com>
2022-04-27 17:16:19 +0300

    after review change class component to functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,27 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC<{}> = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
-  };
-
-  onAdd = (movie: Movie) => {
-    const { movies } = this.state;
-
+  const onAdd = (movie: Movie) => {
     if (movies.every(film => film.imdbID !== movie.imdbID)) {
-      this.setState(prevState => ({
-        movies: [...prevState.movies, movie],
-      }));
+      setMovies(prevState => {
+        return [...prevState, movie];
+      });
     }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie onAddMovie={this.onAdd} />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie onAddMovie={onAdd} />
+      </div>
+    </div>
+  );
+};

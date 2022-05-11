6d9f211e56a0ddcbee4e329bc5dda6e22077fe08
Olga <arnab@ukr.net>
2022-05-11 22:57:39 +0300

    Fixed url

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=f26a7b2d';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=f26a7b2d';
 
 export const fetchMovie = (endpoint: string) => {
   return fetch(`${BASE_URL}${endpoint}`)

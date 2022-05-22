290ccc1ccd43841303ee15ce3c512eeae3aadcb1
MariaBondar <marybondar777@gmail.com>
2022-05-22 14:46:57 +0200

    dev

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e5addb44&';
 
 export const getMovies = async (title: string) => {
-  const response = await fetch(`${BASE_URL}t=${title}`);
+  const response = await fetch(`${BASE_URL}t=[${title}]`);
 
   return response.json();
 };

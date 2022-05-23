cf10a9ad7c9c51aca6501cd27c936b4467b437b2
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-05-23 17:32:06 +0300

    Styles were changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ import { FindMovie } from './components/FindMovie';
 
 export const App: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [repeat, setRepeat] = useState(false);
+
   const addMovies = useCallback((movie: Movie) => {
     const duplicate = movies.find(({ imdbID }) => imdbID === movie.imdbID);
 
     if (!duplicate) {
       setMovies((state) => [...state, movie]);
+    } else {
+      setRepeat(true);
     }
   }, [movies]);
+
+  const handleRepeat = () => {
+    setRepeat(false);
+  };
 
   return (
     <div className="page">
@@ -19,7 +27,11 @@ export const App: React.FC = () => {
         <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <FindMovie addMovies={addMovies} />
+        <FindMovie
+          addMovies={addMovies}
+          repeat={repeat}
+          handleRepeat={handleRepeat}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,13 +1,20 @@
+import classNames from 'classnames';
 import React, { ChangeEvent, useState } from 'react';
 import { getMovie } from '../../api';
 import { MovieCard } from '../MovieCard';
 import './FindMovie.scss';
 
 type Props = {
+  repeat: boolean;
   addMovies: (m: Movie) => void;
+  handleRepeat: () => void;
 };
 
-export const FindMovie: React.FC<Props> = ({ addMovies }) => {
+export const FindMovie: React.FC<Props> = ({
+  addMovies,
+  repeat,
+  handleRepeat,
+}) => {
   const [movie, setMovie] = useState(null);
   const [query, setQuery] = useState('');
   const [error, setError] = useState('');
@@ -28,6 +35,7 @@ export const FindMovie: React.FC<Props> = ({ addMovies }) => {
 
     setQuery(value);
     setError('');
+    handleRepeat();
   };
 
   const submitForm = (event: React.SyntheticEvent) => {
@@ -53,7 +61,7 @@ export const FindMovie: React.FC<Props> = ({ addMovies }) => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={classNames('input', { 'is-danger': error.length > 0 })}
               value={query}
               onChange={changeQuery}
             />
@@ -64,13 +72,19 @@ export const FindMovie: React.FC<Props> = ({ addMovies }) => {
               Can&apos;t find a movie with such a title
             </p>
           )}
+
+          {repeat && (
+            <p className="help is-danger">
+              This movie is already exist in the list
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
           <div className="control">
             <button
               type="button"
-              className="button is-light"
+              className={classNames('button', { 'is-primary': query.trim() })}
               onClick={loadMovie}
             >
               Find a movie
@@ -80,7 +94,7 @@ export const FindMovie: React.FC<Props> = ({ addMovies }) => {
           <div className="control">
             <button
               type="submit"
-              className="button is-primary"
+              className={classNames('button', { 'is-primary': movie })}
             >
               Add to the list
             </button>
@@ -89,8 +103,12 @@ export const FindMovie: React.FC<Props> = ({ addMovies }) => {
       </form>
 
       <div className="container">
-        <h2 className="title">Preview</h2>
-        {movie && <MovieCard movie={movie} />}
+        {movie && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard movie={movie} />
+          </>
+        )}
       </div>
     </>
   );

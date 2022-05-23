a3174266913e94cb554d17509acd543ff9588d4f
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-23 19:22:11 +0300

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,12 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-import { getMovieByTitle } from './api/api';
-
 export const App: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [titleQuery, setTitleQuery] = useState('');
-  const [previewMovie, setPreviewMovie] = useState<Movie | null>(null);
-  const [isFindErrorVisible, setFindErrorVisible] = useState(false);
-  const [isMovieRepeat, setMovieRepeat] = useState(false);
 
   const checkMovieRepeat = useCallback((id: string) => {
     return movies.some(movie => movie.imdbID === id);
   }, [movies]);
-
-  const getMovieFromServer = useCallback(async () => {
-    const newMovie = await getMovieByTitle(titleQuery);
-
-    if (newMovie.Response === 'False') {
-      setPreviewMovie(null);
-      setFindErrorVisible(true);
-
-      return;
-    }
-
-    if (checkMovieRepeat(newMovie.Title)) {
-      setMovieRepeat(true);
-
-      return;
-    }
-
-    setPreviewMovie(newMovie);
-    setFindErrorVisible(false);
-  }, [titleQuery]);
 
   return (
     <div className="page">
@@ -45,16 +19,7 @@ export const App: React.FC = () => {
       </div>
       <div className="sidebar">
         <FindMovie
-          previewMovie={previewMovie}
-          setPreviewMovie={setPreviewMovie}
           setMovies={setMovies}
-          setTitleQuery={setTitleQuery}
-          titleQuery={titleQuery}
-          getMovieFromServer={getMovieFromServer}
-          isFindErrorVisible={isFindErrorVisible}
-          setFindErrorVisible={setFindErrorVisible}
-          isMovieRepeat={isMovieRepeat}
-          setMovieRepeat={setMovieRepeat}
           checkMovieRepeat={checkMovieRepeat}
         />
       </div>
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,42 +1,66 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import './FindMovie.scss';
 import classNames from 'classnames';
 
 import { MovieCard } from '../MovieCard';
+import { getMovieByTitle } from '../../api/api';
 
 type Props = {
-  previewMovie: Movie | null;
-  setPreviewMovie: React.Dispatch<React.SetStateAction<Movie | null>>;
   setMovies: React.Dispatch<React.SetStateAction<Movie[]>>;
-  setTitleQuery: React.Dispatch<React.SetStateAction<string>>;
-  titleQuery: string;
-  getMovieFromServer: () => Promise<void>;
-  isFindErrorVisible: boolean;
-  setFindErrorVisible: React.Dispatch<React.SetStateAction<boolean>>;
-  isMovieRepeat: boolean;
   checkMovieRepeat: (title: string) => boolean;
-  setMovieRepeat: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const FindMovie: React.FC<Props> = ({
-  previewMovie,
-  setPreviewMovie,
   setMovies,
-  setTitleQuery,
-  titleQuery,
-  getMovieFromServer,
-  isFindErrorVisible,
-  setFindErrorVisible,
-  isMovieRepeat,
   checkMovieRepeat,
-  setMovieRepeat,
 }) => {
+  const [isFindErrorVisible, setFindErrorVisible] = useState(false);
+  const [isMovieRepeat, setMovieRepeat] = useState(false);
+  const [titleQuery, setTitleQuery] = useState('');
+  const [previewMovie, setPreviewMovie] = useState<Movie | null>(null);
+
+  const getMovieFromServer = useCallback(async () => {
+    const newMovie = await getMovieByTitle(titleQuery);
+
+    if (newMovie.Response === 'False') {
+      setPreviewMovie(null);
+      setFindErrorVisible(true);
+
+      return;
+    }
+
+    if (checkMovieRepeat(newMovie.Title)) {
+      setMovieRepeat(true);
+
+      return;
+    }
+
+    setPreviewMovie(newMovie);
+    setFindErrorVisible(false);
+  }, [titleQuery]);
+
   const isInputEmpty = useCallback((input: string) => {
     return !input.trim();
   }, []);
 
   const isInputValid = useCallback((input: string) => {
     return input.match(/[A-Za-z0-9А-Яа-я .,!?-]/g);
+  }, []);
+
+  const handleFilterChange = useCallback((value: string) => {
+    if (isInputEmpty(value)) {
+      setTitleQuery('');
+
+      return;
+    }
+
+    if (!isInputValid(value[value.length - 1])) {
+      return;
+    }
+
+    setMovieRepeat(false);
+    setFindErrorVisible(false);
+    setTitleQuery(value);
   }, []);
 
   const handleFindButton = useCallback(async () => {
@@ -90,23 +114,7 @@ export const FindMovie: React.FC<Props> = ({
                 { 'is-danger': isFindErrorVisible || isMovieRepeat },
               )}
               value={titleQuery}
-              onChange={({ target }) => {
-                const { value } = target;
-
-                if (isInputEmpty(value)) {
-                  setTitleQuery('');
-
-                  return;
-                }
-
-                if (!isInputValid(value[value.length - 1])) {
-                  return;
-                }
-
-                setMovieRepeat(false);
-                setFindErrorVisible(false);
-                setTitleQuery(value);
-              }}
+              onChange={({ target }) => handleFilterChange(target.value)}
             />
           </div>
 

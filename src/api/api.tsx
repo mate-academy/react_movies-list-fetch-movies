a9ab15da2e29b4bc5a98bc28b422a7ad1b9efd67
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-05-24 16:56:03 +0300

    url was changed

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=7a6c8562';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=7a6c8562';
 
 export const getMovie = async (endpoint: string) => {
   const response = await fetch(`${BASE_URL}&t=${endpoint}`);

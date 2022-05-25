54f9d571026b1b31418f627733826e476cc812de
Ihor Mural <igor201278789887000@gmail.com>
2022-05-25 17:24:51 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,107 @@
-import React from 'react';
+import React, { useCallback, useState, FormEvent } from 'react';
 import './FindMovie.scss';
+import { getMovies } from '../api/api';
+import { MovieCard } from '../MovieCard';
 
-// import { MovieCard } from '../MovieCard';
+type Props = {
+  addMovie: (x: Movie) => void;
+};
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [queryError, setQueryError] = useState('');
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
+  const onFind = useCallback(() => {
+    if (!query.trim()) {
+      setQueryError('Please, fill correct');
+    } else {
+      const findMovie = async () => {
+        const result = await getMovies(query);
+
+        if (result.Response === 'True') {
+          setMovie(result);
+          setQueryError('');
+        } else {
+          setQueryError('Can not find a movie with this title');
+        }
+      };
+
+      findMovie();
+    }
+  }, [query]);
+
+  const onAdd = useCallback((event: FormEvent) => {
+    event.preventDefault();
+
+    if (!queryError && movie) {
+      addMovie(movie);
+      setMovie(null);
+      setQuery('');
+    }
+  }, [movie, queryError]);
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={onAdd}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className="input"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
+            />
+          </div>
+
+          {queryError && (
+            <p className="help is-danger">
+              {queryError}
+            </p>
+          )}
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              onClick={onFind}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary"
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      {movie && (
+        <div className="container">
+          <h2 className="title">Preview</h2>
+          <MovieCard
+            movie={movie}
           />
         </div>
-
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
-      </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+      )}
+    </>
+  );
+};

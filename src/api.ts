b75b6d7fac2541a40e2d4ba5aedaa37cf48128de
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-05-25 23:16:06 +0300

    solution

---
+++ src/api.ts
@@ -0,0 +1,7 @@
+const URL = 'https://www.omdbapi.com/?apikey=e10e36ec&t=';
+
+export async function getMovie(title: string) {
+  const response = await fetch(URL + title);
+
+  return response.json();
+}

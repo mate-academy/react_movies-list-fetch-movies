63b7a9c93bee71472127be0edf5f46f44c29bb37
Anastasiia Sharkova <sharkova.nastik@gmail.com>
2022-05-26 12:54:30 +0200

    solution v3

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=2d3b5cf4';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=2d3b5cf4';
 
 export const getMovie = async (endpoint: string) => {
   const response = await fetch(`${BASE_URL}&t=${endpoint}`);

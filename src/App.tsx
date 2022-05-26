d055790e157093ad473932662708196e2cc540d8
yurii837 <gorogotskiy@gmail.com>
2022-05-26 17:12:41 +0300

    raw version

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import { Component } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movieFromServ, setMovieFromServ] = useState<Movie[] | []>([]);
+  const [isDuplicate, setIsDuplicate] = useState(false);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
-  };
+  const addMovie = useCallback((newMovie: Movie) => {
+    if (!movieFromServ.some(movie => movie.imdbID === newMovie.imdbID)) {
+      setMovieFromServ([...movieFromServ, newMovie]);
+      setIsDuplicate(false);
+    } else {
+      setIsDuplicate(true);
+    }
+  }, [movieFromServ]);
 
-  render() {
-    const { movies } = this.state;
+  return (
+    <div className="page">
+      <div className="page-content">
 
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+        {movieFromServ && (
+          <MoviesList
+            movies={movieFromServ}
+          />
+        )}
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        {isDuplicate
+        && <h3>Such movie is allready exist</h3>}
+        <FindMovie
+          addMovie={addMovie}
+        />
+      </div>
+    </div>
+  );
+};

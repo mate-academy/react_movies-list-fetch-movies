e53c622f7f56696e94c6db2c0dcf4ed31c1b88f2
Olha Blokhina <olhablokhina@ip-10-10-0-199.eu-central-1.compute.internal>
2022-05-30 14:45:28 +0300

    NewTests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,82 @@
-// WRITE TESTS HERE
+const page = {
+  findMovie(input) {
+    return cy.get('#movie-title')
+      .type(`${input}`)
+      .get('button').eq(0).click();
+  }
+};
+
+const btn = {
+  clickBtn() {
+    return cy.get('button')
+      .eq(1)
+      .click();
+  }
+};
 
 describe('Page', () => {
-  it('should be visitable', () => {
-    cy.visit('/');
+  beforeEach (() => {
+    cy.visit ('/')
+  });
+
+  it('should send a request to www.omdbapi.com', () => {
+    cy.intercept('*/www.omdbapi.com/*', cy.spy()
+      .as('apiCall'));
+    page.findMovie('car');
+    cy.get('@apiCall')
+      .its('callCount')
+      .should('equal', 1);
+  });
+
+  it('should find movie', ()=> {
+    page.findMovie('car');
+    cy.getByDataCy('container')
+      .should('contain', 'Preview');
+    cy.getByDataCy('movie-title')
+      .should('contain', `Dude, Where's My Car?`);
+    cy.getByDataCy('content')
+      .should('contain', 'Two potheads wake up after a night of partying');
+    cy.getByDataCy('card-image')
+      .should('exist');
+  });
+
+  it('should add movie to list', () =>{
+    page.findMovie('car');
+    btn.clickBtn();
+    cy.get('.movies')
+      .children()
+      .should('have.length', 1);
+  });
+
+  it('should not add film twice', () => {
+    page.findMovie('car');
+    btn.clickBtn();
+    cy.get('button')
+      .eq(1)
+      .should('be.disabled');
+  });
+
+  it('should clear the form after adding film to the list', () =>{
+    page.findMovie('car');
+    btn.clickBtn();
+    cy.get('.sidebar')
+      .children()
+      .should('have.length', 1);
+  });
+
+  it('should display error message if film is not found', () => {
+    page.findMovie('!@#');
+    cy.get('.sidebar')
+      .should('contain', "Can't find a movie with such a title");
+  });
+
+  it('should hide error message after changing film title', () => {
+    page.findMovie('!@#');
+    cy.get('.sidebar')
+      .should('contain', "Can't find a movie with such a title");
+    cy.get('#movie-title')
+      .type('{selectall}car');
+    cy.get('.sidebar')
+      .should('not.contain', "Can't find a movie with such a title");
   });
 });

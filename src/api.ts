ad18a61f158f2249b7bcfb6bc765d5d864e460b3
Andriy Shvets <PiratesQ@i.ua>
2022-05-31 15:03:12 +0300

    changed http to https

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d97e20ba';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=d97e20ba';
 
 export const getMovie = async (title: string) => {
   const response = await fetch(`${API_URL}&t=${title}`);

ed6760a868201238e7038aaf45b6b7e8f3088ce5
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-06-02 18:35:33 +0300

    change protocol to https in api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt0098936&apikey=85369871&';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt0098936&apikey=85369871&';
 
 export const getMovies = async (title: string) => {
   const response = await fetch(`${BASE_URL}t=${title}`);

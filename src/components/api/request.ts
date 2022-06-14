7595e63d4ad668baca92e47b2d87b4d361020cf9
Roman Pavlenko <proman9973@gmail.com>
2022-06-14 16:30:34 +0300

    fast fix

--- src/components/api/request.ts
+++ src/components/api/request.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?apikey=2d2038e9&?';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=2d2038e9&?';
 
 export async function request(title : string) {
   const result = await (await fetch(`${BASE_URL}&t=${title}`)).json();

7dacd2b4f74d945b8a5f2bf1b0af089f10e82e1f
Andriyk0 <andriy22221@gmail.com>
2022-06-17 16:44:32 +0300

    movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App:React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+  const addMovies = (value: Movie) => {
+    const dublicate = movies.some(item => item.imdbID === value.imdbID);
+
+    if (!dublicate) {
+      setMovies([...movies, value]);
+    }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie addMovies={addMovies} />
+      </div>
+    </div>
+  );
+};

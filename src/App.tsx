fe98d8c23ed6b61f61883909e21198deb7894b01
Daryna Chekmariova <dashachekmariova@gmail.com>
2022-06-18 20:31:31 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>();
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+  const movieList = (newMovie: Movie) => {
+    if (!movies?.includes(newMovie)) {
+      setMovies((prev) => {
+        return prev
+          ? [...prev, newMovie]
+          : [newMovie];
+      });
+    }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        {
+          movies && <MoviesList movies={movies} />
+        }
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie movieList={movieList} />
+      </div>
+    </div>
+  );
+};
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
 
-// import { MovieCard } from '../MovieCard';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+type Props = {
+  movieList: (newMovie: Movie) => void,
+};
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+export const FindMovie: React.FC<Props> = ({ movieList }) => {
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState<Movie>();
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (movie?.Response === 'False') {
+      setError(true);
+    }
+  }, [movie]);
+
+  const getMovie = async (inputTitle: string) => {
+    const responce = await fetch(`https://www.omdbapi.com/?apikey=e3df8ec5&t=${inputTitle}`);
+
+    return responce.json();
+  };
+
+  return (
+    <>
+      <form className="find-movie">
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              value={title}
+              onChange={(event) => {
+                setTitle(event.target.value);
+                setError(false);
+              }}
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', {
+                'is-danger': error,
+              })}
+            />
+          </div>
+
+          {error && (
+            <p className="help is-danger">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-light"
+              data-cy="find"
+              onClick={() => (
+                getMovie(title)
+                  .then(movieFromServer => setMovie(movieFromServer))
+              )}
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              data-cy="add"
+              disabled={movie?.Response === 'False'}
+              onClick={() => {
+                if (movie) {
+                  movieList(movie);
+                }
+
+                setTitle('');
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">{error && movie?.Error}</h2>
+        {
+          movie && movie?.Response === 'True' && <MovieCard movie={movie} />
+        }
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

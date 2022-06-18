77726b8e924a6681d8bc9a7c77df004661394d3f
Maryna <savna023@gmail.com>
2022-06-18 13:51:07 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,54 +1,106 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import classNames from 'classnames';
+import { getMovie } from '../../api/api';
 import './FindMovie.scss';
 
-// import { MovieCard } from '../MovieCard';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => (
-  <>
-    <form className="find-movie">
-      <div className="field">
-        <label className="label" htmlFor="movie-title">
-          Movie title
-        </label>
+type Props = {
+  onAddMovie: (movie: Movie) => void;
+};
 
-        <div className="control">
-          <input
-            type="text"
-            id="movie-title"
-            placeholder="Enter a title to search"
-            className="input is-danger"
-          />
+export const FindMovie: React.FC<Props> = ({ onAddMovie }) => {
+  const [movie, setMovie] = useState<Movie>();
+  const [isMovie, setIsMovie] = useState(true);
+  const [title, setTitle] = useState('');
+
+  const findMovie = async () => {
+    const foundMovie = await getMovie(title);
+
+    if (foundMovie.Response === 'True') {
+      setMovie(foundMovie);
+    } else {
+      setIsMovie(false);
+    }
+  };
+
+  const titleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+
+    setIsMovie(true);
+  };
+
+  const submitHandler = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await findMovie();
+  };
+
+  return (
+    <>
+      <form
+        className="find-movie"
+        onSubmit={submitHandler}
+      >
+        <div className="field">
+          <label className="label" htmlFor="movie-title">
+            Movie title
+          </label>
+
+          <div className="control">
+            <input
+              type="text"
+              value={title}
+              onChange={titleHandler}
+              id="movie-title"
+              placeholder="Enter a title to search"
+              className={classNames('input', {
+                'is-danger': !isMovie,
+              })}
+            />
+            {!isMovie && (
+              <p className="help is-danger">
+                Can&apos;t find a movie with such a title
+              </p>
+            )}
+          </div>
         </div>
 
-        <p className="help is-danger">
-          Can&apos;t find a movie with such a title
-        </p>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-light"
+              data-cy="find"
+            >
+              Find a movie
+            </button>
+          </div>
+
+          <div className="control">
+            <button
+              type="button"
+              className="button is-primary"
+              data-cy="add"
+              onClick={() => {
+                if (movie) {
+                  onAddMovie(movie);
+                  setTitle('');
+                }
+              }}
+            >
+              Add to the list
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div className="container">
+        <h2 className="title">Preview</h2>
+        {movie && (
+          <MovieCard movie={movie} />
+        )}
       </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button
-            type="button"
-            className="button is-light"
-          >
-            Find a movie
-          </button>
-        </div>
-
-        <div className="control">
-          <button
-            type="button"
-            className="button is-primary"
-          >
-            Add to the list
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <div className="container">
-      <h2 className="title">Preview</h2>
-      {/* <MovieCard  /> */}
-    </div>
-  </>
-);
+    </>
+  );
+};

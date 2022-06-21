aa987aacbd1112bb108af00bfe3bc1d9a3a42de3
Alexandra Protyanova <protyanova22@gmail.com>
2022-06-21 15:22:20 +0300

    solution

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API = ' http://www.omdbapi.com/?i=tt3896198&apikey=16b19e1d';
+
+export const getFilmByTitle = (title: string): Promise<Movie> => {
+  return fetch(`${API}&t=${title}`)
+    .then(response => {
+      return response.json();
+    });
+};

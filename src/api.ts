91a64d29d6e9c404962bc68cf2b274f8aa7b8b89
Alexandra Protyanova <protyanova22@gmail.com>
2022-06-21 16:24:45 +0300

    try

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API = ' https://www.omdbapi.com/?i=tt3896198&apikey=16b19e1d';
+const API = 'https://www.omdbapi.com/?i=tt3896198&apikey=16b19e1d';
 
 export const getFilmByTitle = (title: string): Promise<Movie> => {
   return fetch(`${API}&t=${title}`)

4b0b4f19377494d4d1758543f551377fe6025d98
Oleksandr <pogrebnyak19999@gmail.com>
2022-06-22 22:10:46 +0200

    Fix bug with url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=da70b4e9';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=da70b4e9';
 
 const request = (url: string) => fetch(`${BASE_URL}${url}`)
   .then(resp => resp.json());

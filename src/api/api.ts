4473093ca3e940ea5faf8a24bc6f9e1e18bcfa9c
efdalyalcin <efdalyalcin@hotmail.com>
2022-06-29 14:36:08 +0300

    https problem solved

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=602f3d88';
+const BASE_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=602f3d88';
 
 export const getMovie = async (title: string) => {
   const request = await fetch(`${BASE_URL}&t=${title}`);

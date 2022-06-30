a94ddfc213c16d5a0b03d282fa4724a2ffebc2c2
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2022-06-30 14:15:24 +0200

    fixed api link

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=5cedccaf';
+const API_URL = 'https://www.omdbapi.com/?apikey=5cedccaf';
 
 export const getMovie = async (title: string): Promise<Movie> => {
   const movie = await fetch(`${API_URL}&t=${title}`);

e6b80e8b84f9ba13c9bcd37a2d9aec71d2e25455
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-06-30 18:04:07 +0300

    fix demo link

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 
-interface State {
-  movies: Movie[];
-}
+export const App: React.FC = () => {
+  const [movies, setMovie] = useState<Movie[]>([]);
+  const addMovie = (movie: Movie) => {
+    const noDouble = movies.find(item => item.imdbID === movie.imdbID);
 
-export class App extends Component<{}, State> {
-  state: State = {
-    movies: [],
+    if (!noDouble) {
+      setMovie([...movies, movie]);
+    }
   };
 
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <FindMovie />
-        </div>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MoviesList movies={movies} />
       </div>
-    );
-  }
-}
+      <div className="sidebar">
+        <FindMovie
+          addMovie={addMovie}
+        />
+      </div>
+    </div>
+  );
+};

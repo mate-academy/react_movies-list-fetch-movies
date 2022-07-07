bb4c3d30e041016868a657dfde0d2973d9516d8a
Lilia Mazur <liliamazur2508@gmail.com>
2022-07-07 16:20:31 +0300

    solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=63452f61&t=';
+const API_URL = 'http://www.omdbapi.com/?apikey=63452f61&t=';
 
 export function getMovie(title: string) :Promise<Movie> {
   return fetch(`${API_URL}${title}`)

2d36d0721dc3db47c2e0e42337f60ffda17aff67
mira1zu <mira1zu733@gmail.com>
2022-07-26 14:35:37 +0200

    API fetch fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 const API_KEY = 'fe759ad0';
-const BASE_URL = 'http://www.omdbapi.com/';
+const BASE_URL = 'https://www.omdbapi.com/';
 
 const request = async (url: string) => {
   return (await fetch(`${BASE_URL}${url}`)).json();

6466c7a95b5eb84e5d9ea078fb6aba230ddda61a
Andrii Nychaiuk <anichajuk@gmail.com>
2022-07-26 22:10:41 +0300

    small change

--- src/api.tsx
+++ src/api.tsx
@@ -2,6 +2,5 @@ export function getMovie(title: string): Promise<Movie> {
   return fetch(`https://www.omdbapi.com/?apikey=c48d1bf&t=${title}`)
     .then(response => (response.ok
       ? response.json()
-      // eslint-disable-next-line prefer-promise-reject-errors
-      : Promise.reject('someErrorMessage')));
+      : Promise.reject()));
 }

5989d5603004917a9df940b2ab8a53beeb716f6b
Illia Vodonis <illavodonis@gmail.com>
2022-07-27 13:01:06 +0300

    fixes Base Url

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { Movie } from './react-app-env';
 
-const BASE_URL = 'http://www.omdbapi.com/?apikey=a56ca071&t=';
+const BASE_URL = 'https://www.omdbapi.com/?apikey=a56ca071&t=';
 
 export const getMovie = async (title: string):Promise<Movie> => {
   const res = await fetch(`${BASE_URL}${title}`);

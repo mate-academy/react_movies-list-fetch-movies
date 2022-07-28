21535171ba079ca8ea0fb218a87e3be24cd6232f
Mariia Nedilko <mary.nedilko1@gmail.com>
2022-07-28 16:55:25 +0200

    link fixed

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5d10842a';
+const API_URL = 'http://www.omdbapi.com/?apikey=5d10842a';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

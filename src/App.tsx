0f08cfb9b03b379f7f438363f5a5815136b0fc2a
Roman Marunkevich <vinnibea@gmail.com>
2022-07-28 03:34:15 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
+import { MovieData } from './types/MovieData';
+import { convertMovie } from './api';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, addMovies] = useState<Movie[]>([]);
+  const [query, getQuery] = useState('');
+  const [newMovie, getNewMovie] = useState<MovieData | null>(null);
+  const [updateMovieList, setUpdateMovieList] = useState(false);
+
+  useEffect(() => {
+    if (newMovie === null) {
+      return;
+    }
+
+    addMovies((currentMovies) => {
+      const newFilm = convertMovie(newMovie);
+      const checkImdbId
+        = currentMovies?.find(movie => movie.imdbId === newFilm.imdbId);
+
+      if (!checkImdbId) {
+        return ([
+          ...currentMovies,
+          newFilm,
+        ]);
+      }
+
+      return currentMovies;
+    });
+
+    setUpdateMovieList(false);
+  }, [updateMovieList]);
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        {movies.length && <MoviesList movies={movies} />}
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          getQuery={getQuery}
+          inputValue={query}
+          findNewMovie={getNewMovie}
+          onFormSubmit={setUpdateMovieList}
+        />
       </div>
     </div>
   );
--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,41 @@
+import { Movie } from './types/Movie';
 import { MovieData } from './types/MovieData';
-import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=d049b4a';
+const API_URL = 'https://www.omdbapi.com/?apikey=1c6320fb';
+const picture = 'https://via.placeholder.com/360x270.png';
 
-export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  return fetch(`${API_URL}&t=${query}`)
-    .then(res => res.json())
-    .catch(() => ({
-      Response: 'False',
-      Error: 'unexpected error',
-    }));
+export const request = async (query: string): Promise<MovieData> => {
+  const response = await fetch(`${API_URL}&t=${query}`);
+
+  if (!response.ok) {
+    Promise.reject(new Error('Film not found'));
+  }
+
+  return response.json();
+};
+
+export function convertMovie(film: MovieData): Movie {
+  const {
+    Title: title,
+    Plot: description,
+    imdbID: imdbId,
+    Poster: imgUrl,
+  } = film;
+
+  return {
+    title,
+    description,
+    imdbId,
+    imgUrl: imgUrl === 'N/A'
+      ? picture
+      : imgUrl,
+    // eslint-disable-next-line
+    imdbUrl: 'https://www.imdb.com/title/' + imdbId,
+  };
+}
+
+export function convertQuery(title: string): string {
+  return title.toLowerCase()
+    .trim()
+    .replace(/[\s]/gi, '+');
 }

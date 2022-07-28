3eddd1a8ab8f5d66ad64ae731eda354846183212
Dima Pyvovarchuk <darkfix137@gmail.com>
2022-07-28 13:34:49 +0300

    api url was changed

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=a722759e';
+const API_URL = 'https://www.omdbapi.com/?apikey=a722759e';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

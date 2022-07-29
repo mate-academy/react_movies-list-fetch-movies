fe7a67060366f96cbc2939c977ee9b2d889c8047
Pavlo <Pmoroz2004@gmail.com>
2022-07-29 14:06:04 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,82 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
+import { getMovie } from './api';
+import { MovieCard } from './components/MovieCard';
+import { MovieData } from './types/MovieData';
+import { ResponseError } from './types/ReponseError';
+import { Loader } from './components/Loader';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [hasFound, setHasFound] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const findMovie = async (name: string) => {
+    setMovie(null);
+    setLoading(true);
+
+    try {
+      const movieFromServer: MovieData | ResponseError = await getMovie(name);
+
+      setLoading(false);
+
+      if (movieFromServer.Response === 'True') {
+        setMovie({
+          title: movieFromServer.Title,
+          description: movieFromServer.Plot,
+          imgUrl: movieFromServer.Poster,
+          imdbUrl: movieFromServer.imdbID,
+          imdbId: movieFromServer.imdbID,
+        });
+
+        setHasFound(true);
+      } else {
+        setHasFound(false);
+      }
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const containDublicates = () => {
+    if (movie) {
+      const hasDublicates = movies
+        .map(mov => mov.imdbId)
+        .includes(movie.imdbId);
+
+      return hasDublicates;
+    }
+
+    return false;
+  };
+
+  const addToMovieList = () => {
+    if (movie && !containDublicates()) {
+      setMovies([
+        ...movies,
+        movie,
+      ]);
+      setMovie(null);
+    }
+  };
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        <MoviesList movies={movies} dublicate={containDublicates()} />
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          findMovie={findMovie}
+          onAdd={addToMovieList}
+          hasFound={hasFound}
+        />
+        {loading
+          ? <Loader />
+          : movie && <MovieCard movie={movie} /> }
       </div>
     </div>
   );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
@@ -6,15 +6,34 @@ import { Movie } from '../../types/Movie';
 
 type Props = {
   movies: Movie[];
+  dublicate: boolean,
 };
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard
-        key={movie.imdbId}
-        movie={movie}
-      />
-    ))}
-  </div>
-);
+export const MoviesList: React.FC<Props> = ({ movies, dublicate }) => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => setShow(true), [movies]);
+
+  return (
+    <div className="movies">
+      {dublicate && show && (
+        <div className="notification">
+          <button
+            className="delete"
+            type="button"
+            onClick={() => setShow(false)}
+          >
+            +
+          </button>
+          its dublicate
+        </div>
+      )}
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  );
+};

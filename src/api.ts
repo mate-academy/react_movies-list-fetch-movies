553beca8fddbd96c662fad7312151380eeaedba7
Ihor Shynkar <aedricson@gmail.com>
2022-07-29 16:37:54 +0300

    added search form

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=d049b4a';
+const API_URL = 'https://www.omdbapi.com/?apikey=24c1888d';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

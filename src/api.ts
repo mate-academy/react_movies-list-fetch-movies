de90a331ab03efb11008547750b48feeca024c45
Sasha Havryliuk <princesasha9178@icloud.com>
2022-08-01 08:58:40 +0300

    Solution

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,23 @@
+// import { Movie } from './types/Movie';
 import { MovieData } from './types/MovieData';
-import { ResponseError } from './types/ReponseError';
+// import { MovieData } from './types/MovieData';
+// import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?apikey=e5c72b87';
 
-export function getMovie(query: string): Promise<MovieData | ResponseError> {
+export function getMovie(query: string): Promise<MovieData> {
   return fetch(`${API_URL}&t=${query}`)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        return null;
+      }
+
+      if (res.headers.get('content-type')?.includes('application.json')) {
+        return null;
+      }
+
+      return res.json();
+    })
     .catch(() => ({
       Response: 'False',
       Error: 'unexpected error',

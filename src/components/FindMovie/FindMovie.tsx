f93be6e88a924ff68da0e5fc54cecc194339deb7
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-08-01 08:58:59 +0300

    finished movie search

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import classNames from 'classnames';
+import { getMovie } from '../../api';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  findMovie: (newMovie: Movie) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ findMovie }) => {
+  const [query, setSearch] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [error, setError] = useState(false);
+  const [isLoaded, setLoaded] = useState(false);
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!query) {
+      return;
+    }
+
+    setLoaded(true);
+    getMovie(query)
+      .then(data => {
+        if ('Error' in data) {
+          setError(true);
+          setLoaded(false);
+
+          return;
+        }
+
+        setMovie({
+          title: data.Title,
+          imgUrl: data.Poster,
+          imdbId: data.imdbID,
+          imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
+          description: data.Plot,
+        });
+
+        setError(false);
+        setLoaded(false);
+      });
+  };
+
+  const onAdd = () => {
+    if (!movie) {
+      return;
+    }
+
+    findMovie(movie);
+    setSearch('');
+    setMovie(null);
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={(event) => onSubmit(event)}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +73,16 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={(event) => setSearch(event.target.value)}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {error && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +90,37 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              disabled={!query}
+              className={classNames('button is-light', {
+                'is-loading': isLoaded,
+              })}
             >
               Find a movie
             </button>
           </div>
 
           <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
+            {movie && (
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                disabled={!query}
+                onClick={onAdd}
+              >
+                Add to the list
+              </button>
+            )}
           </div>
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

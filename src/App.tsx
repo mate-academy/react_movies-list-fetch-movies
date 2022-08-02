cb65500c87657f3898387ab9ae5aed7711bf03db
Serhii Vorona <seregavorona94@gmail.com>
2022-08-02 08:43:24 +0300

    1thCommitFetchMovies

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addNewMovie = (newMovie: Movie) => {
+    if (!movies.some(movie => movie.imdbId === newMovie.imdbId)) {
+      setMovies(prevMovies => [
+        ...prevMovies,
+        newMovie,
+      ]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +23,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addNewMovie={addNewMovie} />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useState } from 'react';
+
+import { getMovie } from '../../api';
+import { Movie } from '../../types/Movie';
+import { MovieData } from '../../types/MovieData';
+import { MovieCard } from '../MovieCard';
+
 import './FindMovie.scss';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  addNewMovie: (movie: Movie) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ addNewMovie }) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [query, setQuery] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const createMovie = (findMovie: MovieData) => {
+    const {
+      Poster,
+      Title,
+      Plot,
+      imdbID,
+    } = findMovie;
+
+    setMovie({
+      title: Title,
+      description: Plot,
+      imdbId: imdbID,
+      imgUrl: !Poster
+        ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+        : Poster,
+      imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleFindMovie = () => {
+    setLoading(true);
+
+    getMovie(query).then(findMovie => {
+      if ('Error' in findMovie) {
+        setHasError(true);
+        setLoading(false);
+      } else {
+        createMovie(findMovie);
+        setHasError(false);
+        setLoading(false);
+      }
+    });
+  };
+
+  const handleListAdd = () => {
+    if (movie) {
+      addNewMovie(movie);
+      setMovie(null);
+      setQuery('');
+    }
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSubmit}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +79,18 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,7 +98,12 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames(
+                'button is-light',
+                { 'is-loading': loading },
+              )}
+              disabled={!query}
+              onClick={handleFindMovie}
             >
               Find a movie
             </button>
@@ -41,6 +114,8 @@ export const FindMovie: React.FC = () => {
               data-cy="addButton"
               type="button"
               className="button is-primary"
+              disabled={!movie}
+              onClick={handleListAdd}
             >
               Add to the list
             </button>
@@ -48,10 +123,12 @@ export const FindMovie: React.FC = () => {
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

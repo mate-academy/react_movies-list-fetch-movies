cf835c2e4712ae5bc4ad5436cd04dfff8ff79355
Ivan <igranchak8@gmail.com>
2022-08-08 16:07:36 +0300

    react movies list

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,31 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [moviesFavoriteList, setMoviesFavoriteList] = useState<Movie[]>([]);
+
+  const onAddMovieToFavorites = (newMovie: Movie) => {
+    setMoviesFavoriteList((prevList: Movie[]) => {
+      if (prevList.some(movie => movie.imdbId === newMovie.imdbId)) {
+        return prevList;
+      }
+
+      const newFavoriteList = prevList
+        .map((movie:Movie) => ({ ...movie }));
+
+      newFavoriteList.push({ ...newMovie });
+
+      return newFavoriteList;
+    });
+  };
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        <MoviesList movies={moviesFavoriteList} />
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAddMovieToFavorites={onAddMovieToFavorites} />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+import { getMovie } from '../../api';
+import { Movie } from '../../types/Movie';
 import './FindMovie.scss';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  onAddMovieToFavorites: (movie: Movie) => void,
+};
+
+export const FindMovie: React.FC<Props> = ({ onAddMovieToFavorites }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [foundedMovie, setFoundedMovie] = useState<Movie | null>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const searchHandler = async () => {
+    setIsLoading(true);
+
+    try {
+      const res = await getMovie(searchTerm);
+
+      if ('Title' in res) {
+        const newMovie: Movie = {
+          title: res.Title,
+          description: res.Plot,
+          imgUrl: res.Poster !== 'N/A'
+            ? res.Poster
+            : 'https://via.placeholder.com/360x270.png?text=no%20preview',
+          imdbId: res.imdbID,
+          imdbUrl: `https://www.imdb.com/title/${res.imdbID}`,
+        };
+
+        setFoundedMovie(newMovie);
+      } else {
+        setHasError(true);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -17,41 +56,67 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={searchTerm}
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+                setHasError(false);
+                setFoundedMovie(null);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+
         </div>
 
         <div className="field is-grouped">
           <div className="control">
             <button
+              disabled={!searchTerm}
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={cn('button', 'is-light', { 'is-loading': isLoading })}
+              onClick={(event) => {
+                event.preventDefault();
+                searchHandler();
+              }}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {foundedMovie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  if (foundedMovie) {
+                    onAddMovieToFavorites(foundedMovie);
+                  }
+
+                  setSearchTerm('');
+                  setFoundedMovie(null);
+                }}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {foundedMovie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={foundedMovie} />
+        </div>
+      )}
     </>
   );
 };

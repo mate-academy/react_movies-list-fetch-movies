a435be5b6b3ce6e3b4c53467ec920731a6a07520
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-08-15 21:55:29 +0300

    solution

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import { MovieData } from './types/MovieData';
-import { ResponseError } from './types/ReponseError';
+// import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?apikey=d049b4a';
 
-export function getMovie(query: string): Promise<MovieData | ResponseError> {
+export function getMovie(query: string): Promise<MovieData> {
   return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())
     .catch(() => ({
       Response: 'False',
-      Error: 'unexpected error',
+      Error: 'error',
     }));
 }

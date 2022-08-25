da9c7845684eb9a7e0e255c758b91531b709557c
Yevhen Nikolaienko <illnino380@gmail.com>
2022-08-25 14:05:30 +0300

    implemented movies list with fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addFoundMovieToList = (currentMovie: Movie) => {
+    const isInMovieList = movies
+      .some(movie => movie.imdbId === currentMovie.imdbId);
+
+    if (!isInMovieList) {
+      setMovies(currentMovies => [...currentMovies, currentMovie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +23,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAdd={addFoundMovieToList} />
       </div>
     </div>
   );

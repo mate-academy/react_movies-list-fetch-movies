d2c669290d024df6d9be07b3fac596d0e0c7f0f2
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-08-25 19:51:26 +0300

    initial commit

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+interface Props {
+  addMovie: (arg: Movie) => void;
+}
+
+export const FindMovie: React.FC<Props> = (props) => {
+  const { addMovie } = props;
+
+  const [query, setQuery] = useState('');
+  const [uploadedMovie, setUploadedMovie] = useState<Movie | null>(null);
+  const [movieIsUploaded, setMovieIsUploaded] = useState(false);
+  const [hasError, setHasError] = useState('');
+
+  const findMovie = (event: FormEvent) => {
+    event.preventDefault();
+
+    getMovie(query)
+      .then(response => {
+        if ('Error' in response) {
+          return Promise.reject(response.Error);
+        }
+
+        const {
+          Poster,
+          Title,
+          Plot,
+          imdbID,
+        } = response;
+
+        setUploadedMovie({
+          title: Title,
+          description: Plot,
+          imgUrl: Poster,
+          imdbUrl: `https://m.imdb.com/title/${imdbID}`,
+          imdbId: imdbID,
+        });
+
+        return Promise.resolve();
+      })
+      .catch(error => setHasError(error))
+      .finally(() => setMovieIsUploaded(true));
+  };
+
+  const addMovieToTheCollection = () => {
+    if (uploadedMovie) {
+      addMovie(uploadedMovie);
+    }
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -17,12 +67,16 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={(event) => (setQuery(event.target.value))}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -31,6 +85,8 @@ export const FindMovie: React.FC = () => {
               data-cy="searchButton"
               type="submit"
               className="button is-light"
+              onClick={findMovie}
+              disabled={query.length < 1}
             >
               Find a movie
             </button>
@@ -41,6 +97,8 @@ export const FindMovie: React.FC = () => {
               data-cy="addButton"
               type="button"
               className="button is-primary"
+              onClick={addMovieToTheCollection}
+              disabled={!movieIsUploaded}
             >
               Add to the list
             </button>
@@ -48,10 +106,12 @@ export const FindMovie: React.FC = () => {
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {uploadedMovie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={uploadedMovie} />
+        </div>
+      )}
     </>
   );
 };

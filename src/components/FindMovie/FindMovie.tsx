9c7222cf87d5e446b08871919a08f13018870559
denis-khristyuk <monsieur,apala@gmail.com>
2022-08-28 15:48:01 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import { getMovie } from '../../api';
+import { MovieData } from '../../types/MovieData';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  movies: Movie[];
+  setMovies: (movies: Movie[]) => void;
+  movie: Movie | null;
+  setMovie: (movie: Movie | null) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({
+  movies,
+  setMovies,
+  movie,
+  setMovie,
+}) => {
+  const [inputValue, setInputValue] = useState('');
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const [isErrorShowed, setIsErrorShowed] = useState(false);
+
+  const createMovie = (receivedMovie: MovieData) => {
+    setMovie({
+      title: receivedMovie.Title,
+      description: receivedMovie.Plot,
+      imgUrl: receivedMovie.Poster
+        || 'https://via.placeholder.com/360x270.png?text=no%20preview',
+      imdbUrl: receivedMovie.imdbID,
+      imdbId: receivedMovie.imdbID,
+    });
+  };
+
+  const findMovie = (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+    setIsDataLoading(true);
+    getMovie(inputValue)
+      .then((res) => {
+        if ('Error' in res) {
+          setIsErrorShowed(true);
+        } else {
+          createMovie(res);
+        }
+      })
+      .finally(() => setIsDataLoading(false));
+  };
+
+  const handleMovieAdd = () => {
+    if (movie && !movies.some(item => item.imdbId === movie?.imdbId)) {
+      setMovies([...movies, movie]);
+    }
+
+    setMovie(null);
+    setInputValue('');
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={event => findMovie(event)}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +75,19 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value);
+                setIsErrorShowed(false);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {isErrorShowed && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,7 +95,13 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              disabled={inputValue.trim() === ''}
+              className={classNames(
+                'button is-light',
+                {
+                  'is-loading': isDataLoading,
+                },
+              )}
             >
               Find a movie
             </button>
@@ -41,6 +112,8 @@ export const FindMovie: React.FC = () => {
               data-cy="addButton"
               type="button"
               className="button is-primary"
+              disabled={!movie}
+              onClick={handleMovieAdd}
             >
               Add to the list
             </button>
@@ -50,7 +123,8 @@ export const FindMovie: React.FC = () => {
 
       <div className="container" data-cy="previewContainer">
         <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
+
+        {movie && <MovieCard movie={movie} />}
       </div>
     </>
   );

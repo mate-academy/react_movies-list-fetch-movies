0caa05fa3ffde83f1dda1f7c1094f5d3f582ddb9
Andrew Bessonov <andrewbessik@gmail.com>
2022-08-29 12:57:09 +0300

    Solution 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
@@ -7,11 +7,16 @@ import { Movie } from './types/Movie';
 export const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  const addMovie = (newMovie: Movie): void => {
-    if (movies.every(movie => movie.imdbId !== newMovie.imdbId)) {
-      setMovies(prevMovies => [...prevMovies, newMovie]);
-    }
-  };
+  const addMovie = useCallback(
+    (newMovie: Movie) => {
+      const moviesIncludesInList = movies
+        .some(movieList => movieList.imdbId === newMovie.imdbId);
+
+      if (!moviesIncludesInList) {
+        setMovies(prev => [...prev, newMovie]);
+      }
+    }, [movies],
+  );
 
   return (
     <div className="page">

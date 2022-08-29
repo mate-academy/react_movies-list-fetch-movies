c76eba0742adb8e75a8368e58f56a6ccea65197c
Danil Hodytskyi <danil.hodytskyi1105@gmail.com>
2022-08-29 10:56:48 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    const moviesIncludesInList = movies
+      .some(movieList => movieList.imdbId === newMovie.imdbId);
+
+    if (!moviesIncludesInList) {
+      setMovies(prev => [...prev, newMovie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +23,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovie={addMovie} />
       </div>
     </div>
   );

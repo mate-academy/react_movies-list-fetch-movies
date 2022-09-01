17fb96ad1b919c77761bb8c0f8930c199a2aec3e
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-09-01 14:27:33 +0300

    api link has been formatted to 'https'

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=81ae11e5';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=81ae11e5';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

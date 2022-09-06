2f88745e54ddce9a7cf8b8948b602996aa23d7a2
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-06 23:52:49 +0300

    Added handler for adding new movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { useState } from 'react';
-import './App.scss';
+
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
+
 import { Movie } from './types/Movie';
 
+import './App.scss';
+
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleMovieAdd = (movie: Movie) => {
+    setMovies([...movies, movie]);
+  };
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        <MoviesList
+          movies={movies}
+        />
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          movies={movies}
+          onMovieAdd={handleMovieAdd}
+        />
       </div>
     </div>
   );

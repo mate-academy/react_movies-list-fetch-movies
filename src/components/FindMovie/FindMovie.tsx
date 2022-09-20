1124ada5668b8cf8f14a95932b67bea89def8169
Andriy Nosov <nosovandriy@gmail.com>
2022-09-20 20:41:29 +0300

    Solution git commit -am Solution git commit -am Solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
 
-export const FindMovie: React.FC = () => {
+import { MovieData } from '../../types/MovieData';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard/MovieCard';
+
+type Props = {
+  addNewMovie: (newMovie: MovieData | null) => void,
+};
+
+export const FindMovie: React.FC<Props> = ({ addNewMovie }) => {
+  const [movie, setMovie] = useState<MovieData | null>(null);
+  const [movieTitleQuery, setMovieTitleQuery] = useState('');
+  const [hasLoadingError, setHasLoadingError] = useState(false);
+  const [loadingMovie, setLoadingMovie] = useState(false);
+
+  const handleLoadMovieFromServer = async () => {
+    setLoadingMovie(true);
+
+    try {
+      const newMovie = await getMovie(movieTitleQuery);
+
+      if (!newMovie.imdbID) {
+        setMovie(null);
+        setHasLoadingError(true);
+      } else {
+        setMovie(newMovie);
+        setHasLoadingError(false);
+      }
+    } catch (error) {
+      alert('Error loading data from the server');
+    } finally {
+      setLoadingMovie(false);
+    }
+  };
+
+  const handleFindMovie = (query: string) => {
+    if (query !== movieTitleQuery) {
+      setHasLoadingError(false);
+    }
+
+    setMovieTitleQuery(query);
+  };
+
+  const handleAddNewMovieToTheList = () => {
+    addNewMovie(movie);
+    setMovie(null);
+    setMovieTitleQuery('');
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={event => event.preventDefault()}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,41 +68,60 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={movieTitleQuery}
+              onChange={(event) => handleFindMovie(event.target.value)}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasLoadingError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+
         </div>
 
         <div className="field is-grouped">
           <div className="control">
+
             <button
+              disabled={!movieTitleQuery}
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames(
+                'button is-light',
+                { 'is-loading': loadingMovie },
+              )}
+              onClick={handleLoadMovieFromServer}
             >
-              Find a movie
+              {!movie
+                ? 'Find a movie'
+                : 'Search again'}
             </button>
+
           </div>
 
           <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
+            {movie && (
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAddNewMovieToTheList}
+              >
+                Add to the list
+              </button>
+            )}
           </div>
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
-import { Movie } from '../../types/Movie';
+import { MovieData } from '../../types/MovieData';
 import './MovieCard.scss';
 
 type Props = {
-  movie: Movie,
+  movie: MovieData,
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
-  <div className="card" data-cy="movieCard">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          data-cy="moviePoster"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div>
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img
-              src="images/imdb-logo.jpeg"
-              alt="imdb"
-            />
-          </figure>
-        </div>
-        <div className="media-content">
-          <p className="title is-8" data-cy="movieTitle">{movie.title}</p>
-        </div>
-      </div>
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+  const {
+    Poster,
+    Title,
+    Plot,
+    imdbID,
+  } = movie;
 
-      <div className="content" data-cy="movieDescription">
-        {movie.description}
-        <br />
-        <a href={movie.imdbUrl} data-cy="movieURL">
-          IMDB
-        </a>
+  return (
+    <div className="card" data-cy="movieCard">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            data-cy="moviePoster"
+            src={Poster === 'N/A'
+              ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+              : Poster}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img
+                src="images/imdb-logo.jpeg"
+                alt="imdb"
+              />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-8" data-cy="movieTitle">{Title}</p>
+          </div>
+        </div>
+
+        <div className="content" data-cy="movieDescription">
+          {Plot}
+          <br />
+          <a href={`https://www.imdb.com/title/${imdbID}/`} data-cy="movieURL">
+            IMDB
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};

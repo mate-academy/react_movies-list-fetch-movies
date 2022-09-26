76239f77536cb37d6457aaa059127c9e08b58def
Jamal Amoian <leonidagutin09@gmail.com>
2022-09-26 19:04:23 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (movie: Movie) => {
+    setMovies([...movies, movie]);
+  };
 
   return (
     <div className="page">
@@ -14,7 +18,10 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          addMovie={addMovie}
+          movies={movies}
+        />
       </div>
     </div>
   );

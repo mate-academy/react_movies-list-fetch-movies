965cd6a4e163874cbe78a10c9018a6221b49503e
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-09-29 09:24:47 +0300

    Fetch movies implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const onAddMovie = (movie: Movie) => {
+    const alreadyAdded = movies.find(m => m.imdbId === movie.imdbId);
+
+    if (!alreadyAdded) {
+      setMovies(prev => [...prev, movie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +22,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAddMovie={onAddMovie} />
       </div>
     </div>
   );

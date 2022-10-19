ee495afe185a8eeebd804d19c4a04813424b4472
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-10-19 15:58:47 +0300

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (movie: Movie): void => {
+    const result = !movies.find(mov => mov.imdbId === movie.imdbId);
+
+    if (result) {
+      setMovies([...movies, movie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +22,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovie={addMovie} />
       </div>
     </div>
   );

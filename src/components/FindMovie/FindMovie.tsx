6f613969ec45cf9294c8bc4952de4c1dd604f982
Danil <dasha8854567@gmail.com>
2022-11-01 13:59:57 +0200

    commit1

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  setMovies: (x: Movie[]) => void
+  movies: Movie[]
+};
+
+export const FindMovie: React.FC<Props> = ({ setMovies, movies }) => {
+  const [titleField, setTitleField] = useState('');
+  // eslint-disable-next-line max-len
+  const [movieFromServer, setMovieFromServer] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const [errorSearch, setErrorSearch] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [addToList, setAddToList] = useState(true);
+
   return (
     <>
       <form className="find-movie">
@@ -17,12 +40,19 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={titleField}
+              onChange={(e) => {
+                setErrorSearch(false);
+                setTitleField(e.target.value);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {errorSearch && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,7 +60,41 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames(
+                'button',
+                'is-light',
+                { 'is-loading': isLoading },
+              )}
+              onClick={(e) => {
+                e.preventDefault();
+                setIsLoading(true);
+                getMovie(titleField)
+                  .then(movie => {
+                    if ('Error' in movie) {
+                      setErrorSearch(true);
+                      setIsLoading(false);
+
+                      return Promise.reject();
+                    }
+
+                    setAddToList(false);
+                    setErrorSearch(false);
+                    setIsLoading(false);
+                    setMovieFromServer({
+                      title: movie.Title,
+                      description: movie.Plot,
+                      imgUrl: movie.Poster === 'N/A'
+                        // eslint-disable-next-line max-len
+                        ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+                        : movie.Poster,
+                      imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
+                      imdbId: movie.imdbID,
+                    });
+
+                    return Promise.resolve();
+                  });
+              }}
+              disabled={Boolean(!titleField)}
             >
               Find a movie
             </button>
@@ -40,7 +104,30 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="addButton"
               type="button"
-              className="button is-primary"
+              className={classNames(
+                'button',
+                'is-primary',
+                { 'is-hidden': addToList },
+              )}
+              onClick={(e) => {
+                e.preventDefault();
+                setAddToList(true);
+
+                // eslint-disable-next-line max-len
+                if (!movies.some(movie => movie.title === movieFromServer.title)) {
+                  setMovies([...movies, movieFromServer]);
+                }
+
+                setTitleField('');
+
+                setMovieFromServer({
+                  title: '',
+                  description: '',
+                  imgUrl: '',
+                  imdbUrl: '',
+                  imdbId: '',
+                });
+              }}
             >
               Add to the list
             </button>
@@ -49,8 +136,7 @@ export const FindMovie: React.FC = () => {
       </form>
 
       <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
+        {movieFromServer.title && <MovieCard movie={movieFromServer} />}
       </div>
     </>
   );

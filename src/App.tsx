088c8c91792673fff23bfd6c42e86ea6a163d3fd
Andriy Hladkevych <andriyglad@gmail.com>
2022-11-01 14:28:55 +0200

    Implement Movies list with function to add movie from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isMovieInTheList, setIsMovieInTheList] = useState(false);
+
+  const addMovie = (movie: Movie) => {
+    if (movies.every(movieInList => movieInList.imdbId !== movie.imdbId)) {
+      setMovies(currentMovies => [
+        ...currentMovies,
+        movie,
+      ]);
+    } else {
+      setIsMovieInTheList(true);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +26,10 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          addMovie={addMovie}
+          isMovieInTheList={isMovieInTheList}
+        />
       </div>
     </div>
   );

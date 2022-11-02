e1f81b88c99a7935de1bef3736f083dca5a8655a
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-11-02 19:05:42 +0200

    feat: implemented movies list with add option from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddMovie = useCallback((newMovie: Movie) => {
+    setMovies(currentMovies => [...currentMovies, newMovie]);
+  }, []);
 
   return (
     <div className="page">
@@ -14,7 +18,10 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          movies={movies}
+          addMovie={handleAddMovie}
+        />
       </div>
     </div>
   );

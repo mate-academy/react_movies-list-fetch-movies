42538d3e8b70483e5a443237babb4e495f92476d
bohdan-tron <ikwiczzts@gmail.com>
2022-11-07 12:16:29 +0200

    fix: make check with method some easier to read

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ import { Movie } from './types/Movie';
 export const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const addMovie = (newMovie: Movie) => {
-    const isMovieInList = (movie: Movie) => movie.imdbId === newMovie.imdbId;
+    const isMovieInList = movies
+      .some(movie => movie.imdbId === newMovie.imdbId);
 
-    if (!movies.some(isMovieInList)) {
+    if (!isMovieInList) {
       setMovies(moviesInList => [...moviesInList, newMovie]);
     }
   };

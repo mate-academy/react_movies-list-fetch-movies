d06923759f9293e7c9b6e5ade1f4b61c1285f4fe
Bibicutu <pauleugen@gmail.com>
2022-11-24 23:58:49 +0200

    change http to https

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
 // const API_URL = 'https://www.omdbapi.com/?apikey=9f9df241';
-const API_URL = 'http://www.omdbapi.com/?apikey=9f9df241';
+const API_URL = 'https://www.omdbapi.com/?apikey=9f9df241';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

4617a10e42fd861b05ecc5230a26a2cf11b3334d
Bibicutu <pauleugen@gmail.com>
2022-11-24 23:45:21 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = useCallback((newMovie: Movie) => {
+    if (movies.find(movie => movie.imdbId === newMovie.imdbId) === undefined) {
+      setMovies((previousListOfMovies) => ([
+        ...previousListOfMovies,
+        newMovie,
+      ]));
+    }
+  }, [movies]);
 
   return (
     <div className="page">
@@ -14,7 +23,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovie={addMovie} />
       </div>
     </div>
   );

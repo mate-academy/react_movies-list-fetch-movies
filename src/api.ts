9d7c0099d16565c63413f94966dccf064ba7b1db
Yaroslav Radchuk <105112047+Yaroslav-Radchuk@users.noreply.github.com>
2022-12-03 12:52:11 +0200

    new solution

--- src/api.ts
+++ src/api.ts
@@ -4,9 +4,5 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=fe815c7';
 
 export function getMovie(query: string): Promise<MovieData> {
   return fetch(`${API_URL}&t=${query}`)
-    .then(res => res.json())
-    .catch(() => ({
-      Response: 'False',
-      Error: 'unexpected error',
-    }));
+    .then(res => res.json());
 }

a9746ec43876f8815d791a85a35512f1e8d88265
Denys Topchyi <miliarder97@gmail.com>
2022-12-12 14:44:35 +0200

    first implementation of fetch-movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const uniqMovies = Array.from(new Set(movies));
 
   return (
     <div className="page">
-      <div className="page-content">
-        <MoviesList movies={movies} />
-      </div>
+      {movies && (
+        <div className="page-content">
+          <MoviesList movies={uniqMovies} />
+        </div>
+      )}
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          setMovies={setMovies}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import classnames from 'classnames';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  setMovies: (value: any) => void,
+};
+
+
+export const FindMovie: React.FC<Props> = ({ setMovies }) => {
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState<Movie | undefined>();
+  const [isFailed, setIsFailed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const defaultPicture
+    = 'https://via.placeholder.com/360x270.png?text=no%20preview';
+
+  const addMovieOnClick = () => {
+    setMovies((prevArray: Movie[]) => {
+      const uniqMovieStatus = [...prevArray].some(
+        item => item.imdbId === search?.imdbId,
+      );
+
+      if (uniqMovieStatus) {
+        return prevArray;
+      }
+
+      return [...prevArray, search];
+    });
+
+    setSearch(undefined);
+    setIsLoading(false);
+    setIsFailed(false);
+  };
+
+  const loadMovie = async () => {
+    setIsLoading(true);
+    const movie = await getMovie(query);
+
+    if ('Error' in movie) {
+      setIsFailed(true);
+    } else {
+      setSearch({
+        title: movie.Title,
+        description: movie.Plot,
+        imgUrl: movie.Poster === 'N/A' ? defaultPicture : movie.Poster,
+        imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
+        imdbId: movie.imdbID,
+      });
+
+      setIsFailed(false);
+    }
+
+    setQuery('');
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={(event) => {
+          event.preventDefault();
+          loadMovie();
+        }}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +79,17 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {isFailed && query === '' && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +97,39 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classnames({
+                button: true,
+                'is-light': true,
+                'is-loading': isLoading && !search && !isFailed,
+              })}
+              disabled={query === ''}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {search && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={addMovieOnClick}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
+
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {search && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={search} />
+        </div>
+      )}
     </>
   );
 };

86e336c5fcbb62b6c15ff58a89c8e8fa22c313ba
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-12-21 15:17:00 +0800

    last version

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5bd1467c';
+const API_URL = 'https://www.omdbapi.com/?apikey=5bd1467c';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

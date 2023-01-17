598c124b7acfaf35bf110512e2dafd8f9b75dbb7
Nazar Brunarskyi <3nnazar836@gmail.com>
2023-01-17 13:43:48 +0200

    added optimization

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { FC, memo, useState } from 'react';
 import { getMovie } from '../../api';
 import { Movie } from '../../types/Movie';
 import { MovieData } from '../../types/MovieData';
@@ -9,148 +9,152 @@ interface Props {
   onAdd: React.Dispatch<React.SetStateAction<Movie[]>>,
 }
 
-export const FindMovie: React.FC<Props> = ({ onAdd }) => {
-  const [title, setTitle] = useState('');
-  const [movie, setMovie] = useState<Movie | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+export const FindMovie: FC<Props> = memo(
+  ({ onAdd }) => {
+    const [title, setTitle] = useState('');
+    const [movie, setMovie] = useState<Movie | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-  const handleGetMovie = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    event.preventDefault();
-    setIsLoading(true);
+    const handleGetMovie = (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    ) => {
+      event.preventDefault();
+      setIsLoading(true);
 
-    getMovie(title)
-      .then(response => {
-        if ('Error' in response) {
-          throw new Error('error');
-        }
-
-        const {
-          Poster,
-          Title,
-          Plot,
-          imdbID,
-        } = response as MovieData;
-
-        const posterForMovie = Poster === 'N/A'
-          ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
-          : Poster;
-
-        setMovie({
-          title: Title,
-          description: Plot,
-          imgUrl: posterForMovie,
-          imdbUrl: `https://www.imdb.com/title/${imdbID}`,
-          imdbId: imdbID,
-        });
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => setIsLoading(false));
-  };
-
-  const handleMovieAdding = () => {
-    onAdd((movies) => {
-      if (!movie) {
-        return movies;
-      }
-
-      const movieToAdd = {
-        ...movie,
-      };
-
-      setTitle('');
-      setMovie(null);
-
-      const isMovieAdded = movies.some((film => film.imdbId === movie.imdbId));
-
-      if (isMovieAdded) {
-        return movies;
-      }
-
-      return [
-        ...movies,
-        movieToAdd,
-      ];
-    });
-  };
-
-  const handleInputOfData = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.currentTarget.value);
-    setError(false);
-  };
-
-  return (
-    <>
-      <form className="find-movie">
-        <div className="field">
-          <label className="label" htmlFor="movie-title">
-            Movie title
-          </label>
-
-          <div className="control">
-            <input
-              data-cy="titleField"
-              type="text"
-              id="movie-title"
-              placeholder="Enter a title to search"
-              className="input is-dander"
-              value={title}
-              onChange={handleInputOfData}
-            />
-          </div>
-
-          {
-            error && (
-              <p className="help is-danger" data-cy="errorMessage">
-                Can&apos;t find a movie with such a title
-              </p>
-            )
+      getMovie(title)
+        .then(response => {
+          if ('Error' in response) {
+            throw new Error('error');
           }
 
-        </div>
+          const {
+            Poster,
+            Title,
+            Plot,
+            imdbID,
+          } = response as MovieData;
 
-        <div className="field is-grouped">
-          <div className="control">
-            <button
-              data-cy="searchButton"
-              type="submit"
-              className={`button is-light ${isLoading && 'is-loading'}`}
-              disabled={!title}
-              onClick={handleGetMovie}
-            >
-              {movie ? 'Search Again' : 'Find a movie'}
-            </button>
-          </div>
+          const posterForMovie = Poster === 'N/A'
+            ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+            : Poster;
 
-          <div className="control">
+          setMovie({
+            title: Title,
+            description: Plot,
+            imgUrl: posterForMovie,
+            imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+            imdbId: imdbID,
+          });
+        })
+        .catch(() => {
+          setError(true);
+        })
+        .finally(() => setIsLoading(false));
+    };
+
+    const handleMovieAdding = () => {
+      onAdd((movies) => {
+        if (!movie) {
+          return movies;
+        }
+
+        const movieToAdd = {
+          ...movie,
+        };
+
+        setTitle('');
+        setMovie(null);
+
+        const isMovieAdded = movies.some((film => (
+          film.imdbId === movie.imdbId
+        )));
+
+        if (isMovieAdded) {
+          return movies;
+        }
+
+        return [
+          ...movies,
+          movieToAdd,
+        ];
+      });
+    };
+
+    const handleInputOfData = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTitle(event.currentTarget.value);
+      setError(false);
+    };
+
+    return (
+      <>
+        <form className="find-movie">
+          <div className="field">
+            <label className="label" htmlFor="movie-title">
+              Movie title
+            </label>
+
+            <div className="control">
+              <input
+                data-cy="titleField"
+                type="text"
+                id="movie-title"
+                placeholder="Enter a title to search"
+                className="input is-dander"
+                value={title}
+                onChange={handleInputOfData}
+              />
+            </div>
+
             {
-              movie && (
-                <button
-                  data-cy="addButton"
-                  type="button"
-                  className="button is-primary"
-                  onClick={handleMovieAdding}
-                >
-                  Add to the list
-                </button>
+              error && (
+                <p className="help is-danger" data-cy="errorMessage">
+                  Can&apos;t find a movie with such a title
+                </p>
               )
             }
-          </div>
-        </div>
-      </form>
 
-      {
-        movie && (
-          <div className="container" data-cy="previewContainer">
-            <h2 className="title">Preview</h2>
-            <MovieCard movie={movie} />
           </div>
-        )
-      }
-    </>
-  );
-};
+
+          <div className="field is-grouped">
+            <div className="control">
+              <button
+                data-cy="searchButton"
+                type="submit"
+                className={`button is-light ${isLoading && 'is-loading'}`}
+                disabled={!title}
+                onClick={handleGetMovie}
+              >
+                {movie ? 'Search Again' : 'Find a movie'}
+              </button>
+            </div>
+
+            <div className="control">
+              {
+                movie && (
+                  <button
+                    data-cy="addButton"
+                    type="button"
+                    className="button is-primary"
+                    onClick={handleMovieAdding}
+                  >
+                    Add to the list
+                  </button>
+                )
+              }
+            </div>
+          </div>
+        </form>
+
+        {
+          movie && (
+            <div className="container" data-cy="previewContainer">
+              <h2 className="title">Preview</h2>
+              <MovieCard movie={movie} />
+            </div>
+          )
+        }
+      </>
+    );
+  },
+);

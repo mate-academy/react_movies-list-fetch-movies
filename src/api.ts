5807746e5e78b2b2da5b1f9e9fd5eea4a8eff9a8
Anatolii <batstolya@gmail.com>
2023-01-20 18:05:52 +0200

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,10 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = ' http://www.omdbapi.com/?i=tt3896198&apikey=e48398ea';
-
-function sleeper(ms:number) {
-  return (x: Response): Promise<Response> => {
-    return new Promise(resolve => setTimeout(() => resolve(x), ms));
-  };
-}
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e48398ea';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
-    .then(sleeper(500))
     .then(res => res.json())
     .catch(() => ({
       Response: 'False',

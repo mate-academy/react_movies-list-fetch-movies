80bfcb02a3b50d2efe942f2945121e7e369bb825
John Rust <johnrust12@gmail.com>
2023-01-29 10:06:37 +0200

    Solution 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from 'react';
+import { FC } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
-import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies] = useState<Movie[]>([]);
-
-  return (
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={movies} />
-      </div>
-
-      <div className="sidebar">
-        <FindMovie />
-      </div>
+export const App: FC = () => (
+  <div className="page">
+    <div className="page-content">
+      <MoviesList />
     </div>
-  );
-};
+
+    <div className="sidebar">
+      <FindMovie />
+    </div>
+  </div>
+);
--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,13 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=261e0227';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())
-    .catch(() => ({
+    .catch((error) => ({
       Response: 'False',
-      Error: 'unexpected error',
+      Error: error,
     }));
 }

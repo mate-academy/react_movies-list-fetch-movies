467f73cc3346da1340d2923fa8ba216c92e60ec4
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2023-02-06 13:56:26 +0200

    fetch movies done

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,9 @@ import { ResponseError } from './types/ReponseError';
 
 const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
 
+export const placeholder
+= 'https://via.placeholder.com/360x270.png?text=no%20preview';
+
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())

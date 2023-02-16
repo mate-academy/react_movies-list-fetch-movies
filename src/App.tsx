53a0541eebf9afe003ec1d932d550733dbfdfe93
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-02-16 17:50:02 +0200

    Task done. Have a good time!)

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const addMovies = (movie: Movie) => {
+    if (movies.find((currentMovie => currentMovie.imdbId === movie.imdbId))) {
+      return;
+    }
+
+    setMovies((currentMuvies) => [...currentMuvies, movie]);
+  };
 
   return (
     <div className="page">
       <div className="page-content">
         <MoviesList movies={movies} />
       </div>
-
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovies={addMovies} />
       </div>
     </div>
   );

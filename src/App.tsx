808ed43fb5758d7b9765119193050e495938ae58
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-02-18 16:21:43 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,14 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const addMovie = (newMovie: Movie) => {
+    if (!movies.some(movie => movie.imdbId === newMovie.imdbId)) {
+      setMovies(currentMovies => [...currentMovies, newMovie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +21,11 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          query={searchQuery}
+          setSearchQuery={setSearchQuery}
+          addMovie={addMovie}
+        />
       </div>
     </div>
   );

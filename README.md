293da875386e886febfde3a99a892260cc7931d2
svitjojo <kornotavr@gmail.com>
2023-02-18 15:58:26 +0200

    Result

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ Implement the `FindMovie` component to load movies from [OMDb API](http://www.om
 1. When a user enters a title and submits the form, send a request to `https://www.omdbapi.com/?apikey=[yourkey]&t=[title]`;
     - use the `getMovie` method from the `api.ts`;
 1. The submit button should be disabled when the title field is empty;
-1. Show a spinner on the submit button while waiting for the respose;
+1. Show a spinner on the submit button while waiting for the response;
     - use `is-loading` class;
     - loading should be finished in any case (use `finally`);
 1. If a movie is not found show an error message below the input;
     - hide it after changing the title;
 1. If a movie is found show the preview as a `MovieCard` and the add button;
-    - the API return `MovieData` or `ReposnseError` (see the `types`)
+    - the API return `MovieData` or `ResponseError` (see the `types`)
     - don't forget to normalize received `MovieData`
     - use [the deafult picture](https://via.placeholder.com/360x270.png?text=no%20preview) if the found movie has no poster.
 1. The add button should **add** the movie to the list, **clear** the form and **remove** the preview;
@@ -24,4 +24,4 @@ Implement the `FindMovie` component to load movies from [OMDb API](http://www.om
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Use the [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_movies-list-fetch-movies/) and add it to the PR description.
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://svitjojo.github.io/react_movies-list-fetch-movies/) and add it to the PR description.
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import cn from 'classnames';
 import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
+import { getNormalizeMovie } from '../../utils/getNormalizeMovie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  addMovie: (movie: Movie) => void
+};
+
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const movieFromServer = await getMovie(query);
+
+      if ('Error' in movieFromServer) {
+        setHasError(true);
+        setIsLoading(false);
+
+        return;
+      }
+
+      setMovie(getNormalizeMovie(movieFromServer));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const reset = useCallback(() => {
+    setMovie(null);
+    setQuery('');
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    if (movie) {
+      addMovie(movie);
+      reset();
+    }
+  }, [movie]);
+
+  const handleInput = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(event.target.value);
+      setHasError(false);
+    }, [],
+  );
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSearch}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +70,16 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={handleInput}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +87,36 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={cn('button is-light', {
+                'is-loading': isLoading,
+              })}
+              disabled={!query}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAdd}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

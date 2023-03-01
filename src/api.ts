214d6a96098e4909a8d3dc0cff3587dcd71283d7
Deniskakaka <krollik1000000@gmail.com>
2023-03-01 15:56:35 +0200

    solution

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+// eslint-disable-next-line max-len
+const API_URL = 'https://www.omdbapi.com/?apikey=b5e87ca0';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

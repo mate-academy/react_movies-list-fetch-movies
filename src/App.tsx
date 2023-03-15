b482217d10cf6545af5fcb3ed0f466c68e7149f1
ivanm2706 <ivanmorhun664@gmail.com>
2023-03-15 16:31:28 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
 import { useState } from 'react';
+import { getMovie } from './api';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const error = useState<boolean>(false);
+  const [, setIsErrorMessage] = error;
+  const preview = useState<boolean>(false);
+  const [, setIsPreview] = preview;
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [previewMovie, setPreviewMovie] = useState<Movie | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const findMoviePreview = (value: string) => {
+    setIsLoading(true);
+    getMovie(value)
+      .then((movie) => {
+        if ('Error' in movie) {
+          throw new Error(movie.Error);
+        }
+
+        setPreviewMovie({
+          title: movie.Title,
+          description: movie.Plot,
+          imgUrl: movie.Poster === 'N/A'
+            ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+            : movie.Poster,
+          imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
+          imdbId: movie.imdbID,
+        });
+        setIsPreview(true);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsPreview(false);
+        setIsErrorMessage(true);
+        setIsLoading(false);
+      });
+  };
+
+  const addMovieToList = () => {
+    if (
+      previewMovie
+      && movies.findIndex(({ imdbId }) => imdbId === previewMovie.imdbId) < 0
+    ) {
+      setMovies(state => [...state, previewMovie]);
+    }
+
+    setPreviewMovie(null);
+  };
 
   return (
     <div className="page">
@@ -14,7 +60,14 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          movie={previewMovie}
+          isLoading={isLoading}
+          onFindMovie={findMoviePreview}
+          addMovie={addMovieToList}
+          error={error}
+          preview={preview}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import { FC, useState } from 'react';
+import classNames from 'classnames';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './FindMovie.scss';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  error: [boolean, React.Dispatch<React.SetStateAction<boolean>>],
+  preview: [boolean, React.Dispatch<React.SetStateAction<boolean>>],
+  movie: Movie | null,
+  isLoading: boolean,
+  onFindMovie: (value: string) => void,
+  addMovie: () => void,
+};
+
+export const FindMovie: FC<Props> = ({
+  error,
+  preview,
+  movie,
+  isLoading,
+  onFindMovie,
+  addMovie,
+}) => {
+  const [isPreview, removePreview] = preview;
+  const [isErrorMessage, setErrorMessage] = error;
+  const [valueInput, setValueInput] = useState<string>('');
+
+  const hendlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.currentTarget.value);
+    setErrorMessage(false);
+  };
+
+  const hendlerFindMovie = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    onFindMovie(valueInput);
+  };
+
+  const addMovieToList = () => {
+    setValueInput('');
+    addMovie();
+    removePreview(false);
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -17,12 +58,19 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={valueInput}
+              onChange={hendlerInput}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {isErrorMessage && (
+            <p
+              className="help is-danger"
+              data-cy="errorMessage"
+            >
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +78,40 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames(
+                'button',
+                'is-light',
+                { 'is-loading': isLoading },
+              )}
+              disabled={valueInput === ''}
+              onClick={hendlerFindMovie}
             >
-              Find a movie
+              {!movie ? 'Find a movie' : 'Search again'}
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {isPreview && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={addMovieToList}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
+
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {isPreview && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie || null} />
+        </div>
+      )}
     </>
   );
 };

fd3d423dadb7227ba0eb309b4bc163bf55d50dbc
Skuratovskyi Vlad <vladskur0369@gmail.com>
2023-03-28 14:26:22 +0900

    Solution v0.2

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=d50d4540';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=d50d4540';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

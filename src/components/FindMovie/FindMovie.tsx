0d1d3e85a44e8ae7eefbf8391fafb7bda00b9b25
Sercan Simsek <sercansimsek44@gmail.com>
2023-03-31 13:37:23 +0200

    Solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
+import classNames from 'classnames';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieData } from '../../types/MovieData';
+import { getMovie } from '../../api';
 
-export const FindMovie: React.FC = () => {
+import { MovieCard } from '../MovieCard';
+
+type Props = {
+  addMovie: (movie: Movie) => void,
+};
+
+export const FindMovie: React.FC<Props> = ({ addMovie }) => {
+  const [query, setQuery] = useState('');
+  const [searcResult, setSearcResult] = useState<MovieData | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    getMovie(query)
+      .then((response) => {
+        if ((response as MovieData).Title) {
+          setSearcResult(response as MovieData);
+          setIsSubmit(true);
+
+          return;
+        }
+
+        setHasError(true);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleClear = () => {
+    setQuery('');
+    setHasError(false);
+    setIsSubmit(false);
+    setSearcResult(null);
+  };
+
+  const handleQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setHasError(false);
+    setIsSubmit(false);
+    setSearcResult(null);
+  };
+
+  const getNewMovie = useCallback((newMovie) => {
+    return {
+      title: newMovie.Title,
+      description: newMovie.Plot,
+      imdbId: newMovie.imdbID,
+      imgUrl: newMovie.Poster !== 'N/A'
+        ? newMovie.Poster
+        : 'https://via.placeholder.com/360x270.png?text=no%20preview',
+      imdbUrl: `https://www.imdb.com/title/${newMovie.imdbID}`,
+    };
+  }, [searcResult]);
+
+  const handleAddButton = () => {
+    addMovie(getNewMovie(searcResult));
+    handleClear();
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={onSubmit}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -17,12 +85,16 @@ export const FindMovie: React.FC = () => {
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={handleQuery}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              {'Can\'t find a movie with such a title'}
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +102,39 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames(
+                'button is-light',
+                { 'is-loading': isLoading },
+              )}
+              disabled={!query}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {searcResult && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAddButton}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
+
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {isSubmit && !isLoading && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={getNewMovie(searcResult)} />
+        </div>
+      )}
+
     </>
   );
 };

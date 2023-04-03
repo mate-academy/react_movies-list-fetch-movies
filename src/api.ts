3f8b865e43a9df231235667ce62099350af7df98
Sercan Simsek <sercansimsek44@gmail.com>
2023-04-03 15:35:05 +0200

    mixed content issue fixed

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=19dea528';
+const API_URL = 'https://www.omdbapi.com/?apikey=19dea528';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

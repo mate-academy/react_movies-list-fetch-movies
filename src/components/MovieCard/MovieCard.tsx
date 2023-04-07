82303cfc6b6e7a304a3beea306694c28c44b7fa5
Oleksii-Mishchenko <alex.mishch@gmail.com>
2023-04-07 16:11:46 +0300

    Movies List Fetch fix

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -7,40 +7,47 @@ type Props = {
 };
 
 export const MovieCard: React.FC<Props> = React.memo(
-  ({ movie }) => (
-    <div className="card" data-cy="movieCard">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            data-cy="moviePoster"
-            src={movie.imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img
-                src="images/imdb-logo.jpeg"
-                alt="imdb"
-              />
-            </figure>
-          </div>
-          <div className="media-content">
-            <p className="title is-8" data-cy="movieTitle">{movie.title}</p>
-          </div>
+  ({ movie }) => {
+    const {
+      imdbUrl, imgUrl, title, description,
+    } = movie;
+
+    return (
+      <div className="card" data-cy="movieCard">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              data-cy="moviePoster"
+              src={imgUrl}
+              alt="Film logo"
+            />
+          </figure>
         </div>
 
-        <div className="content" data-cy="movieDescription">
-          {movie.description}
-          <br />
-          <a href={movie.imdbUrl} data-cy="movieURL">
-            IMDB
-          </a>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img
+                  src="images/imdb-logo.jpeg"
+                  alt="imdb"
+                />
+              </figure>
+            </div>
+            <div className="media-content">
+              <p className="title is-8" data-cy="movieTitle">{title}</p>
+            </div>
+          </div>
+
+          <div className="content" data-cy="movieDescription">
+            {description}
+            <br />
+            <a href={imdbUrl} data-cy="movieURL">
+              IMDB
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  ),
+    );
+  },
 );

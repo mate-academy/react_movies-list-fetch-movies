e41b4f79735b50dd1024c41292bbdf1541152885
Serhii Nikolin <sergeynikolin@gmail.com>
2023-04-26 18:59:11 +0300

    Ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,78 @@
-import { useState } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
+import { MovieData } from './types/MovieData';
+// import { ResponseError } from './types/ReponseError';
+
+import { getMovie } from './api';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [query, setQuery] = useState('');
+  const [submitedQuery, setSubmitedQuery] = useState('');
+  const [findedMovie, setFindedMovie] = useState<MovieData | null>(null);
+  const [movieNotFounded, setMovieNotFounded] = useState(false);
+
+  const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    setQuery(e.currentTarget.value);
+  };
+
+  const toFindMovie = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const movie = await getMovie(query) as MovieData;
+
+    if (movie.Title) {
+      setFindedMovie(movie);
+      setMovieNotFounded(false);
+      setSubmitedQuery(query);
+    } else {
+      setMovieNotFounded(true);
+    }
+  };
+
+  const movie = useMemo(() => {
+    if (!findedMovie) {
+      return null;
+    }
+
+    return {
+      title: findedMovie.Title,
+      description: findedMovie.Plot,
+      imgUrl: findedMovie.Poster,
+      imdbUrl: `https://www.imdb.com/title/${findedMovie.imdbID}`,
+      imdbId: findedMovie.imdbID,
+    };
+  }, [submitedQuery, query]);
+
+  const addMovie = () => {
+    if (movie) {
+      const isMoviePresent = movies.find(
+        currMovie => currMovie.imdbId === movie.imdbId,
+      );
+
+      if (!isMoviePresent) {
+        setMovies(currList => {
+          return [
+            ...currList,
+            movie,
+          ];
+        });
+      }
+
+      setFindedMovie(null);
+      setQuery('');
+      setSubmitedQuery('');
+    }
+  };
+
+  useEffect(() => {
+    setFindedMovie(null);
+    setMovieNotFounded(false);
+    setSubmitedQuery('');
+  }, [query]);
 
   return (
     <div className="page">
@@ -14,7 +81,14 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          query={query}
+          changeHandler={changeHandler}
+          movie={movie}
+          toFindMovie={toFindMovie}
+          movieNotFounded={movieNotFounded}
+          addMovie={addMovie}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  query: string;
+  changeHandler: (e: React.FormEvent<HTMLInputElement>) => void;
+  movie: Movie | null;
+  toFindMovie: (e: React.FormEvent<HTMLFormElement>) => void;
+  movieNotFounded: boolean;
+  addMovie: () => void;
+};
+
+export const FindMovie: React.FC<Props> = React.memo(({
+  query, changeHandler, movie, toFindMovie, movieNotFounded, addMovie,
+}) => {
+  const [isLoading, setLoading] = useState(false);
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    await toFindMovie(e);
+    setLoading(false);
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={submitHandler}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -12,17 +33,22 @@ export const FindMovie: React.FC = () => {
 
           <div className="control">
             <input
+              name="input"
               data-cy="titleField"
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
               className="input is-dander"
+              value={query}
+              onChange={changeHandler}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {movieNotFounded && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +56,39 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames('button is-light', {
+                'is-loading': isLoading === true,
+              })}
+              // eslint-disable-next-line no-unneeded-ternary
+              disabled={query === '' ? true : false}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {!!movie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={addMovie}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
       <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
+        {!!movie && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard movie={movie} />
+          </>
+        )}
       </div>
     </>
   );
-};
+});

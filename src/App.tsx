d0037a57feaf6b9f19c59ee86c33888986383170
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-05-09 20:00:18 +0300

    created movies list fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    const isMovieAdded = movies.find(
+      currentMovie => currentMovie.imdbId === newMovie.imdbId,
+    );
+
+    if (!isMovieAdded) {
+      setMovies([
+        ...movies,
+        newMovie,
+      ]);
+    }
+
+    return movies;
+  };
 
   return (
     <div className="page">
@@ -14,7 +29,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAddMovie={addMovie} />
       </div>
     </div>
   );

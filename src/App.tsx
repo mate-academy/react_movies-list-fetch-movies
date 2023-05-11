4dbab481cbd40f4cb8a90f8ce0c31d67290a01c0
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-05-11 15:28:36 +0300

    created movie list

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddMovie = (movie: Movie) => {
+    setMovies((prevMovies: Movie[]) => {
+      if (prevMovies.find((prevMovie) => prevMovie.imdbId === movie.imdbId)) {
+        return prevMovies;
+      }
+
+      return [...prevMovies, movie];
+    });
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onMovieAdd={handleAddMovie} />
       </div>
     </div>
   );

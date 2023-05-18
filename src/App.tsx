e961bfeff3ec523b1c00826fb5b83afc1e8ecc22
Mys Yaroslav <gotluck13@gmail.com>
2023-05-18 12:11:31 +0300

    corrected solution v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import './App.scss';
 import { FindMovie } from './components/FindMovie';
 import { MoviesList } from './components/MoviesList';
@@ -7,9 +7,9 @@ import { Movie } from './types/Movie';
 export const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  const handleSetMovies = (movie: Movie) => {
+  const handleSetMovies = useCallback((movie: Movie) => {
     setMovies((prevMovies) => [...prevMovies, movie]);
-  };
+  }, []);
 
   return (
     <div className="page">

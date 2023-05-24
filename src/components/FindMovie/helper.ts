f29d6bbad14c87a24783182c7e5f3f08d143645c
Zibi95 <sylwester.sob95@gmail.com>
2023-05-24 18:01:56 +0200

    helper function that normalize movie data

---
+++ src/components/FindMovie/helper.ts
@@ -0,0 +1,22 @@
+import { MovieData } from '../../types/MovieData';
+
+export const normalizeMovieData = (movieData: MovieData) => {
+  const {
+    Title,
+    Plot,
+    imdbID,
+    Poster,
+  } = movieData;
+
+  const poster = Poster === 'N/A'
+    ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+    : Poster;
+
+  return {
+    title: Title,
+    description: Plot,
+    imdbId: imdbID,
+    imgUrl: poster,
+    imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+  };
+};

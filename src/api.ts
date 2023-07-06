af415ba04fa498798499caa7478c0998f0d0710d
Oleksandr Plias <pls777@ukr.net>
2023-07-06 16:50:32 +0300

    Solution

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
+import { Movie } from './types/Movie';
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=c6bc86d1';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
@@ -11,3 +12,22 @@ export function getMovie(query: string): Promise<MovieData | ResponseError> {
       Error: 'unexpected error',
     }));
 }
+
+export const expeсtedMovie = (movieData: MovieData): Movie => {
+  const {
+    Title,
+    Plot,
+    Poster,
+    imdbID,
+  } = movieData;
+
+  return {
+    title: Title,
+    description: Plot,
+    imgUrl: Poster === 'N/A'
+      ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+      : movieData.Poster,
+    imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+    imdbId: imdbID,
+  };
+};

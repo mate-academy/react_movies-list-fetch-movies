fadf8f59461cdcd50bf676ca12797282ec2ad2b4
Yura Enttsel <equikmla@gmail.com>
2023-07-17 09:47:06 +0300

    Fixed API URL

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=4a1eb9ef';
+const API_KEY = '4a1eb9ef';
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

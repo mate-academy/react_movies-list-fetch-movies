b6d6f14b3e449b63a59b662b1d52b6548eeaa382
artkas03 <zhonayer2017@gmail.com>
2023-07-20 15:29:06 +0300

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleOnAdd = (newMovie: Movie) => {
+    const isMovieInList = movies.some(movie => {
+      return movie.imdbId === newMovie.imdbId;
+    });
+
+    if (isMovieInList) {
+      return;
+    }
+
+    setMovies([
+      ...movies,
+      newMovie,
+    ]);
+  };
 
   return (
     <div className="page">
@@ -14,7 +29,9 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          onAdd={handleOnAdd}
+        />
       </div>
     </div>
   );

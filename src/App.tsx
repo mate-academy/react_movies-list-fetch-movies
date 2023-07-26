8a2af450197a4f786f76a10a991a900737985ba2
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-07-26 20:28:11 +0300

    search and add movies to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleOnAdd = useCallback((newMovie: Movie) => {
+    const isMovieInList = movies.some(movie => {
+      return movie.imdbId === newMovie.imdbId;
+    });
+
+    if (isMovieInList) {
+      return;
+    }
+
+    setMovies([
+      ...movies,
+      newMovie,
+    ]);
+  }, [movies]);
 
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={movies} />
+        <MoviesList
+          movies={movies}
+        />
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          onAdd={handleOnAdd}
+        />
       </div>
     </div>
   );

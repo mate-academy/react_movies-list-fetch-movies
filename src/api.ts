dfb1d960ae627b943bffb3de45e193675e9401c9
Gabriella Ivanjuk <gabriella.ivanjuk@gmail.com>
2023-07-28 20:27:16 +0300

    solution

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,35 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
+import { Movie } from './types/Movie';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?apikey=5f7fced8';
 
-export function getMovie(query: string): Promise<MovieData | ResponseError> {
+function fetchMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
     .then(res => res.json())
-    .catch(() => ({
-      Response: 'False',
-      Error: 'unexpected error',
-    }));
+    .catch((error: ResponseError) => error);
+}
+
+export async function getMovie(query: string) {
+  const movieFromServer = await fetchMovie(query);
+
+  if ('imdbID' in movieFromServer) {
+    const url
+    = `https://www.imdb.com/title/${movieFromServer.imdbID}`;
+    const img = movieFromServer.Poster === 'N/A'
+      ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+      : movieFromServer.Poster;
+
+    const makeMovie: Movie = {
+      title: movieFromServer.Title,
+      description: movieFromServer.Plot,
+      imgUrl: img,
+      imdbUrl: url,
+      imdbId: movieFromServer.imdbID,
+    };
+
+    return makeMovie;
+  }
+
+  throw new Error(movieFromServer.Error);
 }

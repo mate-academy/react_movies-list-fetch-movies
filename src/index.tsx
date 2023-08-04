fd9324afdd2ca7d1cf2a58e8c91b1637e3ab57cb
DefaultGitUser <usingforgit@gmail.com>
2023-08-04 18:06:07 +0300

    Sol

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+import dotenv from 'dotenv';
 import { App } from './App';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));

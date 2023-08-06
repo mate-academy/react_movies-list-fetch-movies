01db78391564711b5afa931e57cf49df1d18efcb
AndruhaMan <andreysut12@gmail.com>
2023-08-06 23:48:37 +0300

    The task is completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   return (
     <div className="page">
@@ -14,7 +14,17 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          onMovieAdd={(movie) => {
+            const haveThisMovieInList = movies.some(movieInList => (
+              movieInList.imdbId === movie.imdbId
+            ));
+
+            if (!haveThisMovieInList) {
+              setMovies([...movies, movie]);
+            }
+          }}
+        />
       </div>
     </div>
   );

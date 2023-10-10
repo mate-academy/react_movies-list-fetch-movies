843cab340c70411eb39c6d4db3fec9745afd4d6a
Sergey Arnautov <sirius9312@ukr.net>
2023-10-10 10:30:11 +0300

    react movies task

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  movies: Movie[],
+  addMovie: (movies: Movie[]) => void,
+};
+
+export const FindMovie: React.FC<Props> = ({ movies, addMovie }) => {
+  const [titleMovie, setTitleMovie] = useState('');
+  const [errorMessageMovies, setErrorMessageMovies] = useState('');
+  const [submitSpiner, setSubmitSpiner] = useState(false);
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setSubmitSpiner(true);
+    getMovie(titleMovie).then(response => {
+      if ('Error' in response) {
+        // eslint-disable-next-line
+        throw 'Error from server!';
+      }
+
+      setMovie({
+        title: response.Title,
+        description: response.Plot,
+        imgUrl: response.Poster === 'N/A'
+          ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+          : response.Poster,
+        imdbUrl: `https://www.imdb.com/title/${response.imdbID}`,
+        imdbId: response.imdbID,
+      });
+    })
+      .catch(() => setErrorMessageMovies('Can\'t find a movie with such a title'))
+      .finally(() => setSubmitSpiner(false));
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleMovie(e.target.value);
+    setErrorMessageMovies('');
+  };
+
+  const handleAdd = (mov: Movie) => {
+    if (movies.some((movi) => movi.title === mov.title)) {
+      setMovie(null);
+    } else {
+      addMovie([...movies, mov]);
+      setMovie(null);
+    }
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={handleSubmit}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -15,14 +69,21 @@ export const FindMovie: React.FC = () => {
               data-cy="titleField"
               type="text"
               id="movie-title"
+              name="t"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className="input"
+              value={titleMovie}
+              onChange={handleChange}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {
+            errorMessageMovies && (
+              <p className="help is-danger" data-cy="errorMessage">
+                {errorMessageMovies}
+              </p>
+            )
+          }
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +91,39 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={submitSpiner ? 'button is-light is-loading'
+                : 'button is-light'}
+              disabled={
+                !titleMovie.trim()
+              }
             >
-              Find a movie
+              {movie ? 'Search again' : 'Find a movie'}
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {
+            movie && (
+              <div className="control">
+                <button
+                  data-cy="addButton"
+                  type="button"
+                  className="button is-primary"
+                  onClick={() => handleAdd(movie)}
+                >
+                  Add to the list
+                </button>
+              </div>
+            )
+          }
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

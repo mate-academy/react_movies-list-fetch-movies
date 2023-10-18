537172e6de510ec684a9467595ff65e0ebcfab15
Victor Hrygorash <viktor12041991@i.ua>
2023-10-18 17:49:51 +0300

    solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -3,32 +3,27 @@ import { Movie } from '../../types/Movie';
 import './MovieCard.scss';
 
 type Props = {
-  movie: Movie,
+  movie: Movie;
 };
 
 export const MovieCard: React.FC<Props> = ({ movie }) => (
   <div className="card" data-cy="movieCard">
     <div className="card-image">
       <figure className="image is-4by3">
-        <img
-          data-cy="moviePoster"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
+        <img data-cy="moviePoster" src={movie.imgUrl} alt="Film logo" />
       </figure>
     </div>
     <div className="card-content">
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img
-              src="images/imdb-logo.jpeg"
-              alt="imdb"
-            />
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
         <div className="media-content">
-          <p className="title is-8" data-cy="movieTitle">{movie.title}</p>
+          <p className="title is-8" data-cy="movieTitle">
+            {movie.title}
+          </p>
         </div>
       </div>
 

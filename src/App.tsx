524779f290a8974b57d21fa3ede52043c4251679
domkab <dominykas.bobinas@gmail.com>
2023-10-25 12:04:29 +0300

    error still persisting, with logs

--- src/App.tsx
+++ src/App.tsx
@@ -5,79 +5,11 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
-import { getMovie } from './api';
-import { normalizeMovieData } from './_utils/normalizeMovieData';
-import { isResponseError } from './_utils/isResponseError';
 
 export const App: React.FC = () => {
   console.log('rendering App');
 
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [currentSearchTerm, setSearchTerm] = useState<string>('');
-  const [currentSearchCount, setSearchCount] = useState(0);
-  const [currentMovie, setCurrentMovie] = useState<Movie | null>(null);
-  const [isError, setError] = useState<boolean>(false);
-  const [isLoading, setLoading] = useState(false);
-
-  const handleSearch = (term: string) => {
-    setSearchTerm(term);
-  };
-
-  const handleFind = () => {
-    console.log('searching with term', currentSearchTerm);
-    setLoading(true);
-    setSearchCount(prevCount => prevCount + 1);
-    getMovie(currentSearchTerm)
-      .then(movieData => {
-        if (isResponseError(movieData)) {
-          setError(true);
-        } else {
-          setError(false);
-          console.log('movie data in app', movieData);
-          const normalizedMovie = normalizeMovieData(movieData);
-
-          setCurrentMovie(normalizedMovie);
-          setLoading(false);
-          // ... handle movie data
-        }
-      })
-      .catch(error => {
-        setError(true);
-        console.log(error);
-      })
-      .finally(() => {
-        // This code will run whether the Promise is resolved or rejected
-        // You can add any cleanup or finalization logic here
-      });
-  };
-
-  const handleAddMovie = () => {
-    if (currentMovie !== null) {
-      const isAlreadyAdded = movies.some(
-        movie => movie.imdbId === currentMovie.imdbId,
-      );
-
-      if (!isAlreadyAdded) {
-        setMovies(prevMovies => [...prevMovies, currentMovie]);
-      }
-
-      setCurrentMovie(null);
-    } else {
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    console.log(currentSearchTerm, 'currentSearch');
-  }, [currentSearchTerm]);
-
-  useEffect(() => {
-    console.log('Error', isError);
-  }, [isError]);
-
-  useEffect(() => {
-    console.log('Current normalized Movie', currentMovie);
-  }, [currentMovie]);
 
   useEffect(() => {
     console.log('movies array =', movies);
@@ -91,14 +23,8 @@ export const App: React.FC = () => {
 
       <div className="sidebar">
         <FindMovie
-          onSearch={handleSearch}
-          currentSearchTerm={currentSearchTerm}
-          currentSearchCount={currentSearchCount}
-          onFind={handleFind}
-          onAddMovie={handleAddMovie}
-          movie={currentMovie}
-          isLoading={isLoading}
-          isError={isError}
+          movies={movies}
+          setMovies={setMovies}
         />
       </div>
     </div>
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,32 +1,83 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import './FindMovie.scss';
 import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
+import { getMovie } from '../../api';
+import { isResponseError } from '../../_utils/isResponseError';
+import { normalizeMovieData } from '../../_utils/normalizeMovieData';
 
 interface FindMovieProps {
-  onSearch: (term: string) => void;
-  currentSearchTerm: string;
-  currentSearchCount: number;
-  onFind: () => void;
-  isLoading: boolean;
-  isError: boolean;
-  onAddMovie: (movie: Movie) => void;
-  movie: Movie | null;
+  movies: Movie[];
+  setMovies: React.Dispatch<React.SetStateAction<Movie[]>>;
 }
 
-export const FindMovie: React.FC<FindMovieProps> = (
-  {
-    onSearch,
-    currentSearchTerm,
-    currentSearchCount,
-    onFind,
-    isLoading,
-    isError,
-    onAddMovie,
-    movie,
-  },
-) => {
+export const FindMovie: React.FC<FindMovieProps> = ({ movies, setMovies }) => {
+  const [currentSearchTerm, setSearchTerm] = useState<string>('');
+  const [currentSearchCount, setSearchCount] = useState(0);
+  const [currentMovie, setCurrentMovie] = useState<Movie | null>(null);
+  const [isError, setError] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+  };
+
+  const handleFind = async () => {
+    console.log('searching with term', currentSearchTerm);
+    setLoading(true);
+    setSearchCount(prevCount => prevCount + 1);
+
+    try {
+      const movieData = await getMovie(currentSearchTerm);
+
+      if (isResponseError(movieData)) {
+        setError(true);
+        setCurrentMovie(null);
+      } else {
+        setError(false);
+        console.log('movie data in app', movieData);
+        const normalizedMovie = normalizeMovieData(movieData);
+
+        setCurrentMovie(normalizedMovie);
+      }
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleAddMovie = () => {
+    if (currentMovie !== null) {
+      const isAlreadyAdded = movies.some(
+        movie => movie.imdbId === currentMovie.imdbId,
+      );
+
+      if (!isAlreadyAdded) {
+        setMovies(prevMovies => [...prevMovies, currentMovie]);
+      }
+
+      setCurrentMovie(null);
+    } else {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    console.log('Error', isError);
+  }, [isError]);
+
+  useEffect(() => {
+    console.log('Current normalized Movie', currentMovie);
+  }, [currentMovie]);
+
+  useEffect(() => {
+    console.log(currentSearchTerm, 'currentSearch');
+  }, [currentSearchTerm]);
+
   return (
     <>
       <form className="find-movie">
@@ -42,7 +93,7 @@ export const FindMovie: React.FC<FindMovieProps> = (
               id="movie-title"
               placeholder="Enter a title to search"
               className={cn('input', { 'is-danger': isError })}
-              onChange={(e) => onSearch(e.target.value)}
+              onChange={(e) => handleSearch(e.target.value)}
             />
           </div>
 
@@ -58,8 +109,10 @@ export const FindMovie: React.FC<FindMovieProps> = (
             <button
               data-cy="searchButton"
               type="button"
-              className={cn('button is-light', { 'is-loading': isLoading })}
-              onClick={onFind}
+              className={cn('button is-light', {
+                'is-loading': isLoading && !isError,
+              })}
+              onClick={handleFind}
               disabled={!currentSearchTerm}
             >
               {currentSearchCount > 0 ? 'Search Again' : 'Find a movie'}
@@ -67,12 +120,12 @@ export const FindMovie: React.FC<FindMovieProps> = (
           </div>
 
           <div className="control">
-            {movie && (
+            {currentMovie && (
               <button
                 data-cy="addButton"
                 type="button"
                 className="button is-primary"
-                onClick={() => onAddMovie(movie)}
+                onClick={handleAddMovie}
               >
                 Add to the list
               </button>
@@ -82,13 +135,12 @@ export const FindMovie: React.FC<FindMovieProps> = (
       </form>
 
       <div className="container" data-cy="previewContainer">
-        {movie && (
+        {currentMovie && (
           <>
-            <h2 className="title">Preview</h2>
-            <MovieCard movie={movie} />
+            <h2 className="title" data-cy="movieTitle">Preview</h2>
+            <MovieCard movie={currentMovie} />
           </>
         )}
-
       </div>
     </>
   );

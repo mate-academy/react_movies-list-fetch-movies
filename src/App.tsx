7e8b257c96f2928af035b415b6b9b5bcf94ed5ec
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-11-01 16:23:32 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,18 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddMovie = (movie: Movie) => {
+    const isExist = movies.find(m => m.imdbId === movie.imdbId);
+
+    if (!isExist) {
+      setMovies(prevMovies => ([
+        ...prevMovies,
+        movie,
+      ]));
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +25,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAddMovie={handleAddMovie} />
       </div>
     </div>
   );

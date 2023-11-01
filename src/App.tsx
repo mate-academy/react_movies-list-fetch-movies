59a27f418eb6dfb8eec25811a9940efac2c083d4
Helen Tsyhypalo <lena.tsigipalo@gmail.com>
2023-11-01 19:31:25 +0100

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import { useState } from 'react';
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
+import { getMovie } from './api';
+import { MovieData } from './types/MovieData';
+import { ResponseError } from './types/ReponseError';
+
+const POSTER = 'https://via.placeholder.com/360x270.png?text=no%20preview';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [receivedMovie, setReceivedMovie] = useState<Movie | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<ResponseError | null>(null);
+
+  const onSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    getMovie(query)
+      .then((data: MovieData | ResponseError) => {
+        if ('Poster' in data) {
+          setReceivedMovie({
+            title: data.Title,
+            description: data.Plot,
+            imgUrl: data.Poster === 'N/A' ? POSTER : data.Poster,
+            imdbUrl: `https://www.imdb.com/title/${data.imdbID}`,
+            imdbId: data.imdbID,
+          });
+        } else {
+          setError(data);
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const onAddMovie = () => {
+    if (!movies.some(movie => movie.imdbId === receivedMovie?.imdbId)) {
+      setMovies([...movies, receivedMovie] as Movie[]);
+    }
+
+    setQuery('');
+    setReceivedMovie(null);
+  };
+
+  const onChangeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    setError(null);
+  };
 
   return (
     <div className="page">
@@ -14,7 +58,15 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          query={query}
+          onChangeQuery={onChangeQuery}
+          movie={receivedMovie}
+          onAddMovie={onAddMovie}
+          onSubmit={onSubmitForm}
+          error={error}
+          loading={loading}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import classNames from 'classnames';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
+import { ResponseError } from '../../types/ReponseError';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  query: string,
+  onChangeQuery: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  movie: Movie | null,
+  onAddMovie: () => void,
+  onSubmit: (event: React.FormEvent) => void,
+  error: ResponseError | null,
+  loading: boolean,
+};
+
+export const FindMovie: React.FC<Props> = ({
+  query,
+  onChangeQuery,
+  movie,
+  onAddMovie,
+  onSubmit,
+  error,
+  loading,
+}) => {
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={onSubmit}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -12,46 +37,60 @@ export const FindMovie: React.FC = () => {
 
           <div className="control">
             <input
+              value={query}
               data-cy="titleField"
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={classNames('input', {
+                'is-danger': error,
+              })}
+              onChange={onChangeQuery}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {error && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
           <div className="control">
             <button
+              disabled={!query}
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames('button is-light', {
+                'is-loading': loading,
+              })}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={onAddMovie}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

455e06e2b93171d74e6925b0cacac8a72428a1cf
Anna Shevtsova <aniashevtsova@gmail.com>
2023-11-02 11:02:24 +0100

    all tests pass

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
+import { Movie } from './types/Movie';
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?apikey=eec79111';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)
@@ -11,3 +12,15 @@ export function getMovie(query: string): Promise<MovieData | ResponseError> {
       Error: 'unexpected error',
     }));
 }
+
+export const normalizeMovieData = (rawData: MovieData): Movie => {
+  return {
+    title: rawData.Title,
+    description: rawData.Plot,
+    imdbId: rawData.imdbID,
+    imgUrl: rawData.Poster !== 'N/A'
+      ? rawData.Poster
+      : 'https://via.placeholder.com/360x270.png?text=no%20preview',
+    imdbUrl: `https://www.imdb.com/title/${rawData.imdbID}`,
+  };
+};

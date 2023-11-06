9d5ae6c02913746a1d2182182ad65a0a77fde84d
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-11-06 11:46:58 +0100

    task solution

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+export const API_URL = 'https://www.omdbapi.com/?apikey=f4e715f7';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

4ecb6902194d3c65f90aed914cfa271db91b5be0
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-11-07 10:23:31 +0200

    add task solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  movies: Movie[]
+  addMovie: (newMovies: Movie[]) => void
+};
+
+export const FindMovie: React.FC<Props> = ({ movies, addMovie }) => {
+  const [isError, setIsError] = useState(false);
+  const [isVisibleMovieCard, setIsVisibleMovieCard] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [movie, setMovie] = useState<Movie>({
+    title: 'Title',
+    description: 'Plot',
+    imgUrl: 'Poster',
+    imdbUrl: 'imdbUrl',
+    imdbId: 'imdbID',
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsError(false);
+    setInputValue(event.target.value);
+  };
+
+  const handleSearchBtn = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    getMovie(inputValue).then((result) => {
+      if ('Error' in result) {
+        setIsError(true);
+      } else {
+        setIsVisibleMovieCard(true);
+
+        const {
+          Title,
+          Plot,
+          Poster,
+          imdbID,
+        } = result;
+
+        const selectedMovie: Movie = {
+          title: Title,
+          description: Plot,
+          imgUrl: Poster === 'N/A'
+            ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+            : Poster,
+          imdbUrl: `https://www.imdb.com/title/${imdbID}`,
+          imdbId: imdbID,
+        };
+
+        setMovie(selectedMovie);
+      }
+    })
+      .finally(() => {
+        setIsLoading(false); // Set isLoading to false when the request is complete
+      });
+  };
+
+  const handleAddMovieBtn = () => {
+    if (!movies.find(movieItem => movieItem.imdbId === movie.imdbId)) {
+      const newMovies = [...movies, movie];
+
+      addMovie(newMovies);
+    }
+
+    setIsVisibleMovieCard(false);
+    setInputValue('');
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -16,13 +90,19 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className="input"
+              value={inputValue}
+              onChange={handleInputChange}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {
+            isError && (
+              <p className="help is-danger" data-cy="errorMessage">
+                Can&apos;t find a movie with such a title
+              </p>
+            )
+          }
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +110,34 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={`button is-light ${isLoading ? 'is-loading' : ''}`}
+              onClick={handleSearchBtn}
+              disabled={!inputValue}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {isVisibleMovieCard && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAddMovieBtn}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
-
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {isVisibleMovieCard && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

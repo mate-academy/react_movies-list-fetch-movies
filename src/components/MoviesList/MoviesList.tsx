0f1142117827fe6267865dd4f92b8842937b2629
FurArt <iiionerart@gmail.com>
2023-11-21 22:35:48 +0200

    Added solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,18 +3,24 @@ import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
+import { MovieData } from '../../types/MovieData';
 
 type Props = {
   movies: Movie[];
+  previewMovie?: MovieData | null;
 };
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<Props> = ({ movies, previewMovie }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard
-        key={movie.imdbId}
-        movie={movie}
-      />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
+
+    {previewMovie && (
+      <MovieCard
+        key={previewMovie.imdbID}
+        movie={previewMovie}
+      />
+    )}
   </div>
 );

77c9ec9a2f7b7dc609b5f0a439a97292937d1827
Dmytro Krasnoholovenko <errisi.v@gmail.com>
2023-12-04 15:10:05 +0200

    result

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovies(prevMovies => {
+      return prevMovies
+        .find(movie => movie.imdbId === newMovie.imdbId)
+        ? prevMovies
+        : [...prevMovies, newMovie];
+    });
+  };
 
   return (
     <div className="page">
@@ -14,7 +23,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovie={addMovie} />
       </div>
     </div>
   );

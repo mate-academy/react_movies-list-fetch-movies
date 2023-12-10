3d69f4a063bdbad49686a1a72ad81787c7c58327
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-10 23:45:17 +0200

    movies list

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_KEY = '143cb8d8';
+
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
   return fetch(`${API_URL}&t=${query}`)

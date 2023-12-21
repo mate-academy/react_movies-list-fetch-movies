19dd32904cfb00bac3e5b24609a99f85f1c0b82c
Sergej <sergej161995@gmail.com>
2023-12-21 22:22:45 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
-import { Movie } from './types/Movie';
+import type { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovies(currentMovies => {
+      const isAlreadyAdded = currentMovies.some(
+        movie => movie.imdbId === newMovie.imdbId,
+      );
+
+      if (isAlreadyAdded) {
+        return currentMovies;
+      }
+
+      return [...currentMovies, newMovie];
+    });
+  };
 
   return (
     <div className="page">
@@ -14,7 +29,9 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          onAdd={addMovie}
+        />
       </div>
     </div>
   );

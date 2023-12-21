541d543dd7358d81ab5e781c98ef36762f77ea91
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-12-21 12:23:54 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,76 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
+import { getMovie } from './api';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchedMovie, setSearchedMovie] = useState<Movie>({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+  const [title, setTitle] = useState<string>('');
+  const [isTitle, setIsTitle] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleAddMovie = (newMovie: Movie) => {
+    const isDuplicate
+      = movies.some((movie) => movie.imdbId === newMovie.imdbId);
+
+    if (!isDuplicate) {
+      setMovies(prevMovie => [...prevMovie, newMovie]);
+    }
+
+    setTitle('');
+    const resetMovie = {
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    };
+
+    setSearchedMovie(resetMovie);
+  };
+
+  useEffect(() => {
+    if (!title) {
+      return;
+    }
+
+    setIsLoading(false);
+    getMovie(title)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then((movie: any) => {
+        if (movie.Response === 'False' && title) {
+          setIsTitle(true);
+        } else {
+          const newMovie = {
+            title: movie.Title,
+            description: movie.Plot,
+            imgUrl: movie.Poster !== 'N/A' ? movie.Poster
+              : 'https://via.placeholder.com/360x270.png?text=no%20preview',
+            imdbUrl: `https://www.imdb.com/title/${movie.imdbID}`,
+            imdbId: movie.imdbID,
+          };
+
+          setSearchedMovie(newMovie);
+          setIsTitle(false);
+        }
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Unexpected error:', error);
+      })
+      .finally(() => {
+        setIsLoading(true);
+      });
+  }, [title, searchedMovie.title]);
 
   return (
     <div className="page">
@@ -14,7 +79,14 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie
+          setIsTitle={setIsTitle}
+          setTitle={setTitle}
+          movie={searchedMovie}
+          isTitle={isTitle}
+          isLoading={isLoading}
+          handleAddMovie={handleAddMovie}
+        />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {
+  Dispatch, SetStateAction, useEffect, useState,
+} from 'react';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+interface FindMovieProps {
+  setIsTitle: Dispatch<SetStateAction<boolean>>
+  setTitle: Dispatch<SetStateAction<string>>
+  movie: Movie;
+  isTitle: boolean;
+  isLoading: boolean;
+  handleAddMovie: (movie: Movie) => void
+}
+
+export const FindMovie: React.FC<FindMovieProps> = (
+  {
+    setIsTitle, setTitle, movie, isTitle, isLoading, handleAddMovie,
+  },
+) => {
+  const [query, setQuerty] = useState<string>('');
+
+  const handleFormSubmint = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    (setTitle(query));
+  };
+
+  useEffect(() => {
+    setIsTitle(false);
+  }, [query, setIsTitle]);
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={(event) => handleFormSubmint(event)}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +47,17 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className="input"
+              value={query}
+              onChange={(event) => setQuerty(event.target.value)}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {(isTitle) ? (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          ) : null}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +65,38 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={`button ${isLoading ? 'is-light' : 'is-loading'}`}
+              {...(!query && { disabled: true })}
             >
-              Find a movie
+              {movie.title ? 'Search again' : 'Find a movie' }
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {(movie.title) && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  handleAddMovie(movie);
+                  setQuerty('');
+                  setTimeout(() => setQuerty(''), 0);
+                }}
+              >
+                Add to the list
+              </button>
+            </div>
+          ) }
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie.title && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

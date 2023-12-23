9e2d1a3f24fa72b1433e8f74ba86e2dda130d668
Denys Khvashchenko <superdened@gmail.com>
2023-12-23 16:43:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSetMovies = (movie: Movie) => {
+    const hasDuplicate = movies.find(film => film.imdbId === movie.imdbId);
+
+    if (!hasDuplicate) {
+      setMovies(prevMovies => [...prevMovies, movie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +22,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovie={handleSetMovies} />
       </div>
     </div>
   );

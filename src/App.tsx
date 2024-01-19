4c13a4981647d6d8f9e13796f35c822d0cf5dcb9
Kukharuk <its.up.to.you.dk@gmail.com>
2024-01-19 20:42:47 +0100

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
-export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAdd = (movie: Movie) => {
+    const duplicate = movies.find(film => film.imdbId === movie.imdbId);
+
+    if (!duplicate) {
+      setMovies(prev => [...prev, movie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +22,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovies={handleAdd} />
       </div>
     </div>
   );

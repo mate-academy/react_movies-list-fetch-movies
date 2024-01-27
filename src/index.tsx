f30ca9faae2f46a8075a71eed875d9bbf6eb2d0e
Ivan <fanmetalheavy@gmail.com>
2024-01-27 12:54:57 +0200

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import { App } from './App';
+import { MovieProvider } from './components/Store/Store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MovieProvider>
+    <App />
+  </MovieProvider>,
+  document.getElementById('root'),
+);
---
+++ src/services/isResponseError.ts
@@ -0,0 +1,11 @@
+import { MovieData } from '../types/MovieData';
+import { ResponseError } from '../types/ReponseError';
+
+export function isResponseError(
+  data: MovieData | ResponseError,
+): data is ResponseError {
+  return (
+    (data as ResponseError).Response === 'False'
+    && (data as ResponseError).Error !== ''
+  );
+}

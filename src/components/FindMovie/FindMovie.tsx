ec2e461e8d17ef707a3afedb4ffafccfee0ebdd7
Oleh Marushchak <westernpc22@gmail.com>
2024-04-11 21:09:28 +0300

    add fix

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './FindMovie.scss';
+import { DispatchContext, StateContext } from '../../context/ReduxContex';
+import { MovieCard } from '../MovieCard';
+import cn from 'classnames';
 
 export const FindMovie: React.FC = () => {
+  const { movie, query, error, submit } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={event => event.preventDefault()}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -12,45 +18,62 @@ export const FindMovie: React.FC = () => {
 
           <div className="control">
             <input
+              onChange={event =>
+                dispatch({ type: 'setQuery', value: event.target.value })
+              }
               data-cy="titleField"
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={cn('input', { 'is-danger': error })}
+              value={query}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {error && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
           <div className="control">
             <button
+              onClick={() => dispatch({ type: 'onSubmit' })}
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={cn('button is-light', {
+                'is-loading': submit && !movie,
+              })}
+              disabled={!query.length}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && (
+            <div className="control">
+              <button
+                onClick={() => dispatch({ type: 'addMovie' })}
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
       <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
+        {movie && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard movie={movie} />
+          </>
+        )}
       </div>
     </>
   );

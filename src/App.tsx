aa38e555c6251dedd97039dcfe7030e8120307c5
Robert Szałapak <robsza1212@gmail.com>
2024-05-22 20:08:19 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const handleAddMovie = useCallback(
+    (movie: Movie) => {
+      const newMovie = movies.find(
+        currentMovie => currentMovie.imdbId === movie.imdbId,
+      );
+
+      if (movies.length === 0 || !newMovie) {
+        setMovies(prevMovies => [...prevMovies, movie]);
+      }
+    },
+    [movies],
+  );
 
   return (
     <div className="page">
@@ -14,7 +26,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie handleAddMovie={handleAddMovie} />
       </div>
     </div>
   );
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { getMovie } from '../../api';
+import { MovieData } from '../../types/MovieData';
+import { ResponseError } from '../../types/ReponseError';
+import { MovieCard } from '../MovieCard';
+import classNames from 'classnames';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  handleAddMovie: (movie: Movie) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ handleAddMovie }) => {
+  const [movie, setMovie] = useState<Movie | undefined>(undefined);
+  const [moviesTitle, setMovieTitle] = useState<string>('');
+  const [movieIsFound, setMovieIsFound] = useState<boolean>(false);
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const changeDataIntoMovie = (movieData: MovieData) => {
+    const changeDataInMovie: Movie = {
+      title: movieData.Title,
+      description: movieData.Plot,
+      imgUrl: movieData.Poster,
+      imdbUrl: `https://www.omdbapi.com/?apikey=dd841bde&t=${movieData.imdbID}`,
+      imdbId: movieData.imdbID,
+    };
+
+    return changeDataInMovie;
+  };
+
+  const handleSearchButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setLoader(true);
+    setMovieIsFound(true);
+
+    getMovie(moviesTitle)
+      .then((response: MovieData | ResponseError) => {
+        if ('Error' in response) {
+          setMovieIsFound(false);
+          setLoader(false);
+
+          return;
+        }
+
+        const moviesState = changeDataIntoMovie(response);
+
+        setMovie(moviesState);
+        setMovieIsFound(true);
+        setLoader(true);
+      })
+      .finally(() => setLoader(false));
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMovieTitle(event.target.value);
+  };
+
+  const handleAddButton = () => {
+    if (movie) {
+      handleAddMovie(movie);
+    }
+
+    setMovie(undefined);
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -16,13 +78,17 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className="input"
+              value={moviesTitle}
+              onChange={handleInput}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {movie && !movieIsFound && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +96,37 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames('button is-light', {
+                'is-loading': loader,
+              })}
+              onClick={e => handleSearchButton(e)}
+              disabled={moviesTitle === ''}
             >
               Find a movie
             </button>
           </div>
 
           <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
+            {movie && moviesTitle && (
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAddButton}
+              >
+                Add to the list
+              </button>
+            )}
           </div>
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie !== undefined && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };

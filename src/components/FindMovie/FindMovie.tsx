52175ddad50786f92c103c2047e7ef4b1052c000
Denys209818 <deniskravcuk63@gmail.com>
2024-05-24 20:18:07 +0300

    Load full realizarion

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './FindMovie.scss';
+import { getMovie } from '../../api';
+import { MovieData } from '../../types/MovieData';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+import classNames from 'classnames';
+import { MovieContext } from '../MovieContext/MovieContextProvider';
 
 export const FindMovie: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const { setMovies, movies } = useContext(MovieContext);
+
+  const clickEvent = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    getMovie(title).then(respose => {
+      if (respose) {
+        const res = respose as MovieData;
+
+        if (res.Plot && res.imdbID) {
+          setMovie({
+            title: res.Title,
+            imdbId: res.imdbID,
+            imdbUrl: res.Plot,
+            imgUrl: res.Poster,
+            description: res.Plot,
+          });
+        } else {
+          setMovie({
+            title: '',
+            imdbId: '',
+            imdbUrl: '',
+            imgUrl: '',
+            description: '',
+          });
+        }
+      }
+    });
+  };
+
+  const addToList = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (movie) {
+      setMovies([...movies, movie]);
+    }
+
+    setMovie(null);
+    setTitle('');
+  };
+
   return (
     <>
       <form className="find-movie">
@@ -16,13 +67,19 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              className={classNames('input', {
+                'is-danger': movie && !movie.title,
+              })}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {movie && !movie.title && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -31,27 +88,33 @@ export const FindMovie: React.FC = () => {
               data-cy="searchButton"
               type="submit"
               className="button is-light"
+              disabled={!title}
+              onClick={e => clickEvent(e)}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && movie.title && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={e => addToList(e)}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
-
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && movie.title && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import { App } from './App';
+// eslint-disable-next-line max-len
+import { MovieContextProvider } from './components/MovieContext/MovieContextProvider';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <MovieContextProvider>
+    <App />
+  </MovieContextProvider>,
+);

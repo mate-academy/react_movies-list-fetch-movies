41607308f0d19dd02632f9f8899bb5a49f965476
Dmytro <107893428+dima91020@users.noreply.github.com>
2024-06-25 03:22:17 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovies(currentMovies =>
+      currentMovies.find(
+        currentMovie => currentMovie.imdbId === newMovie.imdbId,
+      )
+        ? currentMovies
+        : [...currentMovies, newMovie],
+    );
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie AddMovie={addMovie} />
       </div>
     </div>
   );

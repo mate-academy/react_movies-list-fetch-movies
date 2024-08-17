d2f25aa024efc88369217394643d936af0931fa6
Teollan <serhiikofficial@gmail.com>
2024-08-17 14:26:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = useCallback(
+    (newMovie: Movie) => {
+      if (movies.find(movie => movie.imdbId === newMovie.imdbId)) {
+        return;
+      }
+
+      setMovies([...movies, newMovie]);
+    },
+    [movies],
+  );
 
   return (
     <div className="page">
@@ -14,7 +25,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onAddToList={addMovie} />
       </div>
     </div>
   );

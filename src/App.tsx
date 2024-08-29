46e9c9153d9cce48edb3115b6653243898729ae7
iryna lysko <goiralysko@gmail.com>
2024-08-29 23:27:48 +0300

    get movies from the OMDb API

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (newMovie: Movie) => {
+    setMovies(currMovies => {
+      if (currMovies.some(movie => movie.imdbId === newMovie.imdbId)) {
+        return currMovies;
+      }
+
+      return [...currMovies, newMovie];
+    });
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie addMovieToTheList={addMovie} />
       </div>
     </div>
   );

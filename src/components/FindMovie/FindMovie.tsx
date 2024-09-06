43b9304a2efd438634bed7eb3d4547d71b6dc1c1
Olya <olyavidzi97@gmail.com>
2024-09-06 10:54:59 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FindMovie.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+import { getMovie } from '../../api';
+import cn from 'classnames';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  addMovieToList: (movie: Movie) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ addMovieToList }) => {
+  const [query, setQuery] = useState<string>('');
+  const [foundMovie, setFoundMovie] = useState<Movie | null>(null);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    getMovie(query)
+      .then(movieData => {
+        if ('Error' in movieData) {
+          setErrorMessage(true);
+          setQuery('');
+        } else {
+          const newMovie = {
+            title: movieData.Title,
+            description: movieData.Plot,
+            imgUrl:
+              movieData.Poster === 'N/A'
+                ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+                : movieData.Poster,
+            imdbUrl: 'https://www.imdb.com/title/' + movieData.imdbID,
+            imdbId: movieData.imdbID,
+          };
+
+          setFoundMovie(newMovie);
+        }
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const handleTitleMovie = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleAddToTheList = () => {
+    if (foundMovie) {
+      addMovieToList(foundMovie);
+      setQuery('');
+      setErrorMessage(false);
+      setFoundMovie(null);
+    }
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSubmit}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +69,17 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={cn('input', { 'is-danger': errorMessage })}
+              value={query}
+              onChange={handleTitleMovie}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {errorMessage && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,27 +87,37 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              disabled={!query.trim()}
+              className={cn('button is-light', {
+                'is-loading': loading,
+              })}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {foundMovie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={handleAddToTheList}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
       <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
+        {foundMovie && (
+          <>
+            <h2 className="title">Preview</h2>
+            <MovieCard movie={foundMovie} />
+          </>
+        )}
       </div>
     </>
   );

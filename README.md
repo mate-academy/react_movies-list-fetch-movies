985be962e9b4b3f46bec4f0e453b38464163aca3
Wojtek <wojciechsadocha@gmail.com>
2024-09-09 13:50:12 +0200

    Solution

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Implement the `FindMovie` component to load movies from [OMDb API](http://www.omdbapi.com/) (You need to register and get an API key).
 
+my key: 364c05b4
+
 1. When a user enters a title and submits the form, send a request to `https://www.omdbapi.com/?apikey=[yourkey]&t=[title]`;
    - use the `getMovie` method from the `api.ts`;
 1. The submit button should be disabled when the title field is empty;
@@ -25,4 +27,4 @@ Implement the `FindMovie` component to load movies from [OMDb API](http://www.om
 - Implement a solution following the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline).
 - Use the [React TypeScript cheat sheet](https://mate-academy.github.io/fe-program/js/extra/react-typescript).
 - Open one more terminal and run tests with `npm test` to ensure your solution is correct.
-- Replace `<your_account>` with your Github username in the [DEMO LINK](https://<your_account>.github.io/react_movies-list-fetch-movies/) and add it to the PR description.
+- Replace `<your_account>` with your Github username in the [DEMO LINK](https://wsadocha.github.io/react_movies-list-fetch-movies/) and add it to the PR description.
--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,76 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
 import './FindMovie.scss';
+import cn from 'classnames';
+import { getMovie } from '../../api';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard/MovieCard';
+import { MovieData } from '../../types/MovieData';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  movies: Movie[];
+  setMovies: (movies: Movie[]) => void;
+};
+
+const DEFAULT_IMAGE =
+  'https://via.placeholder.com/360x270.png?text=no%20preview';
+
+export const FindMovie: React.FC<Props> = ({ movies, setMovies }) => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isMovieNotFound, setIsMovieNotFound] = useState(false);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    getMovie(query)
+      .then(mov => {
+        if ('Response' in mov && mov.Response === 'False') {
+          setMovie(null);
+          setIsMovieNotFound(true);
+        } else {
+          const movieData = mov as MovieData;
+
+          const {
+            Title: title,
+            Plot: description,
+            Poster: imgUrl,
+            imdbID: imdbId,
+          } = movieData;
+
+          const image = imgUrl !== 'N/A' ? imgUrl : DEFAULT_IMAGE;
+
+          setMovie({
+            title,
+            description,
+            imgUrl: image,
+            imdbUrl: `https://www.imdb.com/title/${imdbId}`,
+            imdbId,
+          });
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleAdd = (newMovie: Movie) => {
+    const isAdded = movies.filter(
+      singleMovie => singleMovie.imdbId === newMovie.imdbId,
+    );
+
+    if (newMovie && isAdded.length === 0) {
+      setMovies([...movies, newMovie]);
+      setMovie(null);
+      setQuery('');
+    } else {
+      setMovie(null);
+      setQuery('');
+    }
+  };
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handleSubmit}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +82,22 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={cn('input', {
+                'is-danger': isMovieNotFound,
+              })}
+              value={query}
+              onChange={event => {
+                setQuery(event.target.value);
+                setIsMovieNotFound(false);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {isMovieNotFound && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +105,36 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={cn('button is-light', {
+                'is-loading': isLoading,
+              })}
+              disabled={query === ''}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={() => handleAdd(movie)}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          {<MovieCard movie={movie} />}
+        </div>
+      )}
     </>
   );
 };

c2cfc39031c981557a81afd85b8230b3ce1b9fdf
Seva Podolskiy <s_podolskiy@live.com>
2024-09-17 11:07:02 -0400

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleAddingMovie = (movie: Movie) => {
+    if (movies.filter(mvs => mvs.imdbId === movie.imdbId).length === 0) {
+      setMovies(prev => [...prev, movie]);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +20,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onMovieAdd={handleAddingMovie} />
       </div>
     </div>
   );

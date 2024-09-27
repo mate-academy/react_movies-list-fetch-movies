be179190343658b4e132b8a034a0dee013b08af2
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-09-27 18:06:46 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (movie: Movie) => {
+    const isRepeate = movies.find(item => item.title === movie.title);
+
+    if (!isRepeate) {
+      setMovies([...movies, movie]);
+    } else {
+      setMovies(movies);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onMovies={addMovie} />
       </div>
     </div>
   );

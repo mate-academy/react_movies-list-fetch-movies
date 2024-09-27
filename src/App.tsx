5f594da50d735eb6793eabf4fefe1b94cc95b0ce
Kirill Gasan <dp160190gkj@gmail.com>
2024-09-27 19:38:09 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import { FindMovie } from './components/FindMovie';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const addMovie = (movie: Movie) => {
+    const isRepeate = movies.find(item => item.title === movie.title);
+
+    if (!isRepeate) {
+      setMovies([...movies, movie]);
+    } else {
+      setMovies(movies);
+    }
+  };
 
   return (
     <div className="page">
@@ -14,7 +24,7 @@ export const App = () => {
       </div>
 
       <div className="sidebar">
-        <FindMovie />
+        <FindMovie onMovies={addMovie} />
       </div>
     </div>
   );

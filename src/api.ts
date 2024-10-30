ca8e1b6fe8c056659be41b69446b704d3f121244
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-10-30 17:01:10 +0200

    task is done

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'https://www.omdbapi.com/?apikey=your-key';
+const API_URL = 'https://www.omdbapi.com/?apikey=e4112b8d';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  return fetch(`${API_URL}&t=${query}`)
+  return fetch(`${API_URL}&t=${query.split(' ').join('+')}`)
     .then(res => res.json())
     .catch(() => ({
       Response: 'False',

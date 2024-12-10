b35ecfd072400d550862d313f6450abb6b7bc090
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-12-10 13:09:46 +0200

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import './FindMovie.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const FindMovie: React.FC = () => {
+type FindMovieProps = {
+  onSearch: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  query: string;
+  onQuery: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  movie: Movie | null;
+  search: string;
+  isLoading: boolean;
+  onAddMovie: () => void;
+};
+
+export const FindMovie: React.FC<FindMovieProps> = ({
+  onSearch,
+  query,
+  onQuery,
+  movie,
+  search,
+  isLoading,
+  onAddMovie,
+}: FindMovieProps) => {
   return (
     <>
       <form className="find-movie">
@@ -16,13 +36,17 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={`input ${movie?.title === undefined && search === query && query !== '' && 'is-danger'}`}
+              value={query}
+              onChange={onQuery}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {movie?.title === undefined && search === query && query !== '' && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +54,34 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={`button is-light ${isLoading && 'is-loading'}`}
+              disabled={query === ''}
+              onClick={onSearch}
             >
               Find a movie
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie?.title !== undefined && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={onAddMovie}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
-
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie?.title !== undefined && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };
